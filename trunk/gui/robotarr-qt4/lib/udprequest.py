ba299288f6a15037a7b4b17be23a5bd43dec3fb6
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2012-03-10 03:47:59 +0000

    first commit

---
+++ trunk/gui/robotarr-qt4/lib/udprequest.py
@@ -0,0 +1,20 @@
+from socket import *
+
+class UDPRequest:
+
+	def __init__(self, host, port):
+		self.buf = 1024
+		self.addr = (host, port)
+		self.sock = socket(AF_INET,SOCK_DGRAM)
+
+	def __del__(self):
+		self.sock.close()
+
+	def command(self, cmd, *args, **kwds):
+		if args:
+			cmd += ':' + ','.join([str(arg) for arg in args]) 
+		if kwds:
+			cmd += ':' + ','.join(['%s=%s' % (n,v) for n,v in kwds.items()])
+		self.sock.sendto(cmd, self.addr)
+		reply,addr = self.sock.recvfrom(self.buf)
+		return reply
