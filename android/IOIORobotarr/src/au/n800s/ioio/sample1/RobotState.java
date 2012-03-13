cc5b019496add2b889ebd28f3e1473ee4ee79a18
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2012-03-13 05:54:49 +0000

    keep and no keep

--- android/IOIORobotarr/src/au/n800s/ioio/sample1/RobotState.java
+++ android/IOIORobotarr/src/au/n800s/ioio/sample1/RobotState.java
@@ -15,7 +15,40 @@ public class RobotState(JSONObject) {
 		put("pitch", 25.);
 		put("roll", 60.);
 		put("heading", 90.);
+		put("current_heading", -1);
 		put("error", "");
 	}
 
+	synchronized public void x_put(String key, String value) {
+		put(key, value);
+	}
+
+	synchronized public void x_put(String key, boolean value) {
+		put(key, value);
+	}
+
+	synchronized public void x_put(String key, double value) {
+		put(key, value);
+	}
+
+	synchronized public void x_put(String key, int value) {
+		put(key, value);
+	}
+
+	synchronized public String x_getString(String key) {
+		return getString(key);
+	}
+	
+	synchronized public boolean x_getBoolean(String key) {
+		return getBoolean(key);
+	}
+	
+	synchronized public int x_getInt(String key) {
+		return getInt(key);
+	}
+	
+	synchronized public double x_getDouble(String key) {
+		return getDouble(key);
+	}
+	
 }
