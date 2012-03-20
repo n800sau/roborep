e32ba03bfd0930063d4f2b672007fc52f3faf4fe
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2012-03-20 03:59:14 +0000

    bytes2short and reverse

---
+++ trunk/3pi-serial-slave/cnv.c
@@ -0,0 +1,34 @@
+//#include <stdio.h>
+#include <math.h>
+
+#define MAXVAL 0x1FFF
+#define MINVAL -0x1FFF
+
+short bytes2short(char lb, char hb)
+{
+	short rs = (((short)hb & 0x3F)<<7) + lb;
+	if(hb & 0x40) {
+		rs = -rs;
+	}
+	return rs;
+}
+
+void short2bytes(short val, char *bytes)
+{
+	short ainv = abs(val);
+	bytes[0]= ainv & 0x7F;
+	bytes[1] = (ainv >> 7) & 0x3F;
+	if(val < 0) {
+		bytes[1] |= 0x40;
+	}
+}
+
+/*int main()
+{
+	char bytes[2];
+	short2bytes(-200, bytes);
+	printf("%x, %x\n", bytes[0], bytes[1]);
+	short val = bytes2short(bytes[0], bytes[1]);
+	printf("%x, %d\n", val, val);
+	return 0;
+}*/
