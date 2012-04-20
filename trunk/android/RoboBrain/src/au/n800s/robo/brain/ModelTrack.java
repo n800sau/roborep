c936e8c167f408bd7039a0c468e2723c187a54d4
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2012-04-20 06:11:55 +0000

    brain added and models scripts

---
+++ trunk/android/RoboBrain/src/au/n800s/robo/brain/ModelTrack.java
@@ -0,0 +1,17 @@
+package au.n800s.robo.brain;
+
+class ModelTrack extends BaseModel
+{
+
+	@Override
+	public int estimateEnergy()
+	{
+		return super();
+	}
+
+	public String modelService()
+	{
+		return "au.n800s.robo.model.track";
+	}
+
+};
