181b0b5ddcd25e90cdabacbd20802d956fabc347
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2012-05-16 02:36:27 +0000

    arduino_libs added and QTR lib

---
+++ arduino_libs/RobotarrLibs/cycle_check.cpp
@@ -0,0 +1,10 @@
+#include <WProgram.h>
+#include "cycle_check.h"
+
+bool cycleCheck(unsigned long *lastMillis, unsigned int cycle)
+{
+	unsigned long currentMillis = millis();
+	bool rs = currentMillis - *lastMillis >= cycle;
+	if(rs) *lastMillis = currentMillis;
+	return rs;
+}
---
+++ arduino_libs/RobotarrLibs/servo_x.cpp
@@ -0,0 +1,62 @@
+#include <WProgram.h>
+#include "servo_x.h"
+
+ServoX::ServoX():Servo()
+{
+	__angle = -1;
+	__speed = 1;
+	__finished = true;
+}
+
+//speed - degree in secs
+void ServoX::setAngle(int angle, int speed)
+{
+	if(angle >= 0) {
+		__angle = angle;
+		if (speed > 0)
+			__speed = speed;
+		__last_time = millis();
+		__finished = false;
+	}
+}
+
+void ServoX::stop()
+{
+	int angle = read();
+	if(angle != __angle) {
+		setAngle(angle);
+		__finished = true;
+	}
+}
+
+bool ServoX::update()
+{
+	if (__angle >= 0 && !__finished) {
+		unsigned long now = millis();
+//		Serial.print("diff:");
+//		Serial.print(now-__last_time);
+		float step = __speed / 1000. * (now - __last_time);
+//		Serial.print(",step:");
+//		Serial.println(step);
+		if (step >= 1) {
+			__last_time = now;
+			int angle = read();
+			if(angle != __angle) {
+				int dir = (__angle > angle) ? 1 : -1;
+				angle += dir * step;
+				if ((dir > 0 && angle > __angle) || (dir < 0 && angle < __angle))
+					angle = __angle;
+				write(angle);
+			}
+			__finished = angle == __angle;
+		}
+	}
+//			Serial.print("finished:");
+//			Serial.println(__finished);
+	return __finished;
+}
+
+bool ServoX::isFinished()
+{
+	return __finished;
+}
