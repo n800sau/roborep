b3682a5acb8dd6bc1a6c9a9d0180d75194530701
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2012-06-13 00:30:08 +0000

    rfcomm setup script

---
+++ trunk/tank/rfcomm.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+
+
+case "$1" in
+  start)
+		rfcomm bind 0 00:12:01:12:00:52 3
+  ;;
+  stop)
+		rfcomm unbind 0 00:12:01:12:00:52
+  ;;
+  restart|force-reload)
+        $0 stop
+        sleep 1
+        $0 start
+  ;;
+  status)
+        rfcomm
+  ;;
+  *)
+        echo "Usage: $N {start|stop|restart|status}" >&2
+        exit 1
+        ;;
+esac
