53a18874868d75a8b66448c42b7bba482585e907
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2012-10-09 05:28:39 +0000

    polishing communication

--- trunk/pi/utils/read.py
+++ trunk/pi/utils/read.py
@@ -1,21 +1,39 @@
 #!/usr/bin/env python
 
 import serial
-from array import array
+import struct
+import time
 
 ser = serial.Serial('/dev/ttyAMA0', 19200, timeout=1)
 #ser = serial.Serial('/dev/ttyAMA0', 57600, timeout=1)
 #ser = serial.Serial('/dev/ttyAMA0', 4800, timeout=None)
-i = 1
+#i = 0
+timeout = 0
 while True:
 #    print '%.2x ' % ord(ser.read(1))
-    a = array('B')
-    a.append(i%256)
-    ser.write(a.tostring())
-#    ser.write('X<\x0d')
+#    a = array('B')
+#    a.append(i%256)
+#    ser.write(a.tostring())
+    v = struct.pack('ccc', 'X', '<', '.')
+    for c in v:
+        ser.write(c)
+	time.sleep(0.01)
     ser.flush()
-    i += 2
-    print '%s' % ser.readline(),
+#    i += 2
+    while True:
+        line = ser.readline()
+        if line:
+            for c in line:
+                print '%.2x ' % ord(c),
+            print
+            for c in line:
+                print '%.2c ' % c,
+            print
+        else:
+    	    timeout += 1
+	    print 'timeout %d' % timeout
+	    break
+    time.sleep(1)
     
 #while True:
 #    if ser.inWaiting():
