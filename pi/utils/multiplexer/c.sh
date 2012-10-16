1636c5d11c6be777a26f876039ec0a5fa1ebd75e
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2012-10-16 03:11:17 +0000

    more scripts

---
+++ pi/utils/multiplexer/c.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+gcc -o multiplexer multiplexer.c
--- pi/utils/ser2ar.sh
+++ pi/utils/ser2ar.sh
@@ -1,8 +1,2 @@
-gpio-admin export 23
-gpio-admin export 18
-cat /sys/devices/virtual/gpio/gpio18/value && \
-cat /sys/devices/virtual/gpio/gpio23/value && \
-echo out > /sys/devices/virtual/gpio/gpio18/direction && \
-echo out > /sys/devices/virtual/gpio/gpio23/direction && \
 echo 0 > /sys/devices/virtual/gpio/gpio18/value
 echo 0 > /sys/devices/virtual/gpio/gpio23/value
