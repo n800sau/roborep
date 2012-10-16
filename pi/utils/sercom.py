13d12655c505c5a1a46d918cd01ea25e5e31cd2f
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2012-10-16 05:22:41 +0000

    yield in sercom

--- pi/utils/sercom.py
+++ pi/utils/sercom.py
@@ -2,19 +2,21 @@ import serial
 
 class SerCom(serial.Serial):
 
-    def eol_readline(self, eol='\n'):
-	rs = ''
-	r = None
-	while r!= eol and r!='':
-	    r = self.read(1)
-	    rs += r
-	return rs
+	def eol_readline(self, eol='\n'):
+		rs = ''
+		r = None
+		while r!= eol and r!='':
+			r = self.read(1)
+			rs += r
+		return rs
 
-    def eol_readlines(self, eol='\n'):
-	rs = []
-	while True:
-	    rep = self.eol_readline(eol='\r')
-	    if not rep:
-		break
-	    rs.append(rep)
-	return rs
+	def eol_readlines(self, eol='\n', mincount=0):
+		cnt = 0
+		while True:
+			rep = self.eol_readline(eol='\r')
+			if not rep:
+				if cnt > mincount:
+					break
+			else:
+				cnt += 1
+			yield rep
