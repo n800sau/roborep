25821f977e48bceb0413ac82115a0e769ae3cb73
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-01-09 04:09:40 +0000

    set set_pan ...

--- lib/CMUcam4/camTr.cpp
+++ lib/CMUcam4/camTr.cpp
@@ -1,13 +1,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <signal.h>
-
-#include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/calib3d/calib3d.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/opencv.hpp>
-
+#include <malloc.h>
 
 #include <vector>
 #include <iostream>
@@ -42,16 +36,34 @@ int exiting = 0;
 int tilt_ndx = 0;
 int pan_ndx = 0;
 int pan_dir = 1;
-const int servo_steps = 2250 - 750;
+const int servo_min = 750;
+const int servo_max = 2250;
+const int servo_steps = servo_max - servo_min;
 
+
+int h_chan = CMUCAM4_GREEN_CHANNEL;
+int h_bins = CMUCAM4_H4_BINS;
 
 #define E(func) (_E((func), __LINE__))
+
+const char *s_timestamp()
+{
+	static char rs[50];
+	time_t t = time(NULL);
+	struct tm *st = localtime(&t);
+	sprintf(rs, "%.4d.%.2d.%.2d %.2d:%.2d:%.2d", st->tm_year+1900, st->tm_mon+1, st->tm_mday, st->tm_hour, st->tm_min, st->tm_sec);
+	return rs;
+}
 
 long _E(long func, unsigned long line)
 {
   if(func < CMUCAM4_RETURN_SUCCESS)
   {
-    printf("\nCaught error %d on line %d\n", func, line);
+	char s_err[256];
+	sprintf(s_err, "Caught error %d on line %d", func, line);
+	printf("\n%s\n", s_err);
+    redisAsyncCommand(aredis, NULL, NULL, "SET cam_error_code %s,%d", s_timestamp(), func);
+    redisAsyncCommand(aredis, NULL, NULL, "SET cam_error %s,%s", s_timestamp(), s_err);
   }
 
   return func;
@@ -61,9 +73,7 @@ void printH(int tilt, int p, const uint8_t *bins, int length)
 {
 //	redisAsyncCommand(aredis, NULL, NULL, "SET bin:%d:%d %b", tilt, p, bins, length*sizeof(uint8_t));
 	char *buf = (char *)calloc(sizeof(char), length * 3 + 50);
-	time_t t = time(NULL);
-	struct tm *st = localtime(&t);
-	sprintf(buf, "%.4d.%.2d.%.2d %.2d:%.2d:%.2d,%d,%d", st->tm_year+1900, st->tm_mon+1, st->tm_mday, st->tm_hour, st->tm_min, st->tm_sec, tilt, p);
+	sprintf(buf, "%s,%d,%d", s_timestamp(), tilt, p);
 	for(int i = 0; i< length; i++) {
 		sprintf(buf + strlen(buf), ",%d", bins[i]);
 	}
@@ -134,7 +144,7 @@ void clearSD()
 		directorySize = E(cam.listDirectory(de, DE_SIZE, 0));
 		if(directorySize > 0) {
 			for(int i=0; i < std::min(DE_SIZE, directorySize); i++) {
-				cam.removeEntry(de[i].name);
+				E(cam.removeEntry(de[i].name));
 			}
 		}
 	} while(directorySize > DE_SIZE);
@@ -155,7 +165,7 @@ int getImage(IMBUF &ibuf)
 		if(directorySize > 0) {
 			const char *fname = de[directorySize - 1].name;
 			printf("File name:%s\n", fname);
-			ibuf.isize = cam.filePrint(fname, ibuf.ibuf, sizeof(ibuf.ibuf), 0);
+			ibuf.isize = E(cam.filePrint(fname, ibuf.ibuf, sizeof(ibuf.ibuf), 0));
 			if(ibuf.isize < 0) {
 				printf("Error reading file %s\n", fname);
 			}
@@ -164,12 +174,64 @@ int getImage(IMBUF &ibuf)
 	return ibuf.isize;
 }
 
+const char *chan_color(int chan)
+{
+	const char *rs = "none";
+	switch(chan) {
+		case CMUCAM4_GREEN_CHANNEL:
+			rs = "green";
+			break;
+		case CMUCAM4_RED_CHANNEL:
+			rs = "red";
+			break;
+		case CMUCAM4_BLUE_CHANNEL:
+			rs = "blue";
+			break;
+	}
+	return rs;
+}
+
+void storeCurPos()
+{
+    redisAsyncCommand(aredis, NULL, NULL, "SET cur_tilt %d", cam.getServoPosition(CMUCAM4_TILT_SERVO));
+    redisAsyncCommand(aredis, NULL, NULL, "SET cur_pan %d", cam.getServoPosition(CMUCAM4_PAN_SERVO));
+}
+
+void setCurPosCallback(redisAsyncContext *c, void *r, void *privdata) {
+    redisReply *reply = (redisReply *)r;
+    if (reply != NULL) {
+		if(reply->str) {
+			int servo = atoi((char*)privdata);
+			int pos = atoi(reply->str);
+			printf("setting servo %s to %d\n", (char*)privdata, pos);
+			free(privdata);
+			E(cam.setServoPosition(servo, true, pos));
+			switch(servo) {
+				case CMUCAM4_PAN_SERVO:
+					redisAsyncCommand(aredis, NULL, NULL, "DEL set_pan");
+					break;
+				case CMUCAM4_TILT_SERVO:
+					redisAsyncCommand(aredis, NULL, NULL, "DEL set_tilt");
+					break;
+			}
+			storeCurPos();
+		}
+	}
+}
+
+void setCurPos()
+{
+	char buf[50];
+	sprintf(buf, "%d", CMUCAM4_TILT_SERVO);
+    redisAsyncCommand(aredis, setCurPosCallback, strdup(buf), "GET set_tilt");
+	sprintf(buf, "%d", CMUCAM4_PAN_SERVO);
+    redisAsyncCommand(aredis, setCurPosCallback, strdup(buf), "GET set_pan");
+}
+
 void loop()
 {
 	CMUcam4_image_data_t f_data;
 	CMUcam4_tracking_data_t t_data;
-#define HISTOGRAM_CHANNEL CMUCAM4_GREEN_CHANNEL
-#define HISTOGRAM_BINS CMUCAM4_H4_BINS
 	CMUcam4_histogram_data_1_t h_1_data;
 	CMUcam4_histogram_data_2_t h_2_data;
 	CMUcam4_histogram_data_4_t h_4_data;
@@ -177,41 +239,52 @@ void loop()
 	CMUcam4_histogram_data_16_t h_16_data;
 	CMUcam4_histogram_data_32_t h_32_data;
 	CMUcam4_histogram_data_64_t h_64_data;
-	int rs = cam.getHistogram(HISTOGRAM_CHANNEL, HISTOGRAM_BINS);
+	int length;
+	uint8_t *bins;
+	setCurPos();
+	int rs = E(cam.getHistogram(h_chan, h_bins));
 	if ( rs == 0 ) {
-		#if(HISTOGRAM_BINS == CMUCAM4_H1_BINS)
-			cam.getTypeHDataPacket(&h_1_data); // Get a histogram packet.
-			int length = CMUCAM4_HD_1_T_LENGTH;
-			uint8_t *bins = h_1_data.bins;
-		#elif(HISTOGRAM_BINS == CMUCAM4_H2_BINS)
-			cam.getTypeHDataPacket(&h_2_data); // Get a histogram packet.
-			int length = CMUCAM4_HD_2_T_LENGTH;
-			uint8_t *bins = h_2_data.bins;
-		#elif(HISTOGRAM_BINS == CMUCAM4_H4_BINS)
-			cam.getTypeHDataPacket(&h_4_data); // Get a histogram packet.
-			int length = CMUCAM4_HD_4_T_LENGTH;
-			uint8_t *bins = h_4_data.bins;
-		#elif(HISTOGRAM_BINS == CMUCAM4_H8_BINS)
-			cam.getTypeHDataPacket(&h_8_data); // Get a histogram packet.
-			int length = CMUCAM4_HD_8_T_LENGTH;
-			uint8_t *bins = h_8_data.bins;
-		#elif(HISTOGRAM_BINS == CMUCAM4_H16_BINS)
-			cam.getTypeHDataPacket(&h_16_data); // Get a histogram packet.
-			int length = CMUCAM4_HD_16_T_LENGTH;
-			uint8_t *bins = h_16_data.bins;
-		#elif(HISTOGRAM_BINS == CMUCAM4_H32_BINS)
-			cam.getTypeHDataPacket(&h_32_data); // Get a histogram packet.
-			int length = CMUCAM4_HD_32_T_LENGTH;
-			uint8_t *bins = h_32_data.bins;
-		#elif((HISTOGRAM_BINS == CMUCAM4_H64_BINS) && (HISTOGRAM_CHANNEL == CMUCAM4_GREEN_CHANNEL))
-			cam.getTypeHDataPacket(&h_64_data); // Get a histogram packet.
-			int length = CMUCAM4_HD_64_T_LENGTH;
-			uint8_t *bins = h_64_data.bins;
-		#else // 1 to 64 bins...
-			#error "Invalid number of bins"
-			int length = 0;
-			uint8_t *bins = 0;
-		#endif
+		switch(h_bins) {
+			case CMUCAM4_H1_BINS:
+				rs = E(cam.getTypeHDataPacket(&h_1_data)); // Get a histogram packet.
+				length = CMUCAM4_HD_1_T_LENGTH;
+				bins = h_1_data.bins;
+				break;
+			case CMUCAM4_H2_BINS:
+				rs = E(cam.getTypeHDataPacket(&h_2_data)); // Get a histogram packet.
+				length = CMUCAM4_HD_2_T_LENGTH;
+				bins = h_2_data.bins;
+				break;
+			case CMUCAM4_H4_BINS:
+				rs = E(cam.getTypeHDataPacket(&h_4_data)); // Get a histogram packet.
+				length = CMUCAM4_HD_4_T_LENGTH;
+				bins = h_4_data.bins;
+				break;
+			case CMUCAM4_H8_BINS:
+				rs = E(cam.getTypeHDataPacket(&h_8_data)); // Get a histogram packet.
+				length = CMUCAM4_HD_8_T_LENGTH;
+				bins = h_8_data.bins;
+				break;
+			case CMUCAM4_H16_BINS:
+				rs = E(cam.getTypeHDataPacket(&h_16_data)); // Get a histogram packet.
+				length = CMUCAM4_HD_16_T_LENGTH;
+				bins = h_16_data.bins;
+				break;
+			case CMUCAM4_H32_BINS:
+				rs = E(cam.getTypeHDataPacket(&h_32_data)); // Get a histogram packet.
+				length = CMUCAM4_HD_32_T_LENGTH;
+				bins = h_32_data.bins;
+				break;
+			case CMUCAM4_H64_BINS:
+				rs = E(cam.getTypeHDataPacket(&h_64_data)); // Get a histogram packet.
+				length = CMUCAM4_HD_64_T_LENGTH;
+				bins = h_64_data.bins;
+				break;
+			default:
+				length = 0;
+				bins = 0;
+				break;
+		}
 		if(length > 0) {
 			char *buf = (char *)calloc(sizeof(char), length * 3 + 50);
 			if(buf) {
@@ -221,7 +294,7 @@ void loop()
 				for(int i = 0; i< length; i++) {
 					sprintf(buf + strlen(buf), ",%d", bins[i]);
 				}
-				redisAsyncCommand(aredis, NULL, NULL, "SET cur_hist %s", buf, strlen(buf));
+				redisAsyncCommand(aredis, NULL, NULL, "SET %s_hist %s", chan_color(h_chan), buf, strlen(buf));
 				free(buf);
 			}
 		}
@@ -234,154 +307,11 @@ void loop1()
 	CMUcam4_image_data_t f_data;
 	CMUcam4_tracking_data_t t_data;
 
-#define HISTOGRAM_CHANNEL CMUCAM4_GREEN_CHANNEL
-#define HISTOGRAM_BINS CMUCAM4_H4_BINS
-  CMUcam4_histogram_data_1_t h_1_data;
-  CMUcam4_histogram_data_2_t h_2_data;
-  CMUcam4_histogram_data_4_t h_4_data;
-  CMUcam4_histogram_data_8_t h_8_data;
-  CMUcam4_histogram_data_16_t h_16_data;
-  CMUcam4_histogram_data_32_t h_32_data;
-  CMUcam4_histogram_data_64_t h_64_data;
-
-//	printf("\nt=%d, p=%d:\n", tilt_ndx, pan_ndx);
-	cam.setServoPosition(CMUCAM4_TILT_SERVO, true, tilt_ndx+750);
-	cam.setServoPosition(CMUCAM4_PAN_SERVO, true, pan_ndx+750);
-
-	clearSD();
-
 			usleep(1000000L);
-			printf("histogram=%d\n", cam.getHistogram(HISTOGRAM_CHANNEL, HISTOGRAM_BINS));
 			uint16_t buffer[80*80]; // 10 by 10 pixel buffer
 			uint8_t buffer3[80*80][3]; // 10 by 10 pixel buffer
 			memset(buffer, 0, sizeof(buffer));
-/*			printf("sendFrame=%d\n", cam.sendFrame(CMUCAM4_HR_80, CMUCAM4_HR_80, buffer, 80, 0, 80, 0));
 
-			char fname[256];
-			sprintf(fname, "%d.%d.jpg", tilt_ndx, pan_ndx);
-	
-			for(int k=0; k<80*80; k++)
-			{
-				buffer3[k][0] = buffer[k] & 0x1f;
-				buffer3[k][1] = (buffer[k] << 5) & 0x3f;
-				buffer3[k][2] = (buffer[k] << 11) & 0x1f;
-			}
-
-			int channels = 3;
-			IplImage* cv_image = cvCreateImageHeader(cvSize(80,80), IPL_DEPTH_8U, channels);
-			if (!cv_image)
-			{
-				printf("Error creating image\n");    // print error, failed to allocate image!
-			}
-
-			printf("Width step:%d\n", cv_image->widthStep);
-			cvSetData(cv_image, buffer3, cv_image->widthStep);
-
-			int pan_ndx[3] = {CV_IMWRITE_JPEG_QUALITY, 10, 0};
-			cvSaveImage(fname, cv_image, pan_ndx);
-			printf("end\n");
-
-			// release resources
-			cvReleaseImageHeader(&cv_image);
-*/
-
-    #if(HISTOGRAM_BINS == CMUCAM4_H1_BINS)
-      cam.getTypeHDataPacket(&h_1_data); // Get a histogram packet.
-		printH(tilt_ndx, pan_ndx, h_1_data.bins, CMUCAM4_HD_1_T_LENGTH);
-
-    #elif(HISTOGRAM_BINS == CMUCAM4_H2_BINS)
-      cam.getTypeHDataPacket(&h_2_data); // Get a histogram packet.
-		printH(tilt_ndx, pan_ndx, h_2_data.bins, CMUCAM4_HD_2_T_LENGTH);
-
-    #elif(HISTOGRAM_BINS == CMUCAM4_H4_BINS)
-      cam.getTypeHDataPacket(&h_4_data); // Get a histogram packet.
-		printH(tilt_ndx, pan_ndx, h_4_data.bins, CMUCAM4_HD_4_T_LENGTH);
-
-    #elif(HISTOGRAM_BINS == CMUCAM4_H8_BINS)
-      cam.getTypeHDataPacket(&h_8_data); // Get a histogram packet.
-		printH(tilt_ndx, pan_ndx, h_8_data.bins, CMUCAM4_HD_8_T_LENGTH);
-
-    #elif(HISTOGRAM_BINS == CMUCAM4_H16_BINS)
-      cam.getTypeHDataPacket(&h_16_data); // Get a histogram packet.
-		printH(tilt_ndx, pan_ndx, h_16_data.bins, CMUCAM4_HD_16_T_LENGTH);
-
-    #elif(HISTOGRAM_BINS == CMUCAM4_H32_BINS)
-      cam.getTypeHDataPacket(&h_32_data); // Get a histogram packet.
-		printH(tilt_ndx, pan_ndx, h_32_data.bins, CMUCAM4_HD_64_T_LENGTH);
-
-    #elif((HISTOGRAM_BINS == CMUCAM4_H64_BINS) && \
-    (HISTOGRAM_CHANNEL == CMUCAM4_GREEN_CHANNEL))
-      cam.getTypeHDataPacket(&h_64_data); // Get a histogram packet.
-		printH(tilt_ndx, pan_ndx, h_64_data.bins, CMUCAM4_HD_64_T_LENGTH);
-
-    #else // 1 to 64 bins...
-      #error "Invalid number of bins"
-
-    #endif
-
-#define DE_SIZE 4
-	int directorySize;
-	CMUcam4_directory_entry_t de[DE_SIZE]; // Directory entry array.
-	if(E(cam.dumpFrame(CMUCAM4_HR_160, CMUCAM4_VR_120)) == 0) {
-		directorySize = E(cam.listDirectory(de, DE_SIZE, 0));
-		if(directorySize > 0) {
-			const char *fname = de[directorySize - 1].name;
-			uint8_t filebuf[160*120*4];
-			char dfname[100];
-			printf("File name:%s\n", fname);
-			long fsize = cam.filePrint(fname, filebuf, sizeof(filebuf), 0);
-			if(fsize >= 0) {
-				sprintf(dfname, "%d_%d.bmp", tilt_ndx, pan_ndx);
-				FILE *df = fopen(dfname, "wb");
-				if (df) {
-					fwrite(filebuf, 1, fsize, df);
-					fclose(df);
-				} else {
-					printf("Error writing file %s\n", dfname);
-				}
-			} else {
-				printf("Error reading file %s\n", fname);
-			}
-		}
-	}
-
-
-
-/*			cv::Mat M(80,80, CV_16UC3, buffer);
-
-			printf("col:%d,rows:%d\n", M.cols, M.rows);
-			createAlphaMat(M);
-			printf("here1\n");
-
-
-			
-			std::vector<int> compression_params;
-			compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
-			compression_params.push_back(9);
-
-/*			printf("here2\n");
-			for(int y = 0; y < 40 ; y++) {
-				for(int x = 0; x < 40 ; x++) {
-					printf("%4.4X", (int)M.data[x+y*80]);
-				}
-			}*/
-/*			imwrite(fname, M, compression_params);
-			printf("here3\n");
-*/
-
-
-//			cv::FileStorage fs(fname, cv::FileStorage::WRITE);
-//			fs << M;
-//			fs.release();
-//			for(int y = 0; y < 40 ; y++) {
-//				for(int x = 0; x < 40 ; x++) {
-//					if(buffer[y * 80 + x]) {
-//						printf("%4.4X", (int)buffer[y * 80 + x]);
-//					}
-//				}
-				//printf("\n");
-//			}
-			//printf("\n");
 //			cam.pollMode(true);
 //			cam.trackColor(RED_MIN, RED_MAX, GREEN_MIN, GREEN_MAX, BLUE_MIN, BLUE_MAX);
 //			cam.getTypeTDataPacket(&t_data); // Get a tracking packet.
@@ -396,87 +326,55 @@ void loop1()
 //			}
 //			printf("\n");
 //			usleep(1000000L);
-	pan_ndx += 40 * pan_dir;
-	if(pan_ndx >= servo_steps || pan_ndx < 0) {
-		tilt_ndx += 40;
-		pan_dir *= -1;
-		pan_ndx += 40 * pan_dir;
-		if(tilt_ndx >= servo_steps / 2) {
-			tilt_ndx = 0;
-			exit(0);
-		}
-	}
-}
-
-
-
-void createAlphaMat(cv::Mat &mat)
-{
-    for (int i = 0; i < mat.rows; ++i) {
-        for (int j = 0; j < mat.cols; ++j) {
-            cv::Vec4b& rgba = mat.at<cv::Vec4b>(i, j);
-            rgba[0] = UCHAR_MAX;
-            rgba[1] = cv::saturate_cast<uchar>((float (mat.cols - j)) / ((float)mat.cols) * UCHAR_MAX);
-            rgba[2] = cv::saturate_cast<uchar>((float (mat.rows - i)) / ((float)mat.rows) * UCHAR_MAX);
-            rgba[3] = cv::saturate_cast<uchar>(0.5 * (rgba[1] + rgba[2]));
-        }
-    }
-}
-
-int cur_pan = 750 + servo_steps / 2;
-int cur_tilt = 750 + servo_steps / 2;
-
-void storeCurPos()
-{
-    redisAsyncCommand(aredis, NULL, NULL, "SET cur_tilt %d", cur_tilt);
-    redisAsyncCommand(aredis, NULL, NULL, "SET cur_pan %d", cur_pan);
 }
 
 void move_up(json_t *js)
 {
 	int value = json_integer_value(json_object_get(js, "value"));
-	cur_tilt += value;
-	if(cur_tilt > servo_steps) {
-		cur_tilt = servo_steps;
-	}
-	printf("move up to %d\n", cur_tilt);
-	cam.setServoPosition(CMUCAM4_TILT_SERVO, true, cur_tilt+750);
+	printf("move up by %d\n", value);
+	E(cam.setServoPosition(CMUCAM4_TILT_SERVO, true, cam.getServoPosition(CMUCAM4_TILT_SERVO) + value));
 	storeCurPos();
 }
 
 void move_down(json_t *js)
 {
 	int value = json_integer_value(json_object_get(js, "value"));
-	cur_tilt -= value;
-	if(cur_tilt < 0) {
-		cur_tilt = 0;
-	}
-	printf("move down to %d\n", cur_tilt);
-	cam.setServoPosition(CMUCAM4_TILT_SERVO, true, cur_tilt+750);
+	printf("move down by %d\n", value);
+	E(cam.setServoPosition(CMUCAM4_TILT_SERVO, true, cam.getServoPosition(CMUCAM4_TILT_SERVO) - value));
 	storeCurPos();
 }
 
 void move_right(json_t *js)
 {
 	int value = json_integer_value(json_object_get(js, "value"));
-	cur_pan -= value;
-	if(cur_pan < 0) {
-		cur_pan = 0;
-	}
-	printf("move right to %d\n", cur_pan);
-	cam.setServoPosition(CMUCAM4_PAN_SERVO, true, cur_pan+750);
+	printf("move right by %d\n", value);
+	E(cam.setServoPosition(CMUCAM4_PAN_SERVO, true, cam.getServoPosition(CMUCAM4_PAN_SERVO) - value));
 	storeCurPos();
 }
 
 void move_left(json_t *js)
 {
 	int value = json_integer_value(json_object_get(js, "value"));
-	cur_pan += value;
-	if(cur_pan > servo_steps) {
-		cur_pan = servo_steps;
-	}
-	printf("move left to %d\n", cur_pan);
-	cam.setServoPosition(CMUCAM4_PAN_SERVO, true, cur_pan+750);
+	printf("move left by %d\n", value);
+	E(cam.setServoPosition(CMUCAM4_PAN_SERVO, true, cam.getServoPosition(CMUCAM4_PAN_SERVO) + value));
+	storeCurPos();
+}
+
+void set_servo_hpos(json_t *js)
+{
+	int angle = json_integer_value(json_object_get(js, "angle"));
+	int pos = (servo_max - servo_min)/ 180. * (angle + 90) + servo_min;
+	printf("pan to %d\n", pos);
+	E(cam.setServoPosition(CMUCAM4_PAN_SERVO, true, pos));
+	storeCurPos();
+}
+
+void set_servo_vpos(json_t *js)
+{
+	int angle = json_integer_value(json_object_get(js, "angle"));
+	int pos = (servo_max - servo_min)/ 180. * (angle + 90) + servo_min;
+	printf("tilt to %d\n", pos);
+	E(cam.setServoPosition(CMUCAM4_TILT_SERVO, true, pos));
 	storeCurPos();
 }
 
@@ -493,6 +391,53 @@ void make_image(json_t *js)
 	}
 }
 
+void set_histogram_mode(json_t *js)
+{
+	int nbins = json_integer_value(json_object_get(js, "bins"));
+	switch(nbins) {
+		case 1:
+			h_bins = CMUCAM4_H1_BINS;
+			break;
+		case 2:
+			h_bins = CMUCAM4_H2_BINS;
+			break;
+		case 4:
+			h_bins = CMUCAM4_H4_BINS;
+			break;
+		case 8:
+			h_bins = CMUCAM4_H8_BINS;
+			break;
+		case 16:
+			h_bins = CMUCAM4_H16_BINS;
+			break;
+		case 32:
+			h_bins = CMUCAM4_H32_BINS;
+			break;
+		case 64:
+			h_bins = CMUCAM4_H64_BINS;
+			break;
+	}
+	const char *chan = json_string_value(json_object_get(js, "chan"));
+	if(strcmp(chan, "green") == 0) {
+		h_chan = CMUCAM4_GREEN_CHANNEL;
+	} else if (strcmp(chan, "blue") == 0) {
+		h_chan = CMUCAM4_BLUE_CHANNEL;
+	} else if (strcmp(chan, "red") == 0) {
+		h_chan = CMUCAM4_RED_CHANNEL;
+	}
+}
+
+void set_window(json_t *js) {
+	CMUcam4_tracking_window_t tw;
+	int topLeftX = json_integer_value(json_object_get(js, "x1"));
+	int topLeftY = json_integer_value(json_object_get(js, "y1"));
+	int bottomRightX = json_integer_value(json_object_get(js, "x2"));
+	int bottomRightY = json_integer_value(json_object_get(js, "y2"));
+	E(cam.setTrackingWindow(topLeftX, topLeftY, bottomRightX, bottomRightY));
+	E(cam.getTrackingWindow(&tw));
+	redisAsyncCommand(aredis, NULL, NULL, "SET cur_window %s,%d,%d,%d,%d", s_timestamp(), tw.topLeftX, tw.topLeftY, tw.bottomRightX, tw.bottomRightY);
+}
+
 typedef void AFUNC(json_t *js);
 
 struct CMD_FUNC {
@@ -503,7 +448,11 @@ struct CMD_FUNC {
 	{ "move_down", move_down },
 	{ "move_left", move_left },
 	{ "move_right", move_right },
-	{ "make_image", make_image }
+	{ "set_servo_hpos", set_servo_hpos },
+	{ "set_servo_vpos", set_servo_vpos },
+	{ "make_image", make_image },
+	{ "set_histogram_mode", set_histogram_mode },
+	{ "set_window", set_window }
 };
 
 void cmdCallback(redisAsyncContext *c, void *r, void *privdata) {
@@ -563,8 +512,8 @@ void cb_func(evutil_socket_t fd, short what, void *arg)
 {
     printf("cb_func called %d times so far.\n", ++n_calls);
 	loop();
-    if (n_calls > 100)
-       event_del(timer_ev);
+//    if (n_calls > 100)
+//       event_del(timer_ev);
 }
 
 
@@ -586,10 +535,10 @@ int main (int argc, char **argv) {
     }
 	setup();
 	cam_begin();
+	storeCurPos();
     redisLibeventAttach(aredis,base);
     redisAsyncSetConnectCallback(aredis,connectCallback);
     redisAsyncSetDisconnectCallback(aredis,disconnectCallback);
-    redisAsyncCommand(aredis, NULL, NULL, "SET temp.a %b", argv[argc-1], strlen(argv[argc-1]));
     redisAsyncCommand(aredis, cmdCallback, NULL, "LPOP %s", r_cam_cmd);
 	timer_ev = event_new(base, -1, EV_PERSIST, cb_func, NULL);
 	struct timeval one_sec = { 5, 0 };
