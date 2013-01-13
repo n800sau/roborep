0e4dd47f53351f955ffd1e117959a8ee9f907266
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-01-13 22:00:30 +0000

    camtr status script

--- trunk/lib/CMUcam4/stop.sh
+++ trunk/lib/CMUcam4/stop.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-start-stop-daemon --stop --user n800s --pidfile /home/n800s/run/camd.pid --signal 15 --retry 10
+PIDFILE=/home/n800s/run/camd.pid
+
+start-stop-daemon --stop --user n800s --pidfile ${PIDFILE} --signal 15 --retry 10
