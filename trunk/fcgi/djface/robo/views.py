c5ba21ccdd59affabf2feec7e1c5e2152b361917
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-01-15 05:18:36 +0000

    more params to show

--- trunk/fcgi/djface/robo/views.py
+++ trunk/fcgi/djface/robo/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
 from django.http import HttpResponse
-import redis, json, base64
+import redis, json, base64, datetime, time
 import Image
 from StringIO import StringIO
 
@@ -12,7 +12,7 @@ def home(request):
 	return render_to_response('home.html', {}, context_instance=RequestContext(request))
 
 def histogram(request):
-	return render_to_response('histogram.html', {})
+	return render_to_response('histogram.html', {}, context_instance=RequestContext(request))
 
 def move_left(request):
 	redis.Redis().lpush(QCMD, json.dumps({'cmd': 'move_left', 'value': STEP}))
@@ -42,3 +42,40 @@ def get_image(request):
 	df = StringIO()
 	img.save(df, "PNG")
 	return HttpResponse( df.getvalue(), mimetype='image/png')
+
+def set_histogram_mode(request):
+	redis.Redis().lpush(QCMD, json.dumps({'cmd': 'set_histogram_mode', 'bins': request.GET['bins'], 'chan': request.GET['chan']}))
+	return HttpResponse('OK')
+
+def get_histogram(request):
+	cur_hist = redis.Redis().get("cur_histogram")
+	return HttpResponse(cur_hist, mimetype='text/json')
+
+def get_servos(request):
+	return HttpResponse(json.dumps({'cur_pan': redis.Redis().get("cur_pan"), 'cur_tilt': redis.Redis().get("cur_tilt")}), mimetype='text/json')
+
+def monitor(request):
+	return render_to_response('monitor.html', {}, context_instance=RequestContext(request))
+
+def cam_values(request):
+	cur_time = time.time()
+	values = {'timestamp': time.strftime('%d.%m.%Y %H:%M:%S', time.localtime(cur_time))}
+	r = redis.Redis()
+	values['cur_pan'] = r.get('cur_pan')
+	values['cur_tilt'] = r.get('cur_tilt')
+	values['cur_heading_degrees'] = r.get('heading.degrees')
+	values['acc_x'] = r.get('adxl345.x')
+	values['acc_y'] = r.get('adxl345.y')
+	values['acc_z'] = r.get('adxl345.z')
+	try:
+		act_time = float(r.get('timestamp'))
+		atimestamp = time.strftime('%d.%m.%Y %H:%M:%S', time.localtime(act_time))
+		time_diff = int(cur_time - act_time)
+		time_diff = '%2.2dh %2.2dm %2.2ds' % (time_diff // 3600, time_diff // 60 % 60, time_diff % 60)
+	except:
+		atimestamp = 'unknown'
+		time_diff =  'unknown'
+	values['time_diff'] = time_diff
+	values['last_activity_timestamp'] = atimestamp
+	return render_to_response('cam_values.html', values, context_instance=RequestContext(request))
+
