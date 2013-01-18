d513dd2915dc3298b398f4c3974753f6e87452b8
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-01-18 04:20:28 +0000

    path adjustments

--- trunk/lib/camServant/start.sh
+++ trunk/lib/camServant/start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 PIDFILE=/home/n800s/run/camd.pid
-DAEMON=/home/n800s/work/sourceforge/robotarr-code/lib/CMUcam4/camTr
+DAEMON=/home/n800s/work/sourceforge/robotarr-code/lib/camServant/camTr
 start-stop-daemon -v --start --user n800s --make-pidfile --pidfile ${PIDFILE} --background --no-close --startas ${DAEMON} --chuid n800s -- ${DAEMON_ARGS}
