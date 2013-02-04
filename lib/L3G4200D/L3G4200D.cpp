acf428d4437df69716cd51bd6184751e32f494d3
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-02-04 05:12:58 +0000

    L3G4200D added

---
+++ lib/L3G4200D/L3G4200D.cpp
@@ -0,0 +1,103 @@
+#include "L3G4200D.h"
+#include <math.h>
+#include <syslog.h>
+
+L3G4200D::L3G4200D(uint8_t address):ReServant("cmd.l3g4200d", "l3g4200d"),g()
+{
+	m_Address = address;
+}
+
+
+// Turns on the L3G4200D's gyro and places it in normal mode.
+void L3G4200D::enableDefault(int scale)
+{
+	i2cwire.selectDevice(m_Address, "L3G4200D");
+  // 0x0F = 0b00001111
+  // Normal power mode, all axes enabled
+	i2cwire.writeToDevice(L3G4200D_CTRL_REG1, 0x0F);
+
+  //From  Jim Lindblom of Sparkfun's code
+
+  // Enable x, y, z and turn off power down:
+  i2cwire.writeToDevice(L3G4200D_CTRL_REG1, 0b00001111);
+
+  // If you'd like to adjust/use the HPF, you can edit the line below to configure CTRL_REG2:
+  i2cwire.writeToDevice(L3G4200D_CTRL_REG2, 0b00000000);
+
+  // Configure CTRL_REG3 to generate data ready interrupt on INT2
+  // No interrupts used on INT1, if you'd like to configure INT1
+  // or INT2 otherwise, consult the datasheet:
+  i2cwire.writeToDevice(L3G4200D_CTRL_REG3, 0b00001000);
+
+  // CTRL_REG4 controls the full-scale range, among other things:
+
+  if(scale == 250){
+    i2cwire.writeToDevice(L3G4200D_CTRL_REG4, 0b00000000);
+  }else if(scale == 500){
+    i2cwire.writeToDevice(L3G4200D_CTRL_REG4, 0b00010000);
+  }else{
+    i2cwire.writeToDevice(L3G4200D_CTRL_REG4, 0b00110000);
+  }
+
+  // CTRL_REG5 controls high-pass filtering of outputs, use it
+  // if you'd like:
+  i2cwire.writeToDevice(L3G4200D_CTRL_REG5, 0b00000000);
+
+}
+
+// Reads the 3 gyro channels and stores them in vector g
+bool L3G4200D::read()
+{
+	int16_t buf[3];
+	bool rs = false;
+
+	// assert the MSB of the address to get the gyro 
+	// to do slave-transmit subaddress updating.
+	i2cwire.selectDevice(m_Address, "L3G4200D");
+	rs = i2cwire.requestFromDevice(L3G4200D_OUT_X_L | (1 << 7), 6, (uint8_t*)buf) == 6;
+
+	g.x = buf[0];
+	g.y = buf[1];
+	g.z = buf[2];
+
+	return rs;
+}
+
+void L3G4200D::vector_cross(const vector *a,const vector *b, vector *out)
+{
+  out->x = a->y*b->z - a->z*b->y;
+  out->y = a->z*b->x - a->x*b->z;
+  out->z = a->x*b->y - a->y*b->x;
+}
+
+float L3G4200D::vector_dot(const vector *a,const vector *b)
+{
+  return a->x*b->x+a->y*b->y+a->z*b->z;
+}
+
+void L3G4200D::vector_normalize(vector *a)
+{
+  float mag = sqrt(vector_dot(a,a));
+  a->x /= mag;
+  a->y /= mag;
+  a->z /= mag;
+}
+
+void L3G4200D::create_servant()
+{
+	ReServant::create_servant();
+	enableDefault();
+}
+
+void L3G4200D::loop()
+{
+	if(read()) {
+		redisAsyncCommand(aredis, NULL, NULL, "SET l3g4200d.x %g", g.x);
+		redisAsyncCommand(aredis, NULL, NULL, "SET l3g4200d.y %g", g.y);
+		redisAsyncCommand(aredis, NULL, NULL, "SET l3g4200d.z %g", g.z);
+	}
+
+	syslog(LOG_NOTICE, "gyro:%d %4d %4d\n", g.x, g.y, g.z);
+
+	ReServant::loop();
+}
