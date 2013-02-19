e7ba0b54995cd3da9f0582be4f5960ed20d08292
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-02-19 04:13:06 +0000

    more calc

--- trunk/calc/cc.py
+++ trunk/calc/cc.py
@@ -4,9 +4,26 @@ t = 350
 
 p = 6
 
-tot = 350
-for i in range(30 * 12):
-	add = tot * (p / 12.) / 100.
-	tot = tot + add - 2.1
+load = 350000
+tot = load
+oldtot = load
+pd = 0
+mn = 2200
+mrval = 270./7*30
+y = 0
+while tot > 0:
+	mpd = 0
+	mpdmr = 0
+	for m in range(12):
+		add = tot * (p / 12.) / 100.
+		tot = tot + add - mn
+		mpd += mn
+		mpdmr += mn - mrval
+	pd += mpd
+	tot += 200
+	print 'y:%d, tot:%d, diff:%d, mpd:%d, mpdmr:%d, addp:%d' % (y,tot,oldtot-tot,mpd, mpdmr, (mn-mrval)/30.*7)
+	oldtot = tot
+	y += 1
 
-print 'tot=', tot
+
+print 'years=', y
