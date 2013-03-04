3ad62f00c4a2aa861c5becbe9f077f4014ba67b4
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-03-04 21:54:11 +0000

    requestFromDevice for uint16_t

--- trunk/lib/I2CWire/I2CWire.cpp
+++ trunk/lib/I2CWire/I2CWire.cpp
@@ -30,20 +30,37 @@ void I2CWire::selectDevice(uint8_t addr, const char * name)
    }
 }
 
-int I2CWire::requestFromDevice(uint8_t addr, int num, uint8_t buf[])
+int I2CWire::requestFromDevice(uint8_t addr, int length, uint8_t buf[])
 {
-      if (write(fd, &addr, 1) != 1)
-      {
-         // Send the register to read from
-         fprintf(stderr, "Error writing to i2c slave\n");
-         //exit(1);
-      }
-
-	int n = read(fd, buf, num);
-	if( n != num) {
-		fprintf(stderr, "Error reading from i2c slave (received %d != requested %d)\n", n, num);
+	if (write(fd, &addr, 1) != 1)
+	{
+		// Send the register to read from
+		fprintf(stderr, "Error writing to i2c slave\n");
+		//exit(1);
 	}
-	return n;
+
+	int rs = read(fd, buf, length);
+	if( rs != length) {
+		fprintf(stderr, "Error reading from i2c slave (received %d != requested %d)\n", rs, length);
+		rs = 0;
+	}
+	return rs;
+}
+
+int I2CWire::requestFromDeviceHL(uint8_t addr, int length, uint16_t buf[])
+{
+	int rs = requestFromDevice(addr, length * 2, (uint8_t *)buf);
+	if(rs > 0) {
+		for(int i=0; i<length; i++) {
+			buf[i] = ((buf[i]<<8) & 0xFF00) | ((buf[i]>>8) & 0xFF);
+		}
+	}
+	return rs;
+}
+
+int I2CWire::requestFromDeviceLH(uint8_t addr, int length, uint16_t buf[])
+{
+	return requestFromDevice(addr, length * 2, (uint8_t *)buf);
 }
 
 void I2CWire::writeToDevice(uint8_t reg, uint8_t val)
