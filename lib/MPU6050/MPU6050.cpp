17830908f8c10700aa72f44c7ec314ec533d2fca
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-03-07 22:55:20 +0000

    after make depend

--- lib/MPU6050/MPU6050.cpp
+++ lib/MPU6050/MPU6050.cpp
@@ -165,11 +165,3 @@ void MPU6050::loop()
 	json2redislist();
 	ReServant::loop();
 }
-
-int fsize(FILE *fp){
-    int prev=ftell(fp);
-    fseek(fp, 0L, SEEK_END);
-    int sz=ftell(fp);
-    fseek(fp,prev,SEEK_SET); //go back to where we were
-    return sz;
-}
