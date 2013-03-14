c976c23d424236dc5a33865daab68ce9cd31a5ff
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-03-14 09:49:15 +0000

    9 gauges

--- trunk/android/RoboSensorView/src/au/n800s/robo/sensorview/GaugeView.java
+++ trunk/android/RoboSensorView/src/au/n800s/robo/sensorview/GaugeView.java
@@ -2,6 +2,7 @@ package au.n800s.robo.sensorview;
 
 import java.util.ArrayList;
 import java.util.List;
+import android.util.AttributeSet;
 
 import java.lang.Math;
 
@@ -11,6 +12,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Paint.Style;
 import android.graphics.Paint.Align;
+import android.graphics.drawable.Drawable;
 import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
@@ -18,25 +20,52 @@ import android.view.View.OnTouchListener;
 
 public class GaugeView extends View {
 
-	Paint paint = new Paint();
-	Float angle;  // View to draw a compass
+	private Paint paint = new Paint();
+	private Float angle;  // View to draw a compass
+	private int	 bgcolor;
+
+	public GaugeView(Context context, AttributeSet attrs, int defStyle)
+	{
+		super(context, attrs, defStyle);
+		init();
+	}
+
+	public GaugeView(Context context, AttributeSet attrs)
+	{
+		super(context, attrs);
+		init();
+	}
 
 	public GaugeView(Context context)
 	{
 		super(context);
+		init();
+	}
+
+	private void init()
+	{
 		paint.setColor(0xff00ff00);
-		paint.setStyle(Style.STROKE);
 		paint.setStrokeWidth(2);
 		paint.setAntiAlias(true);
-	};
+	}
+
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        setMeasuredDimension(100, 130);
+    }
 
 	protected void onDraw(Canvas canvas)
 	{
 		int width = getWidth();
 		int height = getHeight();
+		paint.setColor(this.bgcolor);
+		Log.d("RoboSensorView", String.format("Color:%d", this.bgcolor));
+		paint.setStyle(Style.FILL);
+        canvas.drawRect(0, 0, width, height, paint);
+		paint.setStyle(Style.STROKE);
+		paint.setColor(0xff00ff00);
 		int centerx = width/2;
 		int centery = height/2;
-		int radius = Math.min(height, width)/2;
+		int radius = Math.min(height, width)/2 - 20;
 		canvas.drawCircle(centerx, centery, radius, paint);
 		paint.setTextAlign(Align.CENTER);
 		canvas.drawText("N", centerx, centery - radius - 10, paint);
@@ -50,6 +79,13 @@ public class GaugeView extends View {
 		paint.setColor(0xff0000ff);
 		canvas.drawLine(centerx, centery, centerx, centery - radius, paint);
 		paint.setColor(0xff00ff00);
+	}
+
+	public void setBackgroundColor(int clr)
+	{
+		super.setBackgroundColor(clr);
+		this.bgcolor = clr;
+		invalidate();
 	}
 
 	public void setAngle(Float angle)
