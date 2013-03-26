12bffd173fc56d8acf34073ae028e03ed79ed0a7
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-03-26 00:55:40 +0000

    update of test scripts again

--- trunk/lib/userver/example_call.sh
+++ trunk/lib/userver/example_call.sh
@@ -1,2 +1,3 @@
 #echo '{"cmd": "send_full_data", "interval": 100, "count": 20}' | nc -u -p7980 115.70.59.149 7980
-echo '{"cmd": "send_full_data", "interval": 100, "count": 20}' | test_udp/updcli 192.168.1.24 7880
+#echo '{"cmd": "send_full_data", "interval": 100, "count": 20}' | test_udp/updcli 192.168.1.24 7880
+echo '{"cmd": "send_full_data", "interval": 100, "count": 20}' | test_udp/updcli 115.70.59.149 7980
