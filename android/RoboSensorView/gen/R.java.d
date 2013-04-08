b4bd6649bb77e3c19e71200123e547cd192526f9
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-04-08 20:52:38 +0000

    android viewer update

--- android/RoboSensorView/gen/R.java.d
+++ android/RoboSensorView/gen/R.java.d
@@ -4,7 +4,9 @@
 /mnt/sda5/n800s/work/sourceforge/robotarr-code/android/RoboSensorView/res/drawable-ldpi/ic_launcher.png \
 /mnt/sda5/n800s/work/sourceforge/robotarr-code/android/RoboSensorView/res/layout/main.xml \
 /mnt/sda5/n800s/work/sourceforge/robotarr-code/android/RoboSensorView/res/drawable-mdpi/ic_launcher.png \
+/mnt/sda5/n800s/work/sourceforge/robotarr-code/android/RoboSensorView/res/menu/options.xml \
 /mnt/sda5/n800s/work/sourceforge/robotarr-code/android/RoboSensorView/res/drawable-hdpi/ic_launcher.png \
+/mnt/sda5/n800s/work/sourceforge/robotarr-code/android/RoboSensorView/res/xml/settings.xml \
 /mnt/sda5/n800s/work/sourceforge/robotarr-code/android/RoboSensorView/bin/res/drawable-ldpi/ic_launcher.png \
 /mnt/sda5/n800s/work/sourceforge/robotarr-code/android/RoboSensorView/bin/res/drawable-mdpi/ic_launcher.png \
 /mnt/sda5/n800s/work/sourceforge/robotarr-code/android/RoboSensorView/bin/res/drawable-hdpi/ic_launcher.png \
---
+++ android/RoboSensorView/src/au/n800s/robo/sensorview/SettingsActivity.java
@@ -0,0 +1,33 @@
+package au.n800s.robo.sensorview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class SettingsActivity extends PreferenceActivity {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.settings);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(Menu.NONE, 0, 0, "Show current settings");
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case 0:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+        }
+        return false;
+    }
+    
+}
