e6116227245476b8c0a19f47db26545d88dc6631
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-07-24 06:49:56 +0000

    last 10 lines to plot

--- trunk/lib/MPU6050/plot.sh
+++ trunk/lib/MPU6050/plot.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env gnuplot
 
+wc=system("wc -l data.txt|cut -f1 -d ' '")+0
+fl=wc-10
+if (fl < 1) fl=1
 set terminal dumb
+set autoscale
 set datafile separator "\t"
 set xdata time
+set timefmt "%s" 
+#set xrange[-100:*]
 set timefmt "%d/%m/%Y %H:%M:%S"
 set format x "%H:%M:%S"
-#set xrange[2000:3000]
 #set yrange[0:10]
-plot "data.txt" every ::1 using 1:2  with lines title "Example"
+plot "data.txt" every ::fl::wc using 1:2  with lines title "Example"
+#replot
+#pause -5
+#reread
 
