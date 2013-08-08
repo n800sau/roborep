df05d0cdb3397b5286c3a6adad2be66138f6dd5b
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-08-08 23:28:23 +0000

    acc_reader.py and readed values

---
+++ trunk/picaxe/ADXL345/acc_reader.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import serial
+
+ser = serial.Serial(
+	port='/dev/ttyUSB0',
+    baudrate=4800
+)
+
+ser.open()
+for line in ser:
+#	print line
+	l = line.split(':')
+	if len(l) == 2:
+		r,v = [v.strip() for v in l]
+		t = {'0': 'X', '2': 'Y', '4': 'Z'}.get(r, None)
+		if t:
+			v = int(v)
+			if v > 0x8000:
+				v -= 0x10000
+			print t, '=', v
+		if t == 'Z':
+			print
+
+
