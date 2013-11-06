2c12b029b3fac681c38ed3f4cb69ab54cfd8fefd
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-11-06 23:37:14 +0000

    PC command shell

--- trunk/ino/cmd_base/src/main.ino
+++ trunk/ino/cmd_base/src/main.ino
@@ -31,55 +31,92 @@ void setup(void)
 
 }
 
-bool has_reply = true;
+bool set_servo(uint8_t low, uint8_t pan, uint8_t tilt)
+{
+	payload_t payload = { PL_SETSERVO, millis(), packets_sent++ };
+	payload.d.servo.low = low;
+	payload.d.servo.pan = pan;
+	payload.d.servo.tilt = tilt;
+	Serial.print("Sending...");
+//	Serial.print(payload.counter);
+//	Serial.print(", ms=");
+//	Serial.print(payload.ms);
+//	Serial.print(", ");
+	Serial.print(payload.d.servo.low);
+	Serial.print("\t");
+	Serial.print(payload.d.servo.pan);
+	Serial.print("\t");
+	Serial.println(payload.d.servo.tilt);
+	RF24NetworkHeader header(STICK_NODE);
+	return network.write(header,&payload,sizeof(payload));
+}
+
+#define STEP 1
+int tiltdir = STEP;
+stickservo_t servo = {90, 90, 90};
+
+void move_servo()
+{
+	int pos = servo.low + tiltdir;
+	if(pos >= 180) {
+		tiltdir = -STEP;
+	}
+	if(pos <= 0) {
+		tiltdir = STEP;
+	}
+	if (set_servo(pos, NO_SET, NO_SET))
+		Serial.println("ok.");
+	else
+		Serial.println("failed.");
+}
 
 void loop(void)
 {
 	// Pump the network regularly
 	network.update();
-
-	if(has_reply) {
+	if ( network.available() ) {
+		RF24NetworkHeader header;
+		payload_t payload;
+		network.read(header,&payload,sizeof(payload));
+//		Serial.print("Received packet #");
+//		Serial.print(payload.counter);
+//		Serial.print(" at ");
+//		Serial.println(payload.ms);
+		switch(payload.pload_type) {
+			case PL_MPU:
+				Serial.print("quat\t");
+				Serial.print(payload.d.mpu.quaternion[0]);
+				Serial.print("\t");
+				Serial.print(payload.d.mpu.quaternion[1]);
+				Serial.print("\t");
+				Serial.print(payload.d.mpu.quaternion[2]);
+				Serial.print("\t");
+				Serial.println(payload.d.mpu.quaternion[3]);
+				Serial.print("grav\t");
+				Serial.print(payload.d.mpu.gravity[0]);
+				Serial.print("\t");
+				Serial.print(payload.d.mpu.gravity[1]);
+				Serial.print("\t");
+				Serial.println(payload.d.mpu.gravity[2]);
+				break;
+			case PL_SERVO_STATE:
+				Serial.print("servo\t");
+				Serial.print(payload.d.servo.low);
+				Serial.print("\t");
+				Serial.print(payload.d.servo.pan);
+				Serial.print("\t");
+				Serial.println(payload.d.servo.tilt);
+				servo = payload.d.servo;
+				break;
+		}
+	} else {
 		// If it's time to send a message, send it!
 		unsigned long now = millis();
 		if ( now - last_sent >= interval)
 		{
 			last_sent = now;
-	
-			payload_t payload = { PL_CMD, millis(), packets_sent++ };
-			payload.d.cmd.cmd = CMD_MPU;
-
-//			Serial.print("Sending...");
-//			Serial.print(payload.counter);
-//			Serial.print(", ms=");
-//			Serial.print(payload.ms);
-//			Serial.print(", ");
-//			RF24NetworkHeader header(STICK_NODE);
-//			bool ok = network.write(header,&payload,sizeof(payload));
-//			if (ok)
-//				Serial.println("ok.");
-//			else
-//				Serial.println("failed.");
-			has_reply = false;
-		}
-	} else {
-		if ( network.available() ) {
-			RF24NetworkHeader header;
-			payload_t payload;
-			network.read(header,&payload,sizeof(payload));
-			Serial.print("Received packet #");
-			Serial.print(payload.counter);
-			Serial.print(" at ");
-			Serial.println(payload.ms);
-			Serial.print("quat\t");
-			Serial.print(payload.d.mpu.quaternion[0]);
-			Serial.print("\t");
-			Serial.print(payload.d.mpu.quaternion[1]);
-			Serial.print("\t");
-			Serial.print(payload.d.mpu.quaternion[2]);
-			Serial.print("\t");
-			Serial.println(payload.d.mpu.quaternion[3]);
-			has_reply = true;
+			//move_servo();
 		}
 	}
-	delay(100);
+	delay(10);
 }
--- trunk/ino/include/common.h
+++ trunk/ino/include/common.h
@@ -1,33 +1,36 @@
 #ifndef __COMMON_H
 #define __COMMON_H
 
-enum PLOAD_TYPE : uint8_t
+//#include 
+
+enum PLOAD_TYPE
 {
-	PL_CMD,
+	PL_SETSERVO = 1,
+	PL_SERVO_STATE,
 	PL_MPU
 };
 
-enum CMD_T : uint8_t
-{
-	CMD_MPU
-};
+#define NO_SET 0xff
+// 0xff - no set
+typedef struct __stickservo_t {
+	uint8_t low;
+	uint8_t pan;
+	uint8_t tilt;
+} stickservo_t;
 
-struct cmd_t {
-	CMD_T cmd;
-};
-
-struct mpu_t {
+typedef struct __mpu_t {
 	int16_t quaternion[4];
-};
+	int16_t gravity[3];
+} mpu_t;
 
 // Structure of our payload
 struct payload_t {
-	PLOAD_TYPE pload_type;
-	unsigned long ms;
-	unsigned long counter;
+	uint8_t pload_type; //PLOAD_TYPE
+	uint64_t ms;
+	uint64_t counter;
 	union {
-		uint8_t buf[32];
-		cmd_t cmd;
+		uint8_t buf[32 - sizeof(uint8_t) - sizeof(uint64_t) - sizeof(uint64_t)];
+		stickservo_t servo;
 		mpu_t mpu;
 	} d;
 };
