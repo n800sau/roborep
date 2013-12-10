12666f5c45dcf88dcaa90f16931f5e59169f8f6b
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-12-10 05:57:10 +0000

    catkin_ws added

---
+++ ROS/catkin_ws/src/oculus2wd/scripts/talker.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import rospy
+from oculus2wd.msg import drive
+
+def talker():
+    pub = rospy.Publisher('/oculus_base_command', drive)
+    rospy.init_node('drive_node')
+    while not rospy.is_shutdown():
+        pub.publish(drive(ord('b'), 10, 10, 1))
+        print 'published'
+        rospy.sleep(1.0)
+
+
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
+
