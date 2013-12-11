fde417643e357b0a25a91bfa2c7547ecfceb8401
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-12-11 04:24:54 +0000

    ros_oculus_base working

---
+++ trunk/ino/ros_oculus_base/cmdint.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python
+
+import cmd, sys, os, atexit, readline, socket, time
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'include', 'swig'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lib_py'))
+
+from forklift import run_process, terminate_all
+import libcommon_py as common
+
+import rospy
+from std_msgs.msg import String
+from oculus2wd.msg import drive
+
+def talker():
+	while not rospy.is_shutdown():
+		self.pub.publish(drive(ord('b'), 10, 10, 1))
+		print 'published'
+		rospy.sleep(1.0)
+
+
+
+class OculusShell(cmd.Cmd):
+	intro = 'Welcome to the oculus shell.	Type help or ? to list commands.\n'
+	prompt = '(oculus) '
+
+	def __init__(self):
+		cmd.Cmd.__init__(self)
+		rospy.init_node('drive_node')
+		self.pub = rospy.Publisher('/oculus_base_command', drive)
+		rospy.Subscriber("/oculus_base_reply", String, self.on_reply)
+		self.file = None
+		self.logfile = None
+
+	def write_log(self, line):
+		if not self.logfile:
+			self.logfile = file('oculus_base.log', 'a+')
+		self.logfile.write('%s\n' % line.rstrip())
+		self.logfile.flush()
+
+	def batstat(self):
+		print 'Battery status: %d%%' % (float(file('/sys/class/power_supply/BAT0/charge_now').read())/float(file('/sys/class/power_supply/BAT0/charge_full').read())*100),
+		current = int(file('/sys/class/power_supply/BAT0/current_now').read())
+		if current == 0:
+			print ',charging...'
+		else:
+			print ',current=%s' % current
+
+	def on_reply(self, reply):
+		print reply
+
+	# ----- oculus commands -----
+
+	def do_step_fw(self, arg):
+		'Step forward'
+		self.pub.publish(drive(ord('f'), 255, 1))
+		time.sleep(min(1,float(arg)))
+		self.do_stop('')
+
+	def do_step_back(self, arg):
+		'Step back'
+		self.pub.publish(drive(ord('b'), 255, 1))
+		time.sleep(min(1,float(arg)))
+		self.do_stop('')
+
+	def do_forward(self, arg):
+		'Forward (0-255)'
+		self.pub.publish(drive(ord('f'), int(arg), 1))
+
+	def do_back(self, arg):
+		'Back (0-255)'
+		self.pub.publish(drive(ord('b'), int(arg), 1))
+
+	def do_step_left(self, arg):
+		'Step left'
+		self.pub.publish(drive(ord('l'), 255, 1))
+		time.sleep(min(1,float(arg)))
+		self.do_stop('')
+
+	def do_step_right(self, arg):
+		'Step right'
+		self.pub.publish(drive(ord('r'), 255, 1))
+		time.sleep(min(1,float(arg)))
+		self.do_stop('')
+
+	def do_setcam(self, arg):
+		'Set cam angle [75-100]'
+		self.pub.publish(drive(ord('v'), int(arg), 1))
+
+	def do_release_cam(self, arg):
+		'Release cam'
+		self.pub.publish(drive(ord('w'), 0, 1))
+
+	def do_version(self, arg):
+		'Get firmware version'
+		self.pub.publish(drive(ord('y'), 0, 1))
+
+	def do_stop(self, arg):
+		'Stop motors'
+		self.pub.publish(drive(ord('s'), 0, 1))
+
+	def do_echo(self, arg):
+		'Echo on/off: ECHO <ON|OFF>'
+		if arg.lower() == 'on':
+			self.pub.publish(drive(ord('e'), 1, 1))
+		elif arg.lower() == 'off':
+			self.pub.publish(drive(ord('e'), 0, 1))
+
+
+	# ----- oculus commands end-----
+
+	def do_quit(self, arg):
+		'Exit the shell:	 QUIT'
+		self.close()
+		sys.exit(0)
+		return True
+
+	def do_eof(self, arg):
+		print
+		self.do_quit(arg)
+
+	# ----- record and playback -----
+
+	def do_record(self, arg):
+		'Save future commands to filename:	RECORD destroy.cmd'
+		self.file = open(arg, 'w')
+
+	def do_playback(self, arg):
+		'Playback commands from a file:	 PLAYBACK destroy.cmd'
+		self.close()
+		with open(arg) as f:
+			self.cmdqueue.extend(f.read().splitlines())
+
+	def precmd(self, line):
+		line = line.lower()
+		if self.file and 'playback' not in line:
+			print >>self.file, line
+		return line
+
+	def emptyline(self):
+		self.batstat()
+
+	def close(self):
+		if self.file:
+			self.file.close()
+			self.file = None
+		if self.logfile:
+			self.logfile.close()
+			self.logfile = None
+
+if __name__ == '__main__':
+	histfile = os.path.join(os.environ["HOME"], ".oculushist")
+	try:
+		readline.read_history_file(histfile)
+	except IOError:
+		pass
+	atexit.register(readline.write_history_file, histfile)
+	atexit.register(terminate_all)
+	del histfile
+	OculusShell().cmdloop()
---
+++ trunk/ino/ros_oculus_base/run.sh
@@ -0,0 +1,1 @@
+rosrun rosserial_python serial_node.py /dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A4015M29-if00-port0
