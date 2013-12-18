972232ee59185e760a6cbebc9e8dae22910382ea
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-12-18 22:48:40 +0000

    --make-pidfile

--- trunk/ROS/start.sh
+++ trunk/ROS/start.sh
@@ -2,4 +2,6 @@
 
 PIDFILE=/home/n800s/run/roscore.pid
 DAEMON=`which roscore`
-start-stop-daemon -v --start --user n800s --make-pidfile --pidfile ${PIDFILE} --background --startas ${DAEMON} --chuid n800s -- ${DAEMON_ARGS}
+#DAEMON_ARGS="-p 11111 --pid=$PIDFILE"
+start-stop-daemon -v --start --user n800s --background --make-pidfile --pidfile $PIDFILE --startas ${DAEMON} --chuid n800s -- ${DAEMON_ARGS}
+#start-stop-daemon -v --start --user n800s --pidfile $PIDFILE --startas ${DAEMON} --chuid n800s -- ${DAEMON_ARGS}
