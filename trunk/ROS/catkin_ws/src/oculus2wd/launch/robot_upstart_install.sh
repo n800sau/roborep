77421da9b1d67f68a65429ea25c3d2276dbf741f
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2013-12-28 22:13:33 +0000

    install/uninstall scripts

---
+++ trunk/ROS/catkin_ws/src/oculus2wd/launch/robot_upstart_install.sh
@@ -0,0 +1,1 @@
+rosrun robot_upstart install oculus2wd/launch/minimal.launch --interface wlan0
---
+++ trunk/ROS/catkin_ws/src/oculus2wd/launch/robot_upstart_uninstall.sh
@@ -0,0 +1,2 @@
+su -c "source /opt/ros/hydro/setup.bash; rosrun robot_upstart uninstall oculus2wd"
+#su -c "PATH=/opt/ros/hydro/bin:$PATH rosrun robot_upstart uninstall oculus2wd"
