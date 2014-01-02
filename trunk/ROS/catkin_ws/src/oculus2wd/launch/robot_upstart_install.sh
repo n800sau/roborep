917cab4be8d70bb8cc59e6417b19aacf09f0a4d0
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-01-02 06:05:44 +0000

    about autostart

--- trunk/ROS/catkin_ws/src/oculus2wd/launch/robot_upstart_install.sh
+++ trunk/ROS/catkin_ws/src/oculus2wd/launch/robot_upstart_install.sh
@@ -1,1 +1,2 @@
-rosrun robot_upstart install oculus2wd/launch/minimal.launch --interface wlan0 --job oculus2wd
+rosrun robot_upstart install oculus2wd/launch/minimal.launch --interface wlan0 --job oculus2wd \
+	--setup ${HOME}/work/roborep/ROS/catkin_ws/devel/setup.bash
