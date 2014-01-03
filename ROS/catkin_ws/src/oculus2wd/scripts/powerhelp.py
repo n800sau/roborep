34f4a15d023b17e06c563986462d64ba7d0f6141
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-01-03 01:54:15 +0000

    adjusting switch off mode of powerhelp.py

--- ROS/catkin_ws/src/oculus2wd/scripts/powerhelp.py
+++ ROS/catkin_ws/src/oculus2wd/scripts/powerhelp.py
@@ -10,16 +10,27 @@ from oculus2wd.msg import battery
 
 LEVEL_OFFSET = 0 #for debug purpose it is possible to offset levels by this value
 LEVELS = (10, 20, 40)
+#minimal battery level
+LEVEL_MIN = 5
 
 VOICE = 'voice_kal_diphone'
 #VOICE = 'voice_rab_diphone' #too quiet
 VOICE_RU = 'voice_msu_ru_nsh_clunits' #needs festvox-ru to be installed
 
+TIME2OFF = 120
+
+#to keep pauses between sayings
 last_say_time = 0
+
+#to stop repeating the same level
 last_level = 0
+
+#to have timeout befor switch off
+uptime2off = None
 
 alert_mode = False
 english_mode = True
+powering_off = False
 
 def say(words, voice=None):
 	global last_say_time, english_mode
@@ -29,20 +40,29 @@ def say(words, voice=None):
 
 def callback(msg):
 #		rospy.loginfo(rospy.get_name() + ": Received %s" % msg.status)
-	global last_say_time, alert_mode, english_mode, last_level
+	global last_say_time, alert_mode, english_mode, last_level, uptime2off, powering_off
 	tdiff = time() - last_say_time
 	if msg.discharging:
 		if msg.level < LEVELS[0] + LEVEL_OFFSET:
 			alert_mode = True
-			if tdiff > 20:
+			uptime = float(file('/proc/uptime').read().split()[0])
+			if uptime2off is None:
+				uptime2off = uptime
+			if (tdiff > 10 and not english_mode) or tdiff > 20:
+				time2off = TIME2OFF + uptime2off - uptime
 				# let it some time (5 minutes) to pass after turning on to let the user to cancel the node
-				uptime = float(file('proc/uptime').read().split()[0])
-				if uptime > 300:
+				if time2off < 0 or msg.level < LEVEL_MIN + LEVEL_OFFSET:
+					powering_off = True
 					say('Прощай, жестокий мир. Я выключаюсь!', VOICE_RU)
 					sleep(15)
 					say('Good bye, cruel world. Powering off.')
 					sleep(10)
 					os.system('sudo poweroff')
+				else:
+					if english_mode:
+						say('%d seconds remaining' % time2off)
+					else:
+						say('До выключения осталось %d секунд.' % time2off, VOICE_RU)
 		elif msg.level < LEVELS[1] + LEVEL_OFFSET:
 			alert_mode = True
 			if (tdiff > 10 and not english_mode) or tdiff > 20:
@@ -57,8 +77,13 @@ def callback(msg):
 		last_level = msg.level
 	else:
 		if alert_mode:
-			say('Ok! I\'m happy now.')
+			if powering_off:
+				say('Too late. I has sad.')
+				sleep(10)
+			else:
+				say('Ok! I\'m happy now.')
 			alert_mode = False
+		uptime2off = None
 		last_level = 0
 
 # Ordered this way to minimize wait time.
