89de719c36912e8c91cc172d9131e6ff8a32157a
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-01-07 08:40:13 +0000

    eye.jpg added

--- ROS/opencv/view.sh
+++ ROS/opencv/view.sh
@@ -11,7 +11,7 @@
 #rosrun image_view image_view image:=/camera/depth_registered/image_raw
 #rosrun image_view image_view image:=/camera/depth_registered/hw_registered/image_rect_raw
 
-#rosrun image_view image_view image:=/camera/rgb/image_raw
-rosrun image_view image_saver image:=/camera/rgb/image_raw
+rosrun image_view image_view image:=/camera/rgb/image_raw
+#rosrun image_view image_saver image:=/camera/rgb/image_raw
 
 #rosrun image_view image_view image:=$1
