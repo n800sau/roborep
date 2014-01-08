8ecb8696c22e34b133a6bce2e3f464596245edf3
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-01-08 08:58:41 +0000

    imu_ir.py fixed

--- trunk/ROS/catkin_ws/src/oculus2wd/scripts/imu_ir.py
+++ trunk/ROS/catkin_ws/src/oculus2wd/scripts/imu_ir.py
@@ -7,6 +7,8 @@ from time import time, sleep
 from sound_play.msg import SoundRequest
 from sound_play.libsoundplay import SoundClient
 from sensor_msgs.msg import Imu
+from oculus2wd.msg import drive_status
+from oculus2wd.msg import drive
 
 from twisted.internet import reactor, defer
 from twisted.internet.serialport import SerialPort
@@ -16,34 +18,98 @@ from twisted.internet.task import LoopingCall
 DEVICE = '/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A1014RKM-if00-port0'
 BAUD = 57600
 
+
+
+
 # Ordered this way to minimize wait time.
 rospy.init_node('imu_ir', anonymous = True)
 soundhandle = SoundClient()
 
-class Echo(basic.LineReceiver):
+class Processor(basic.LineReceiver):
+
+	def __init__(self, *args, **kwds):
+		self.commands = {
+			0x40BF906F: {'code': 'FORWARD', 'proc': self.do_forward},
+			0x40BF807F: {'code': 'BACK', 'proc': self.do_back},
+			0x40BF50AF: {'code': 'LEFT', 'proc': self.do_left},
+			0x40BF10EF: {'code': 'RIGHT', 'proc': self.do_right},
+			0x40BFD02F: {'code': 'OK', 'proc': self.do_stop},
+			0xFFFFFFFF: {'code': 'REPEAT', 'proc': self.do_repeat},
+		}
 
 	def connectionMade(self):
 		self.pub = rospy.Publisher('/oculus2wd/imu', Imu)
+		self.cmdpub = rospy.Publisher('/oculus_base_command', drive)
 		rospy.loginfo("Serial port connected")
+		self.imu = None
 #		self.transport.write('>>> ')
 
 	def lineReceived(self, line):
-		line = line.strip().split('\t')
-		if line[0] == 'quat':
-			imu = Imu()
-			imu.orientation.x = line[1]
-			imu.orientation.y = line[2]
-			imu.orientation.z = line[3]
-			imu.orientation.w = line[4]
-			self.pub.publish(imu)
-		print line
+		line = line.strip()
+		if line == 'START':
+			self.imu = Imu()
+		elif not self.imu is None:
+			if line == 'END':
+#				print 'IMU=', self.imu
+				self.pub.publish(self.imu)
+				self.imu = None
+			else:
+				line = line.split('\t')
+				if line[0] == 'quat':
+					self.imu.orientation.w = float(line[1])
+					self.imu.orientation.x = float(line[2])
+					self.imu.orientation.y = float(line[3])
+					self.imu.orientation.z = float(line[4])
+#				elif line[0] == 'euler':
+#					self.imu.angular_velocity.x = float(line[1])
+#					self.imu.angular_velocity.y = float(line[2])
+#					self.imu.angular_velocity.z = float(line[3])
+				elif line[0] == 'ypr':
+					self.imu.angular_velocity.x = float(line[1])
+					self.imu.angular_velocity.y = float(line[2])
+					self.imu.angular_velocity.z = float(line[3])
+				elif line[0] == 'areal':
+					self.imu.linear_acceleration.x = float(line[1])
+					self.imu.linear_acceleration.y = float(line[2])
+					self.imu.linear_acceleration.z = float(line[3])
+		else:
+			line = line.split('\t')
+			if line[0] == 'ir':
+				cmd = self.commands.get(int(line[1]), None)
+				if cmd:
+					rospy.loginfo(cmd['code'])
+					cmd['proc']()
+#		print line
 #		self.sendLine('Echo: ' + line)
 #		self.transport.write('>>> ')
+
+	def do_forward(self):
+		'Forward (0-255)'
+		self.cmdpub.publish(drive(ord('f'), 255, 1))
+
+	def do_back(self):
+		'Back (0-255)'
+		self.cmdpub.publish(drive(ord('b'), 255, 1))
+
+	def do_stop(self):
+		'Stop motors'
+		self.cmdpub.publish(drive(ord('s'), 0, 1))
+
+	def do_left(self):
+		'Step left'
+		self.cmdpub.publish(drive(ord('l'), 255, 1))
+
+	def do_right(self):
+		'Step right'
+		self.cmdpub.publish(drive(ord('r'), 255, 1))
+
+	def do_repeat(self):
+		pass
 
 def sayBye():
 	print "bye bye."
 
-SerialPort(Echo(), DEVICE, reactor, baudrate=BAUD)
+SerialPort(Processor(), DEVICE, reactor, baudrate=BAUD)
 
 #loopObj = LoopingCall(rospy.spinOnce)
 #loopObj.start(0.01, now=True)
