6c17da3f97b8ae4b0355b676e9c091971880b428
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-03-06 04:28:54 +0000

    blob_filter.py added

---
+++ ROS/catkin_ws/src/oculus2wd/scripts/blob_filter.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+import rospy
+from cmvision.msg import Blobs
+
+
+def callback(msg, pub):
+#	print 'Count=%d' % msg.blob_count
+	if msg.blob_count > 0:
+		pub.publish(msg)
+
+
+if __name__ == '__main__':
+	rospy.init_node('msg_filter', anonymous = True)
+	pub = rospy.Publisher('/blobs_filtered', Blobs)
+	rospy.Subscriber("/blobs", Blobs, callback, pub)
+	rospy.spin()
