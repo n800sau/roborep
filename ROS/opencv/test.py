c245d394ff8f324541368846fcccb94c0291c6b4
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-03-14 10:35:36 +0000

    some changes

--- ROS/opencv/test.py
+++ ROS/opencv/test.py
@@ -11,11 +11,12 @@ from cv_bridge import CvBridge, CvBridgeError
 class image_converter:
 
 	def __init__(self):
-		self.image_pub = rospy.Publisher('/image_circle',Image)
+#		self.image_pub = rospy.Publisher('/image_circle',Image)
 
 		cv.NamedWindow("Image window", 1)
 		self.bridge = CvBridge()
-		self.image_sub = rospy.Subscriber('/image_raw',Image,self.callback)
+		self.image_sub = rospy.Subscriber('/camera/rgb/image_raw',Image,self.callback)
+#		self.image_sub = rospy.Subscriber('/image_raw',Image,self.callback)
 
 	def callback(self,data):
 		try:
@@ -23,17 +24,17 @@ class image_converter:
 		except CvBridgeError, e:
 			print e
 
-		(cols,rows) = cv.GetSize(cv_image)
-		if cols > 60 and rows > 60 :
-			cv.Circle(cv_image, (50,50), 10, 255)
+#		(cols,rows) = cv.GetSize(cv_image)
+#		if cols > 60 and rows > 60 :
+#			cv.Circle(cv_image, (50,50), 10, 255)
 
 		cv.ShowImage("Image window", cv_image)
-		cv.WaitKey(3)
+#		cv.WaitKey(3)
 
-		try:
-			self.image_pub.publish(self.bridge.cv_to_imgmsg(cv_image, "bgr8"))
-		except CvBridgeError, e:
-			print e
+#		try:
+#			self.image_pub.publish(self.bridge.cv_to_imgmsg(cv_image, "bgr8"))
+#		except CvBridgeError, e:
+#			print e
 
 def main(args):
 	rospy.init_node('image_converter', anonymous=True)
