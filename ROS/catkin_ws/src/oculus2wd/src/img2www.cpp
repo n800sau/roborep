d42a29a29646c5a148a7d2b712019e9660119fd0
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-04-09 03:37:18 +0000

    working img2www with skin and some color

--- ROS/catkin_ws/src/oculus2wd/src/img2www.cpp
+++ ROS/catkin_ws/src/oculus2wd/src/img2www.cpp
@@ -9,7 +9,9 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/opencv.hpp>
 
-std::string imgfname;
+#include"skin_detector.hpp"
+
+std::string imgfname, clrfname, skinfname;
 image_transport::Subscriber img_sub;
 
 ros::Time last_stamp;
@@ -19,26 +21,46 @@ bool service(std_srvs::Empty::Request &req, std_srvs::Empty::Response &res) {
   return true;
 }
 
+void write_img(const cv::Mat img, std::string fname)
+{
+	cv::Mat c_img = img.clone();
+	char text[50];
+	time_t t = last_stamp.sec;
+	std::strftime(text, sizeof(text), "%H:%M:%S", localtime(&t));
+//	printf("%s\n", text);
+	cv::putText(c_img, text, cvPoint(10,10), CV_FONT_HERSHEY_PLAIN, 0.5, cvScalar(0, 0, 250), 1, CV_AA);
+	unsigned pos = fname.find_last_of('.');
+	std::string ext = fname.substr(pos+1);
+	std::string tmpfname = fname + ".tmp." + ext;
+	cv::imwrite(tmpfname.c_str(), c_img);
+	rename(tmpfname.c_str(), fname.c_str());
+}
+
 void saveimage_cb(const sensor_msgs::ImageConstPtr& msg)
 {
-	cv::Mat dst;
+	SkinDetector mySkinDetector;
+	cv::Mat dst, imgHSV, mask, skinMat;
 	ros::Duration diff = msg->header.stamp - last_stamp;
 	if(diff > step) {
 		last_stamp = msg->header.stamp;
 		cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8);
 		cv::resize(cv_ptr->image, dst, cv::Size(160,120));
+		dst.copyTo(imgHSV);
 		cv::cvtColor(dst, dst, CV_BGR2RGB);
-		char text[50];
-		time_t t = last_stamp.sec;
-		std::strftime(text, sizeof(text), "%H:%M:%S", localtime(&t));
-//		printf("%s\n", text);
-		cv::putText(dst, text, cvPoint(10,10), CV_FONT_HERSHEY_PLAIN, 0.5, cvScalar(0, 0, 250), 1, CV_AA);
-		unsigned pos = imgfname.find_last_of('.');
-		std::string ext = imgfname.substr(pos+1);
-		std::string tmpfname = imgfname + ".tmp." + ext;
-		cv::imwrite(tmpfname.c_str(), dst);
-		printf("%s\n", imgfname.c_str());
-		rename(tmpfname.c_str(), imgfname.c_str());
+		write_img(dst, imgfname);
+
+		cv::GaussianBlur( imgHSV, imgHSV, cv::Size( 3, 3 ), 0, 0 );
+		cv::cvtColor(imgHSV, imgHSV, CV_BGR2HSV);
+//		cv::inRange(imgHSV, cv::Scalar(170,160,60), cv::Scalar(180,255,255), mask);
+		cv::inRange(imgHSV, cv::Scalar(90,0,0), cv::Scalar(180,255,255), mask);
+//		cv::inRange(imgHSV, cv::Scalar(0,0,0), cv::Scalar(255,255,255), mask);
+//		cv::GaussianBlur( mask, mask, cv::Size( 3, 3 ), 0, 0 );
+		cv::cvtColor(mask, mask, CV_GRAY2RGB);
+		write_img(mask, clrfname);
+//		write_img(imgHSV, clrfname);
+		skinMat= mySkinDetector.getSkin(dst);
+		write_img(skinMat, skinfname);
+
 	}
 }
 
@@ -54,7 +76,9 @@ int main(int argc, char ** argv)
 	img_sub = it.subscribe(topic, 1, saveimage_cb);
 
 	ros::NodeHandle local_nh("~");
-	local_nh.param("filename", imgfname, std::string(std::string("/var/www/rgbframe/frame.png")));
+	local_nh.param("filename_raw", imgfname, std::string(std::string("/var/www/rgbframe/frame.png")));
+	local_nh.param("filename_color", clrfname, std::string(std::string("/var/www/rgbframe/frame_color.png")));
+	local_nh.param("filename_skin", skinfname, std::string(std::string("/var/www/rgbframe/frame_skin.png")));
 	ros::ServiceServer save = local_nh.advertiseService ("save", service);
 
 	ros::spin();
