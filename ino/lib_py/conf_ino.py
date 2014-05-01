31a789b6c8c8258b18823b6b018096c665844644
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-05-01 06:18:35 +0000

    local path in parameters

--- ino/lib_py/conf_ino.py
+++ ino/lib_py/conf_ino.py
@@ -6,20 +6,20 @@ from configobj import ConfigObj
 
 
 class Configuration(object):
-    def __init__(self, *files):
-        self.cfg = ConfigObj()
-        for f in files:
-            self.cfg.merge(ConfigObj(os.path.expanduser(f)))
+	def __init__(self, *files):
+		self.cfg = ConfigObj()
+		for f in files:
+			self.cfg.merge(ConfigObj(os.path.expanduser(f)))
 
-    def as_dict(self, section_name):
-        section = self.cfg.get(section_name, ConfigObj())
-        result = self._as_plain_dict(self.cfg)
-        result.update(self._as_plain_dict(section))
-        return result
+	def as_dict(self, section_name):
+		section = self.cfg.get(section_name, ConfigObj())
+		result = self._as_plain_dict(self.cfg)
+		result.update(self._as_plain_dict(section))
+		return result
 
-    def _as_plain_dict(self, section):
-        return dict((key.replace('-', '_'), section[key]) for key in section.scalars)
+	def _as_plain_dict(self, section):
+		return dict((key.replace('-', '_'), section[key]) for key in section.scalars)
 
 
-def configure():
-    return Configuration('/etc/ino.ini', '~/.inorc', 'ino.ini')
+def configure(locdir='.'):
+	return Configuration('/etc/ino.ini', '~/.inorc', os.path.join(locdir, 'ino.ini'))
