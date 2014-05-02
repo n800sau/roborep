ee49afac1619198d7784e22067494524d0d01ceb
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-05-02 06:12:55 +0000

    attempt to make some data reading on wake up instead of one

--- trunk/ino/nrf_mpu6050_lowpwr/src/main.ino
+++ trunk/ino/nrf_mpu6050_lowpwr/src/main.ino
@@ -48,9 +48,14 @@ MPU6050 mpu;
 
 int16_t corr_x = -1660, corr_y = 750, corr_z = -550;
 
-int wakePin = 3;                 // pin used for waking up
-int wakeInt = wakePin - 2;       // interrupt used for waking up
-volatile bool interrupted;
+int wakePin = 3;				 // pin used for waking up
+int wakeInt = wakePin - 2;		 // interrupt used for waking up
+
+#define COUNTER_INIT 20
+
+volatile bool interrupted = false;
+volatile int counter = COUNTER_INIT;
+
 
 void setup() {
 
@@ -100,9 +105,8 @@ void setup() {
 	Serial.println();
 
 	mpu.setInterruptMode(MPU6050_INTMODE_ACTIVELOW);
-	mpu.setIntEnabled((1 << MPU6050_INTERRUPT_MOT_BIT) | (1 << MPU6050_INTERRUPT_ZMOT_BIT));
 // to make periodic wakening uncomment the next line
-//	mpu.setIntEnabled((1 << MPU6050_INTERRUPT_MOT_BIT) | (1 << MPU6050_INTERRUPT_DATA_RDY_BIT));
+	mpu.setIntEnabled(1 << MPU6050_INTERRUPT_DATA_RDY_BIT);
 
 	mpu.setMotionDetectionThreshold(1);
 	mpu.setMotionDetectionDuration(1);
@@ -114,11 +118,11 @@ void setup() {
 	mpu.setWakeFrequency(MPU6050_WAKE_FREQ_10);
 	mpu.setWakeCycleEnabled(true);
 
-//    attachInterrupt(wakeInt, wakeUpNow, LOW); // use interrupt 0 (pin 2) and run function
+	attachInterrupt(wakeInt, wakeUpNow, LOW); // use interrupt 0 (pin 2) and run function
 
 }
 
-void wakeUpNow()        // here the interrupt is handled after wakeup
+void wakeUpNow()		// here the interrupt is handled after wakeup
 {
   // execute code here after wake-up before returning to the loop() function
   // timers and code using timers (serial.print and more...) will not work here.
@@ -127,75 +131,82 @@ void wakeUpNow()        // here the interrupt is handled after wakeup
 	interrupted = true;
 }
 
-void sleepNow()         // here we put the arduino to sleep
+void sleepNow()			// here we put the arduino to sleep
 {
+	mpu.setIntEnabled((1 << MPU6050_INTERRUPT_MOT_BIT) | (1 << MPU6050_INTERRUPT_ZMOT_BIT));
+
 	Serial.println("Timer: Entering Sleep mode");
-	delay(100);     // this delay is needed, the sleep 
-    /* Now is the time to set the sleep mode. In the Atmega8 datasheet
-     * http://www.atmel.com/dyn/resources/prod_documents/doc2486.pdf on page 35
-     * there is a list of sleep modes which explains which clocks and 
-     * wake up sources are available in which sleep mode.
-     *
-     * In the avr/sleep.h file, the call names of these sleep modes are to be found:
-     *
-     * The 5 different modes are:
-     *     SLEEP_MODE_IDLE         -the least power savings 
-     *     SLEEP_MODE_ADC
-     *     SLEEP_MODE_PWR_SAVE
-     *     SLEEP_MODE_STANDBY
-     *     SLEEP_MODE_PWR_DOWN     -the most power savings
-     *
-     * For now, we want as much power savings as possible, so we 
-     * choose the according 
-     * sleep mode: SLEEP_MODE_PWR_DOWN
-     * 
-     */  
-    set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here
+	delay(100);		// this delay is needed, the sleep 
+	/* Now is the time to set the sleep mode. In the Atmega8 datasheet
+	 * http://www.atmel.com/dyn/resources/prod_documents/doc2486.pdf on page 35
+	 * there is a list of sleep modes which explains which clocks and 
+	 * wake up sources are available in which sleep mode.
+	 *
+	 * In the avr/sleep.h file, the call names of these sleep modes are to be found:
+	 *
+	 * The 5 different modes are:
+	 *	   SLEEP_MODE_IDLE		   -the least power savings 
+	 *	   SLEEP_MODE_ADC
+	 *	   SLEEP_MODE_PWR_SAVE
+	 *	   SLEEP_MODE_STANDBY
+	 *	   SLEEP_MODE_PWR_DOWN	   -the most power savings
+	 *
+	 * For now, we want as much power savings as possible, so we 
+	 * choose the according 
+	 * sleep mode: SLEEP_MODE_PWR_DOWN
+	 * 
+	 */	 
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN);   // sleep mode is set here
 
-    sleep_enable();          // enables the sleep bit in the mcucr register
-                             // so sleep is possible. just a safety pin 
+	sleep_enable();			 // enables the sleep bit in the mcucr register
+							 // so sleep is possible. just a safety pin 
 
-    /* Now it is time to enable an interrupt. We do it here so an 
-     * accidentally pushed interrupt button doesn't interrupt 
-     * our running program. if you want to be able to run 
-     * interrupt code besides the sleep function, place it in 
-     * setup() for example.
-     * 
-     * In the function call attachInterrupt(A, B, C)
-     * A   can be either 0 or 1 for interrupts on pin 2 or 3.   
-     * 
-     * B   Name of a function you want to execute at interrupt for A.
-     *
-     * C   Trigger mode of the interrupt pin. can be:
-     *             LOW        a low level triggers
-     *             CHANGE     a change in level triggers
-     *             RISING     a rising edge of a level triggers
-     *             FALLING    a falling edge of a level triggers
-     *
-     * In all but the IDLE sleep modes only LOW can be used.
-     */
+	/* Now it is time to enable an interrupt. We do it here so an 
+	 * accidentally pushed interrupt button doesn't interrupt 
+	 * our running program. if you want to be able to run 
+	 * interrupt code besides the sleep function, place it in 
+	 * setup() for example.
+	 * 
+	 * In the function call attachInterrupt(A, B, C)
+	 * A   can be either 0 or 1 for interrupts on pin 2 or 3.	
+	 * 
+	 * B   Name of a function you want to execute at interrupt for A.
+	 *
+	 * C   Trigger mode of the interrupt pin. can be:
+	 *			   LOW		  a low level triggers
+	 *			   CHANGE	  a change in level triggers
+	 *			   RISING	  a rising edge of a level triggers
+	 *			   FALLING	  a falling edge of a level triggers
+	 *
+	 * In all but the IDLE sleep modes only LOW can be used.
+	 */
 
-    attachInterrupt(wakeInt, wakeUpNow, LOW); // use interrupt 0 (pin 2) and run function
-                                       // wakeUpNow when pin 2 gets LOW 
+//	attachInterrupt(wakeInt, wakeUpNow, LOW); // use interrupt 0 (pin 2) and run function
+									   // wakeUpNow when pin 2 gets LOW 
 
-    sleep_mode();            // here the device is actually put to sleep!!
-                             // THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP
+	sleep_mode();			 // here the device is actually put to sleep!!
+							 // THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP
 
-    sleep_disable();         // first thing after waking from sleep:
-                             // disable sleep...
-    detachInterrupt(wakeInt);      // disables interrupt 0 on pin 2 so the 
-                             // wakeUpNow code will not be executed 
-                             // during normal running time.
+	sleep_disable();		 // first thing after waking from sleep:
+							 // disable sleep...
+
+//	detachInterrupt(wakeInt);	   // disables interrupt 0 on pin 2 so the 
+	delay(1000);  // waits for a second
+
+	mpu.setIntEnabled(1 << MPU6050_INTERRUPT_DATA_RDY_BIT);
+	counter = COUNTER_INIT;
+
+							 // wakeUpNow code will not be executed 
+							 // during normal running time.
 
 }
 
 void loop() {
 
-	delay(1000);                           // waits for a second
-
 	if(interrupted) {
 
 		interrupted = false;
+
 
 		int st = mpu.getIntStatus();
 		Serial.print("\tst: 0x");
@@ -254,10 +265,13 @@ void loop() {
 		radio.powerDown();
 #endif // WITH_NRF
 
+		counter--;
+		if(counter < 0) {
+			sleepNow();		// sleep function called here
+		}
 	}
 
-	delay(100);     // this delay is needed, the sleep 
-                      //function will provoke a Serial error otherwise!!
-	sleepNow();     // sleep function called here
+	delay(100);		// this delay is needed, the sleep 
+					  //function will provoke a Serial error otherwise!!
 
 }
