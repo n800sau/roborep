7a2f6ca914f7c7e3797f39cf2e3bc0cb4a8bd302
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-05-07 01:55:52 +0000

    while fixing duplicate csv data problem

--- ino/pc2nrf/nrfredis_client.py
+++ ino/pc2nrf/nrfredis_client.py
@@ -13,8 +13,6 @@ MAX2READ = 100
 
 if __name__ == '__main__':
 
-	files = {}
-
 	cfgobj = configure(os.path.dirname(__file__))
 	cfg = cfgobj.as_dict('serial2redis')
 	params = {'host': cfg.get('redis_host', 'localhost')}
@@ -27,35 +25,48 @@ if __name__ == '__main__':
 	queues = {}
 	while True:
 		try:
-			for rk in r.smembers('s.queues'):
-				els = r.lrange(rk, 0, MAX2READ)
-				lastel = (queues.get(rk, []) + [None])[0]
-				try:
-					lastndx = els.index(lastel)
-				except ValueError:
-					lastndx = -1
-				if lastndx >= 0:
-					#remove old elements
-					els = els[:lastndx]
+			for marker in r.smembers('s.queues'):
+				lasttimestamp = None
+				if marker not in queues:
+					queues[marker] = {'lastel' : None}
+					queues[marker]['file'] = file(os.path.join(os.path.dirname(__file__), marker + '.csv'), 'a+')
+					queues[marker]['csv'] = csv.writer(queues[marker]['file'])
+					# read last csv file line
+					for row in csv.reader(queues[marker]['file']):
+						ldict = dict(zip(row[::2], row[1::2]))
+						lasttimestamp = int(ldict['secs'])
+				qdata = queues[marker]
+				els = r.lrange(marker, 0, MAX2READ)
+				if qdata['lastel']:
+					# remove old elements
+					try:
+						lastndx = els.index(qdata['lastel'])
+					except ValueError:
+						lastndx = -1
+					if lastndx >= 0:
+						els = els[:lastndx]
 				if els:
-					queues[rk] = els
-				#process new elements
+					# save the last element for the future reference
+					qdata['lastel'] = els[0]
+				# write remains to csv
 				for line in reversed(els):
-					if line:
-						marker = line[0]
-						line = line.split(common.DATA_SEPARATOR)[1:]
-						ldict = dict(zip(line[::2], line[1::2]))
+					line = [s.strip() for s in line.split(common.DATA_SEPARATOR)[1:]]
+					ldict = dict(zip(line[::2], line[1::2]))
+					if lasttimestamp is None or lasttimestamp < int(ldict['secs']):
 #						print time.strftime('%d/%m/%Y %H:%M:%S', time.localtime(float(ldict['secs'])))
-						if marker not in files:
-							files[marker] = {}
-							files[marker]['file'] = file(os.path.join(os.path.dirname(__file__), marker + '.csv'), 'a')
-							files[marker]['csv'] = csv.writer(files[marker]['file'])
-						files[marker]['csv'].writerow([
-								time.strftime('%d.%m.%Y %H:%M:%S'),
-								time.strftime('%d/%m/%Y %H:%M:%S', time.localtime(float(ldict['secs'])))
-							] + [s.strip() for s in line])
-						files[marker]['file'].flush()
+						qdata['csv'].writerow(line + [
+								time.strftime('%d/%m/%Y %H:%M:%S', time.localtime(float(ldict['secs']))),
+								time.strftime('%d.%m.%Y %H:%M:%S')
+							]
+						)
+#					else:
+#						print 'Old data', time.strftime('%d/%m/%Y %H:%M:%S', time.localtime(float(ldict['secs'])))
+					qdata['file'].flush()
 		except KeyboardInterrupt:
 			break
 		except:
 			traceback.print_exc()
+
+	for qdata in queues.values():
+		qdata['file'].close()
+
