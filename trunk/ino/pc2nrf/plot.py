4a9ba32d37eb675ff65b69e6d8cce5b9fb4c73af
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-05-19 01:51:05 +0000

    a little bit fixed plot

--- trunk/ino/pc2nrf/plot.py
+++ trunk/ino/pc2nrf/plot.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
 
+import csv
 import matplotlib as mpl
 mpl.use('Agg')
 import numpy as np
 import matplotlib.pyplot as plt
 
-data = np.genfromtxt('q.S.csv', delimiter=',', usecols = (1, 3), names=['x', 'y'])
+f = file('q.R.csv')
+vf = csv.reader(f)
+data_x = []
+data_y = []
+for row in vf:
+    v = dict(zip(row[::2], row[1::2]))
+    data_x.append(int(v['secs']))
+    data_y.append(float(v['acc_x']))
+
+#data = np.genfromtxt('q.R.csv', delimiter=',', usecols = (1, 7), names=['x', 'y'])
+
+
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
@@ -14,7 +26,7 @@ ax1.set_title("Numbers")
 ax1.set_xlabel('time')
 ax1.set_ylabel('Z-z-z')
 
-ax1.plot(data['x'], data['y'], color='r', label='the data')
+ax1.plot(data_x, data_y, color='r', label='the data')
 
 leg = ax1.legend()
 
