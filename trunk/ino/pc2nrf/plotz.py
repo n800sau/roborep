4f647c2f639b8f2e80ed8a000c0633fd61348e76
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-06-02 01:31:48 +0000

    to remember sent files in redis set, x,y,z plots. to try send real door open plot only

--- trunk/ino/pc2nrf/plotz.py
+++ trunk/ino/pc2nrf/plotz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import csv, time
+import csv, time, os, sys, redis
 import matplotlib as mpl
 mpl.use('Agg')
 import numpy as np
@@ -11,7 +11,12 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 
-TIMEOUT = 120 #secs
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'include', 'swig'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lib_py'))
+
+from conf_ino import configure
+
+NEXT_EVENT_TIMEOUT = 120 #secs
 
 def send_email(msgtxt, fnamelist):
 	msg = MIMEMultipart()
@@ -30,12 +35,22 @@ def send_email(msgtxt, fnamelist):
 	smtpserver.sendmail(msg['From'], msg['To'], msg.as_string())
 	smtpserver.close()
 
+cfgobj = configure(os.path.dirname(__file__))
+cfg = cfgobj.as_dict('serial2redis')
+params = {'host': cfg.get('redis_host', 'localhost')}
+redis_port = cfg.get('redis_port', None)
+if redis_port:
+	params['port'] = redis_port
 
-timeout = timedelta(seconds=TIMEOUT)
+r = redis.Redis(**params)
+
+timeout = timedelta(seconds=NEXT_EVENT_TIMEOUT)
 f = file('q.R.csv')
 vf = csv.reader(f)
 data_time = []
 data_x = []
+data_y = []
+data_z = []
 last_t = None
 fnamelist = []
 for row in vf:
@@ -43,31 +58,55 @@ for row in vf:
 	t = datetime.fromtimestamp(float(ldict['secs']) + float(ldict['moffset'])/1000)
 	if last_t is None or last_t + timeout < t:
 		# draw plot and start next plot
-		if data_x:
+		if (data_x and max(data_x)-min(data_x) > 100) or (data_y and max(data_y)-min(data_y) > 100) or (data_z and max(data_z)-min(data_z) > 100):
 			# draw plot
-			fig = plt.figure()
-			ax1 = fig.add_subplot(111)
 
-			ax1.set_title("Numbers")
-			ax1.set_xlabel('time')
-			ax1.set_ylabel('Z-z-z')
+			if data_x:
+				fig = plt.figure()
+				ax1 = fig.add_subplot(221)
+				ax1.set_title("X")
+				ax1.set_xlabel('time')
+				ax1.set_ylabel('value')
+				ax1.plot(data_time, data_x, color='r', label='X')
 
-			ax1.plot(data_time, data_x, color='r', label='the data')
+			if data_y:
+				ax2 = fig.add_subplot(222)
+				ax2.set_title("Y")
+				ax2.set_xlabel('time')
+				ax2.set_ylabel('value')
+				ax2.plot(data_time, data_y, color='g', label='Y')
+
+			if data_z:
+				ax3 = fig.add_subplot(223)
+				ax3.set_title("X")
+				ax3.set_xlabel('time')
+				ax3.set_ylabel('value')
+				ax3.plot(data_time, data_z, color='b', label='Z')
+
+
 			plt.gcf().autofmt_xdate()
 
 			leg = ax1.legend()
 
 			#plt.show()
 			fname = 'plot_%s.png' % last_t.strftime('%Y.%m.%d-%H:%M')
+			fname = os.path.join('/var/www/http/atad', fname)
 			plt.savefig(fname, bbox_inches='tight')
 			fnamelist.append(fname)
 			print fname, 'saved'
+			if not r.sismember('s.files', fname):
+#				send_email(r, 'door plots', [fname])
+				r.sadd('s.files', fname)
+				print fname, 'sent'
 		data_x = []
+		data_y = []
+		data_z = []
 		data_time = []
 	data_time.append(t)
 	data_x.append(float(ldict['acc_x']))
+	data_y.append(float(ldict['acc_y']))
+	data_z.append(float(ldict['acc_z']))
 	last_t = t
-send_email('dorr plots', fnamelist)
 
 #data = np.genfromtxt('q.R.csv', delimiter=',', usecols = (1, 7), names=['x', 'y'])
 
