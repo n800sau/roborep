f059b03863e19ebc9f3158ccc26bfeee737dbfcc
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-06-04 02:56:51 +0000

    send xyz plots

--- ino/pc2nrf/plotz.py
+++ ino/pc2nrf/plotz.py
@@ -35,6 +35,35 @@ def send_email(msgtxt, fnamelist):
 	smtpserver.sendmail(msg['From'], msg['To'], msg.as_string())
 	smtpserver.close()
 
+def draw_plot(ldict):
+
+	fig = plt.figure()
+
+	if data_x:
+		ax1 = fig.add_subplot(221)
+		ax1.set_title("X")
+		ax1.set_xlabel('time')
+		ax1.set_ylabel('value')
+		ax1.plot(data_time, data_x, color='r', label='X')
+
+	if data_y:
+		ax2 = fig.add_subplot(222)
+		ax2.set_title("Y")
+		ax2.set_xlabel('time')
+		ax2.set_ylabel('value')
+		ax2.plot(data_time, data_y, color='g', label='Y')
+
+	if data_z:
+		ax3 = fig.add_subplot(223)
+		ax3.set_title("X")
+		ax3.set_xlabel('time')
+		ax3.set_ylabel('value')
+		ax3.plot(data_time, data_z, color='b', label='Z')
+
+	plt.gcf().autofmt_xdate()
+	ax1.legend()
+
+
 cfgobj = configure(os.path.dirname(__file__))
 cfg = cfgobj.as_dict('serial2redis')
 params = {'host': cfg.get('redis_host', 'localhost')}
@@ -52,7 +81,6 @@ data_x = []
 data_y = []
 data_z = []
 last_t = None
-fnamelist = []
 for row in vf:
 	ldict = dict(zip(row[::2], row[1::2]))
 	t = datetime.fromtimestamp(float(ldict['secs']) + float(ldict['moffset'])/1000)
@@ -60,44 +88,17 @@ for row in vf:
 		# draw plot and start next plot
 		if (data_x and max(data_x)-min(data_x) > 100) or (data_y and max(data_y)-min(data_y) > 100) or (data_z and max(data_z)-min(data_z) > 100):
 			# draw plot
-
-			if data_x:
-				fig = plt.figure()
-				ax1 = fig.add_subplot(221)
-				ax1.set_title("X")
-				ax1.set_xlabel('time')
-				ax1.set_ylabel('value')
-				ax1.plot(data_time, data_x, color='r', label='X')
-
-			if data_y:
-				ax2 = fig.add_subplot(222)
-				ax2.set_title("Y")
-				ax2.set_xlabel('time')
-				ax2.set_ylabel('value')
-				ax2.plot(data_time, data_y, color='g', label='Y')
-
-			if data_z:
-				ax3 = fig.add_subplot(223)
-				ax3.set_title("X")
-				ax3.set_xlabel('time')
-				ax3.set_ylabel('value')
-				ax3.plot(data_time, data_z, color='b', label='Z')
-
-
-			plt.gcf().autofmt_xdate()
-
-			leg = ax1.legend()
-
-			#plt.show()
-			fname = 'plot_%s.png' % last_t.strftime('%Y.%m.%d-%H:%M')
-			fname = os.path.join('/var/www/http/atad', fname)
+			bname = 'plot_%s.png' % last_t.strftime('%Y.%m.%d-%H:%M')
+			draw_plot(ldict)
+			fname = os.path.join('/var/www/http/atad', bname)
 			plt.savefig(fname, bbox_inches='tight')
-			fnamelist.append(fname)
 			print fname, 'saved'
-			if not r.sismember('s.files', fname):
-#				send_email(r, 'door plots', [fname])
-				r.sadd('s.files', fname)
-				print fname, 'sent'
+			if not r.sismember('s.files', bname):
+				send_email('door plots', [fname])
+				r.sadd('s.files', bname)
+				print bname, 'sent'
+			#plt.show()
+
 		data_x = []
 		data_y = []
 		data_z = []
