f304ee68c0169cb31578da6acdc1804f3c88b432
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-06-11 01:42:21 +0000

    arduino compatibility lib added

---
+++ trunk/lib/compatibility/compatibility.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   compatiblity.h
+ * Author: purinda
+ *
+ * Created on 24 June 2012, 3:08 PM
+ */
+
+#ifndef COMPATIBLITY_H
+#define	COMPATIBLITY_H
+
+void __msleep(int milisec);
+void __usleep(int milisec);
+void __start_timer();
+long __millis();
+
+#endif	/* COMPATIBLITY_H */
+
