2d6d090c0531c97862d098a0184353d6de44f57b
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-06-11 06:39:24 +0000

    catching exception

--- lib/gpio/gpio.h
+++ lib/gpio/gpio.h
@@ -1,14 +1,3 @@
-/* 
- * https://github.com/mrshu/GPIOlib
- * Copyright (c) 2011, Copyright (c) 2011 mr.Shu
- * All rights reserved. 
- * 
- * Modified on 24 June 2012, 11:06 AM
- * File:   gpio.h
- * Author: purinda (purinda@gmail.com)
- * 
- */
-
 #ifndef GPIO_H
 #define	GPIO_H
 
--- lib/pc2nrf/pc2nrfd.cpp
+++ lib/pc2nrf/pc2nrfd.cpp
@@ -1,9 +1,16 @@
 #include "pc2nrf.h"
-#include <stdio.h>
+#include <iostream>
+#include <exception>
+#include <errno.h>
 
 int main(int argc, char const **argv)
 {
-	PC2NRF srv = PC2NRF();
-	srv.run();
-	return 0;
+	try {
+		PC2NRF srv = PC2NRF();
+		srv.run();
+		return 0;
+	} catch (const std::exception& e) {
+		std::cerr << "Error " << e.what() << endl << strerror(errno) << endl;
+		return 1;
+	}
 }
