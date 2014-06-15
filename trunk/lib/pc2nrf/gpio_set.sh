2ada6faf96578bfa0734a78b17112dd0f212ac27
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-06-15 11:58:03 +0000

    change of irq pin

--- trunk/lib/pc2nrf/gpio_set.sh
+++ trunk/lib/pc2nrf/gpio_set.sh
@@ -2,18 +2,22 @@
 
 sudo su -c '
 GROUP=n800s
-for p in 115 117
+for p in 115 117 72 70
 do
 echo $p >/sys/class/gpio/export
 echo out >/sys/class/gpio/gpio${p}/direction
 chmod g+w /sys/class/gpio/gpio${p}/value
 chgrp ${GROUP} /sys/class/gpio/gpio${p}/value
 done
-for p in 60
+for p in 39 73 71
 do
 echo $p >/sys/class/gpio/export
 echo in >/sys/class/gpio/gpio${p}/direction
 chmod g+r /sys/class/gpio/gpio${p}/value
 chgrp ${GROUP} /sys/class/gpio/gpio${p}/value
+done
+for p in 39 73 71
+do
+echo falling > /sys/class/gpio/gpio${p}/edge
 done
 '
