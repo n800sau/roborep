5781538cad16fd8d3cc375d9ca450496d4bcfed3
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-06-17 05:11:48 +0000

    change of ping and working pc2nrf

--- lib/pc2nrf/gpio_set.sh
+++ lib/pc2nrf/gpio_set.sh
@@ -2,21 +2,21 @@
 
 sudo su -c '
 GROUP=n800s
-for p in 115 117 72 70
+for p in 115 117 72
 do
 echo $p >/sys/class/gpio/export
 echo out >/sys/class/gpio/gpio${p}/direction
 chmod g+w /sys/class/gpio/gpio${p}/value
 chgrp ${GROUP} /sys/class/gpio/gpio${p}/value
 done
-for p in 39 73 71
+for p in 39 73
 do
 echo $p >/sys/class/gpio/export
 echo in >/sys/class/gpio/gpio${p}/direction
 chmod g+r /sys/class/gpio/gpio${p}/value
 chgrp ${GROUP} /sys/class/gpio/gpio${p}/value
 done
-for p in 39 73 71
+for p in 39 73
 do
 echo falling > /sys/class/gpio/gpio${p}/edge
 done
