8eaf557bb1833fe7483defeb6738d53abb23439f
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-06-18 06:12:27 +0000

    cmd.<myid> queue changed to subscribe cmd.<myid>

--- lib/ReServant/hiredis_ext.h
+++ lib/ReServant/hiredis_ext.h
@@ -2,10 +2,13 @@
 
 #define __HIREDIS_EXT_H
 
+#include <stdio.h>
 #include <hiredis.h>
 #include <async.h>
 
 void *redisCommandN(redisContext *c, int num, ...);
 int redisAsyncCommandN(redisAsyncContext *ac, redisCallbackFn *fn, void *privdata, int num, ...);
+
+void fprint_reply(redisReply *reply, int indent=0, FILE *stream=stdout);
 
 #endif
