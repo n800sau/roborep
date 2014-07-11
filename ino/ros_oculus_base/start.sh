358f22e10c76e3194a9518f25dcbe493007848c2
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-07-11 06:03:55 +0000

    ser2tcp_twisted.py added

--- ino/ros_oculus_base/start.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-PIDFILE=${HOME}/run/oculus_mega.pid
-DAEMON="`which rosrun` rosserial_python serial_node.py /dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A4015M29-if00-port0"
-start-stop-daemon -v --start --user n800s --make-pidfile --pidfile ${PIDFILE} --background --startas ${DAEMON} --chuid n800s -- ${DAEMON_ARGS}
