88adaff99ad2cfba0f36585d2999cc65c4000033
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-09-16 01:34:29 +0000

    encoder enhansment

--- ino/ros_icc_base/src/main.ino
+++ ino/ros_icc_base/src/main.ino
@@ -1,12 +1,15 @@
-int EN1 = 6;
-int EN2 = 5;
-int IN1 = 7;
-int IN2 = 4;
+int period2stop = 200;
 
+
+int lEN = 6;
+int rEN = 5;
+int lIN = 7;
+int rIN = 4;
+
+volatile int lMotorDest = 0;
 volatile bool lMotorReverse = false;
-volatile bool lMotorStop = true;
+volatile int rMotorDest = 0;
 volatile bool rMotorReverse = false;
-volatile bool rMotorStop = true;
 
 int Eleft = 3;
 int Eright = 2;
@@ -44,60 +47,94 @@ volatile uint8_t intRhistory = 0;
 
 void lIntCB()
 {
-	if(lMotorStop || micros() - intLtime < threshold )
+	if( micros() - intLtime < threshold )
 		return;
 	intLhistory = intLsignal;
 	intLsignal = bitRead(PIND,2);
 	if ( intLhistory==intLsignal )
 		return;
 	intLtime = micros();
-	lEncSteps += (lMotorReverse) ? 1 : -1;
+	if(lMotorDest) {
+		lMotorDest += (lMotorDest < 0) ? 1 : -1;
+	}
+	lEncSteps += (lMotorReverse) ? -1 : 1;
 }
 
 void rIntCB()
 {
-	if (rMotorStop || micros() - intRtime < threshold )
+	if (micros() - intRtime < threshold )
 		return;
 	intRhistory = intRsignal;
 	intRsignal = bitRead(PIND,3);
 	if ( intRhistory==intRsignal )
 		return;
 	intRtime = micros();
-	rEncSteps += (rMotorReverse) ? 1 : -1;
+	if(rMotorDest) {
+		rMotorDest += (rMotorDest < 0) ? 1 : -1;
+	}
+	rEncSteps += (rMotorReverse) ? -1 : 1;
 }
 
 
-//Latest version use pin 4 instead of pin 8
-void setLeftMotor(int pwm, boolean reverse)
+void setLeftMotor(int pwm, int steps)
 {
-	analogWrite(EN1,pwm); //set pwm control, 0 for stop, and 255 for maximum speed
-	digitalWrite(IN1,(reverse) ? HIGH : LOW);
-	lMotorReverse = reverse;
-	lMotorStop = pwm == 0;
+	if(steps) {
+		lMotorDest += steps;
+	} else {
+		lMotorDest = 0;
+	}
+	analogWrite(lEN,pwm); //set pwm control, 0 for stop, and 255 for maximum speed
+	lMotorReverse = lMotorDest < 0;
+	digitalWrite(lIN,(lMotorReverse) ? HIGH : LOW);
 }
 
-void setRightMotor(int pwm, boolean reverse)
+void setRightMotor(int pwm, int steps)
 {
-	analogWrite(EN2,pwm);
-	digitalWrite(IN2,(reverse) ? HIGH : LOW);
-	rMotorReverse = reverse;
-	rMotorStop = pwm == 0;
+	if(steps) {
+		rMotorDest += steps;
+	} else {
+		rMotorDest = 0;
+	}
+	analogWrite(rEN,pwm);
+	rMotorReverse = rMotorDest < 0;
+	digitalWrite(rIN,(rMotorReverse < 0) ? HIGH : LOW);
+}
+
+void stop()
+{
+	setLeftMotor(0, 0);
+	setRightMotor(0, 0);
+}
+
+int lastLVal = -1;
+int lastRVal = -1;
+
+void printDests()
+{
+	Serial.print("M ");
+	Serial.print(lMotorDest);
+	Serial.print(" ");
+	Serial.println(rMotorDest);
 }
 
 void printEncoders()
 {
-	Serial.print(lEncSteps);
-	Serial.print(" ");
-	Serial.println(rEncSteps);
+	int lval = lEncSteps;
+	int rval = rEncSteps;
+	if(lastLVal != lval || lastRVal != rval) {
+		Serial.print(lastLVal=lval);
+		Serial.print(" ");
+		Serial.println(lastRVal=rval);
+	}
 }
 
 void setup()
 {
 	Serial.begin(57600);
-	pinMode(EN1, OUTPUT);
-	pinMode(EN2, OUTPUT);
-	pinMode(IN1, OUTPUT);
-	pinMode(IN2, OUTPUT);
+	pinMode(lEN, OUTPUT);
+	pinMode(rEN, OUTPUT);
+	pinMode(lIN, OUTPUT);
+	pinMode(rIN, OUTPUT);
 	pinMode(Eleft, INPUT);
 	pinMode(Eright, INPUT);
 	digitalWrite(Eleft, HIGH);
@@ -106,18 +143,18 @@ void setup()
 	attachInterrupt(lInt, lIntCB, CHANGE);
 }
 
-void stop()
-{
-	setLeftMotor(0,false);
-	setRightMotor(0,false);
-}
-
 void loop()
 {
 	char val;
 	while(1)
 	{
 		printEncoders();
+		if(lMotorDest == 0) {
+			setLeftMotor(0, 0);
+		}
+		if(rMotorDest == 0) {
+			setRightMotor(0, 0);
+		}
 		val = Serial.read();
 		if(val!=-1)
 		{
@@ -125,31 +162,47 @@ void loop()
 			switch(val)
 			{
 				case 'w'://Move ahead
-					setLeftMotor(240,true);
-					setRightMotor(240,true);
-					delay(100);
+					setLeftMotor(240,5);
+					setRightMotor(240,5);
+					printDests();
+					delay(period2stop);
+					printDests();
 					stop();
 					break;
 				case 'x'://move back
-					setLeftMotor(240,false);
-					setRightMotor(240,false);
-					delay(100);
+					setLeftMotor(240,-5);
+					setRightMotor(240,-5);
+					printDests();
+					delay(period2stop);
+					printDests();
 					stop();
 					break;
 				case 'd'://turn left
-					setLeftMotor(240,false);
-					setRightMotor(240,true);
-					delay(100);
+					setLeftMotor(240,-5);
+					setRightMotor(240,5);
+					printDests();
+					delay(period2stop);
+					printDests();
 					stop();
 					break;
 				case 'a'://turn right
-					setLeftMotor(240,true);
-					setRightMotor(240,false);
-					delay(100);
+					setLeftMotor(240,5);
+					setRightMotor(240,-5);
+					printDests();
+					delay(period2stop);
+					printDests();
 					stop();
 					break;
 				case 's'://stop
 					stop();
+					break;
+				case 'p':
+					period2stop += 100;
+					Serial.println(period2stop);
+					break;
+				case 'l':
+					period2stop -= 100;
+					Serial.println(period2stop);
 					break;
 			}
 		}
