e12ad98c92f9fed5ce1825f28e8aff751ac30d61
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2014-09-24 01:01:20 +0000

    before design change to encoders-main

--- ino/ros_icc_base/src/main.ino
+++ ino/ros_icc_base/src/main.ino
@@ -1,23 +1,49 @@
 #include <voltage.h>
+#include "../../include/printf.h"
 
+// time in millisecs to stop if no encoder reading
+#define TIME2STOP 10000
+#define COUNT_PER_REV 20.0
+#define WHEEL_DIAMETER 0.065
+#define BASELINE 0.14
+
+#define ENC_STEP (COUNT_PER_REV * WHEEL_DIAMETER / M_PI)
+
+// count of encoder ticks until stop
 int stepSize = 5;
 
-int lEN = 6;
-int rEN = 5;
-int lIN = 7;
-int rIN = 4;
+// motor pins
+int rEN = 6;
+int lEN = 5;
+int rIN = 7;
+int lIN = 4;
 
+
+// encoder count before the left motor stops
 volatile int lMotorDest = 0;
+// left motor direction
 volatile bool lMotorReverse = false;
 
+// encoder count before the right motor stops
 volatile int rMotorDest = 0;
+// right motor direction
 volatile bool rMotorReverse = false;
 
+// left motor default power
+int lPower = 255;
+// right motor default power
+int rPower = 230;
+
+// encoder pins
 int Eleft = 3;
 int Eright = 2;
 
+// encoder interrupts
 int lInt = Eleft - 2;
 int rInt = Eright - 2;
+
+// calculated coordinates and angle
+volatile float angl = 0, x = 0, y = 0;
 
 // 'threshold' is the De-bounce Adjustment factor for the Rotary Encoder. 
 //
@@ -34,9 +60,18 @@ int rInt = Eright - 2;
 volatile unsigned long threshold = 10000;
 
 
+// left encoder steps total
 volatile long lEncSteps = 0;
-volatile long rEncSteps = 0;
+// left encoder steps total of the last calculation
+long lMSteps = 0;
 
+// right encoder steps total
+volatile long rEncSteps = 0;
+// right encoder steps total of the last calculation
+long rMSteps = 0;
+
+// last calculation time
+volatile unsigned long last_millis = millis();
 
 // Working variables for the interrupt routines
 //
@@ -47,10 +82,16 @@ volatile uint8_t intRsignal = 0;
 volatile uint8_t intLhistory = 0;
 volatile uint8_t intRhistory = 0;
 
+void resetLastMillis()
+{
+	last_millis = millis();
+}
+
 void lIntCB()
 {
 	if( micros() - intLtime < threshold )
 		return;
+	resetLastMillis();
 	intLhistory = intLsignal;
 	intLsignal = bitRead(PIND,Eright);
 	if ( intLhistory==intLsignal )
@@ -59,13 +100,15 @@ void lIntCB()
 	if(lMotorDest) {
 		lMotorDest += (lMotorDest < 0) ? 1 : -1;
 	}
-	lEncSteps += (lMotorReverse) ? -1 : 1;
+	int v = (lMotorReverse) ? -1 : 1;
+	lEncSteps += v;
 }
 
 void rIntCB()
 {
 	if (micros() - intRtime < threshold )
 		return;
+	resetLastMillis();
 	intRhistory = intRsignal;
 	intRsignal = bitRead(PIND,Eleft);
 	if ( intRhistory==intRsignal )
@@ -74,12 +117,14 @@ void rIntCB()
 	if(rMotorDest) {
 		rMotorDest += (rMotorDest < 0) ? 1 : -1;
 	}
-	rEncSteps += (rMotorReverse) ? -1 : 1;
+	int v = (rMotorReverse) ? -1 : 1;
+	rEncSteps += v;
 }
 
 
 void setLeftMotor(int pwm, int steps)
 {
+	resetLastMillis();
 	if(lMotorDest && steps == 0) {
 		Serial.println("Stop Left");
 	}
@@ -101,6 +146,7 @@ void setLeftMotor(int pwm, int steps)
 
 void setRightMotor(int pwm, int steps)
 {
+	resetLastMillis();
 	if(rMotorDest && steps == 0) {
 		Serial.println("Stop Right");
 	}
@@ -144,13 +190,34 @@ void printEncoders()
 	if(lastLVal != lval || lastRVal != rval) {
 		Serial.print(lastLVal=lval);
 		Serial.print(" ");
-		Serial.println(lastRVal=rval);
+		Serial.print(lastRVal=rval);
+		Serial.print(" ");
+		Serial.print(x);
+		Serial.print(" ");
+		Serial.println(y);
 	}
+}
+
+float xypos(int left, int right)
+{
+	if(left || right) {
+		Serial.print("left=");
+		Serial.print(left);
+		Serial.print(", right=");
+		Serial.println(right);
+	}
+	float Sl = ENC_STEP * left;
+	float Sr = ENC_STEP * right;
+	float s = (Sr + Sl) / 2;
+	angl = (Sr - Sl) / BASELINE + angl;
+	x = s * cos(angl) + x;
+	y = s * sin(angl) + y;
 }
 
 void setup()
 {
 	Serial.begin(57600);
+	printf_begin();
 	pinMode(lEN, OUTPUT);
 	pinMode(rEN, OUTPUT);
 	pinMode(lIN, OUTPUT);
@@ -163,17 +230,49 @@ void setup()
 	attachInterrupt(rInt, lIntCB, CHANGE);
 }
 
+void keep_straight(bool reverse)
+{
+	int diff = abs(rMotorDest - lMotorDest);
+	if(diff > 1) {
+		if(reverse) {
+			if(rMotorDest > lMotorDest) {
+				// speed up right
+			} else {
+				// speed up left
+			}
+		} else {
+			if(rMotorDest > lMotorDest) {
+				// speed up left
+			} else {
+				// speed up right
+			}
+		}
+		printf("DIFF=%d\r\n", diff);
+	}
+}
+
 void loop()
 {
 	char val;
 	while(1)
 	{
+		if(lMotorReverse == rMotorReverse && (lMotorDest || lMotorDest) ) {
+			keep_straight(lMotorReverse);
+		}
 		printEncoders();
-		if(lMotorDest == 0) {
+		xypos(lEncSteps - lMSteps, rEncSteps - rMSteps);
+		lMSteps = lEncSteps;
+		rMSteps = rEncSteps;
+		if( ((lMotorReverse) ? -lMotorDest : lMotorDest ) <= 0) {
 			setLeftMotor(0, 0);
 		}
-		if(rMotorDest == 0) {
+		if( ((rMotorReverse) ? -rMotorDest : rMotorDest ) <= 0) {
 			setRightMotor(0, 0);
+		}
+		unsigned long cur_millis = millis();
+		unsigned long millisdiff = (last_millis > cur_millis) ? ((unsigned long) -1) - last_millis + cur_millis : cur_millis - last_millis;
+		if( millisdiff > TIME2STOP) {
+			stop();
 		}
 		val = Serial.read();
 		if(val!=-1)
@@ -182,20 +281,24 @@ void loop()
 			switch(val)
 			{
 				case 'w'://Move ahead
-					setLeftMotor(240,stepSize);
-					setRightMotor(240,stepSize);
+					stop();
+					setLeftMotor(lPower,stepSize);
+					setRightMotor(rPower,stepSize);
 					break;
 				case 'x'://move back
-					setLeftMotor(240,-stepSize);
-					setRightMotor(240,-stepSize);
+					stop();
+					setLeftMotor(lPower,-stepSize);
+					setRightMotor(rPower,-stepSize);
 					break;
-				case 'd'://turn left
-					setLeftMotor(240,-stepSize);
-					setRightMotor(240,stepSize);
+				case 'a'://turn left
+					stop();
+					setLeftMotor(lPower,-stepSize);
+					setRightMotor(rPower,stepSize);
 					break;
-				case 'a'://turn right
-					setLeftMotor(240,stepSize);
-					setRightMotor(240,-stepSize);
+				case 'd'://turn right
+					stop();
+					setLeftMotor(lPower,stepSize);
+					setRightMotor(rPower,-stepSize);
 					break;
 				case 's'://stop
 					stop();
