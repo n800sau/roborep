159ef653b136157b177bc98f4f8fd7a51c4deb25
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2015-03-11 00:52:46 +0000

    working test output json

--- trunk/ino/HUB-ee/src/main.ino
+++ trunk/ino/HUB-ee/src/main.ino
@@ -13,7 +13,7 @@
 // l3g4200d - 0x69
 
 
-int led = 13;
+int led = 6;
 int lon = 0;
 
 void setup()
@@ -37,44 +37,41 @@ void setup()
 void printState()
 {
 	int v = readVccMv();
-	Serial.print("V:");
+	Serial.print("{\"V\":");
 	Serial.print(v);
-	Serial.print(",head:");
+	Serial.print(",\"head\":");
 	Serial.print(headingDegrees);
-	Serial.print(",acc_x:");
+	Serial.print(",\"acc_x\":");
 	Serial.print(accel_scaled.XAxis);
-	Serial.print(",gyro_x:");
+	Serial.print(",\"gyro_x\":");
 	Serial.print((int)gyro.g.x);
 	Serial.println();
 
-	Serial.print("T:");
+	Serial.print(", \"T\":");
 	Serial.print(bmp.readTemperature());
-	Serial.print(" *C");
 
-	Serial.print(", P:");
+	Serial.print(", \"P\":");
 	Serial.print(bmp.readPressure());
-	Serial.print(" Pa");
 
 	// Calculate altitude assuming 'standard' barometric
 	// pressure of 1013.25 millibar = 101325 Pascal
-	Serial.print(", Alt:");
+	Serial.print(", \"Alt\":");
 	Serial.print(bmp.readAltitude());
-	Serial.print(" m");
 
   // you can get a more precise measurement of altitude
   // if you know the current sea level pressure which will
   // vary with weather and such. If it is 1015 millibars
   // that is equal to 101500 Pascals.
 	// Sydney 1018.1 hPa
-	Serial.print(", R.alt:");
+	Serial.print(", \"R.alt\":");
 	Serial.print(bmp.readAltitude(101500));
-	Serial.print(" m");
-	Serial.print(", IR dist:");
+	Serial.print(", \"IRdist\":");
 	Serial.print(distance);
-	Serial.print(", Presence detected:");
+	Serial.print(", \"Presence\":");
 	Serial.print(MVcount);
 
-	Serial.println();
+	Serial.println("}");
+	Serial.println(".");
 }
 
 void loop()
