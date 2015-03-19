6c8ec461b577ed9dabd0f86aef64a948e0d44a20
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2015-03-19 02:35:40 +0000

    slowing down motors when Count is low

--- ino/HUB-ee/src/motor_proc.h
+++ ino/HUB-ee/src/motor_proc.h
@@ -1,4 +1,5 @@
 extern volatile int motor1QeiCounts, motor2QeiCounts;
+extern float motor1Coef, motor2Coef;
 
 void setup_motors();
 void process_motors();
@@ -7,3 +8,6 @@ void mv_forward(int ms);
 void mv_back(int ms);
 void turn_left(int ms);
 void turn_right(int ms);
+
+void calibrate_motors();
+
