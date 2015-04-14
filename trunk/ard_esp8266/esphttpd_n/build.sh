98faf646580d75e89bb3ad5d8a507c4cc3810e31
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2015-04-14 03:20:47 +0000

    esphttpd_n and libraries added

---
+++ trunk/ard_esp8266/esphttpd_n/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PWD=`pwd`
+SKETCH=`basename $PWD`
+echo $SKETCH
+rm -rf /tmp/esp8266
+/opt/arduino-1.6.1/arduino -v --verify --pref build.path=/tmp/esp8266 --pref sketchbook.path=.. --board esp8266com:esp8266:esp01 ${SKETCH}.ino &> build.log
