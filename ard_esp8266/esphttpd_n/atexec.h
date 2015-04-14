a58d9ac1470d54e90798e27dac3c9983055ad770
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2015-04-14 04:48:50 +0000

    separate class to process at commands

--- ard_esp8266/esphttpd_n/atexec.h
+++ ard_esp8266/esphttpd_n/atexec.h
@@ -1,10 +1,23 @@
+#ifndef __ATEXEC_H
+#define __ATEXEC_H
+
 #include <WiFiClient.h>
+#include <LimitedList.h>
+
 
 class AtExec {
 	protected:
 		WiFiClient client;
+		LimitedList<String, 5> argv;
 	public:
-		inline AtExec(WiFiClient client): client(client) {}
+		inline AtExec(WiFiClient client):
+			client(client),argv(false)
+			{}
 
 		void parseCommand(String &cmd);
+
+		static void print_help(AtExec *self);
+		static void config_baud(AtExec *self);
 };
+
+#endif //__ATEXEC_H
