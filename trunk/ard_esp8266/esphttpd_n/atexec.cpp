8b418e820eede459334353c8b6976521029c306a
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2015-04-14 04:48:50 +0000

    separate class to process at commands

--- trunk/ard_esp8266/esphttpd_n/atexec.cpp
+++ trunk/ard_esp8266/esphttpd_n/atexec.cpp
@@ -1,17 +1,56 @@
 #include "atexec.h"
 
-void AtExec::parseCommand(String &cmd) {
-	Serial.print("parse command=");
-	Serial.println(cmd);
+typedef struct config_commands {
+	String command;
+	void (*function)(AtExec *self);
+} config_commands_t;
 
-	if(cmd.equals("quit")) {
+const config_commands_t config_commands[] = { 
+	{ "HELP", &AtExec::print_help },
+	{ "BAUD", &AtExec::config_baud },
+//	{ "IFCONFIG", &do_ifconfig },
+//	{ "RESET", &config_cmd_reset }, 
+//	{ "MODE", &config_cmd_mode },
+//	{ "STA", &config_cmd_sta },
+//	{ "AP", &config_cmd_ap },
+//	{ "IORST", &io_reset },
+//	{ "DEBUG", &config_debug_mode },
+	{ NULL, NULL }
+};
+
+void AtExec::parseCommand(String &line) {
+
+	Serial.print("parse command=");
+	Serial.println(line);
+
+	if(line.startsWith("+++AT")) {
+
+		int pos = 5;
+
+		while(pos > -1) {
+			int nextpos = line.indexOf(" ", pos+1);
+			String el = ((nextpos < 0) ? line.substring(pos) : line.substring(pos, nextpos));
+			el.trim();
+			if(el.length() > 0) {
+				argv.push(el);
+			}
+			pos = nextpos;
+		}
+
+		if(argv.count() > 0) {
+			String cmd = argv.get(0);
+			for (int i = 0; config_commands[i].command; ++i) {
+				if (cmd.equals(config_commands[i].command)) {
+					config_commands[i].function(this);
+					break;
+				}
+			}
+		}
+	}
+
+/*	if(cmd.equals("quit")) {
 		client.stop();
 	} else if(cmd.equals("help")) {
-		Serial.println("Help output");
-		client.println("--- Telnet Server Help ---");
-		client.println("on    : switch on the Main Power");
-		client.println("off   : switch off the Main Power");
-		client.println("quit  : close the connection");
 	} else if(cmd.equals("on")) {
 		digitalWrite(5, HIGH);
 		client.println("set on");
@@ -21,5 +60,21 @@ void AtExec::parseCommand(String &cmd) {
 	} else {
 		client.print("Invalid command:");
 		client.println(cmd);
-	}
+	}*/
 }
+
+void AtExec::print_help(AtExec *self) {
+	self->client.println("--- Telnet Server Help ---");
+	self->client.println("on    : switch on the Main Power");
+	self->client.println("off   : switch off the Main Power");
+	self->client.println("quit  : close the connection");
+}
+
+void AtExec::config_baud(AtExec *self) {
+	for(int i=0; i<self->argv.count(); i++) {
+		Serial.println(self->argv.get(i));
+	}
+//	Serial.end();
+//	Serial.begin(115200);
+}
+
