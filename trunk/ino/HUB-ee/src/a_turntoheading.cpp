51fd917a5582cad4c5977179b50c3f717330e011
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2015-04-24 06:43:53 +0000

    action classes -> files

---
+++ trunk/ino/HUB-ee/src/a_turntoheading.cpp
@@ -0,0 +1,26 @@
+#include "a_turntoheading.h"
+#include "motor_proc.h"
+#include "hmc5883l_proc.h"
+
+TurnToHeading::TurnToHeading(int heading) {
+	this->heading = heading % 360;
+	if(this->heading < 0)
+		this->heading += 360;
+}
+
+bool TurnToHeading::loop() {
+	if(abs(heading-headingDegrees) > 5) {
+		int diff = headingDegrees - heading;
+		if((diff > 0 && diff < 180) || (diff < 0 && diff < -180)) {
+			// turn right
+			turn_right(1000);
+		} else {
+			// turn left
+			turn_left(1000);
+		}
+	} else {
+		stop();
+		return false;
+	}
+	return true;
+}
---
+++ trunk/ino/HUB-ee/src/action.h
@@ -0,0 +1,11 @@
+#ifndef __ACTION_H
+
+#define __ACTION_H
+
+class Action {
+	public:
+		virtual bool loop() = 0;
+};
+
+
+#endif //__ACTION_H
