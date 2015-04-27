07bd6fd4521cfb49ac9c939ff055107a5b9c356c
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2015-04-27 06:06:45 +0000

    mask bw bmp

--- ino/HUB-ee/www/data.js
+++ ino/HUB-ee/www/data.js
@@ -1,4 +1,52 @@
 init = function() {
+	var data = null;
+	var bwdata = null;
+
+	var draw = function()
+	{
+		$('#canvas').each(function(idx, el) {
+			var ctx = el.getContext('2d');
+			ctx.clearRect (0, 0, el.width, el.height);
+			var w = 60;
+			var h = 80;
+			var imgData=ctx.createImageData(h,w);
+			var imgDataMasked=ctx.createImageData(h,w);
+			var coef5 = 256/(1 << 5);
+			var coef6 = 256/(1 << 6);
+			for(var y=0; y<h; y++) {
+				for(var x=0; x<w; x++) {
+					var dp = (y*w+x)*2;
+
+					var bwp = y+x*h;
+					var bwbyte = Math.floor(bwp / 8);
+					var bwbit = 7 - (bwp % 8);
+
+					var msk = (bwdata[bwbyte] >> bwbit) & 1;
+
+					var word = data[dp] + (data[dp+1] << 8);
+//					var b1 = (word & 0x1f) * coef5;
+//					var g1 = ((word >> 5) & 0x3f) * coef6;
+//					var r1 = (word >> 11) * coef5;
+					var b = (data[dp] & 0x1f) * coef5;
+					var g = ((data[dp] >> 5) + ((data[dp+1] & 0x7) << 3)) * coef6;
+					var r = (data[dp+1] >> 3) * coef5;
+					dp = (y+x*h)*4;
+					imgData.data[dp] = r;
+					imgData.data[dp+1] = g;
+					imgData.data[dp+2] = b;
+					imgData.data[dp+3] = 255;
+					imgDataMasked.data[dp] = r;
+					imgDataMasked.data[dp+1] = g;
+					imgDataMasked.data[dp+2] = b;
+					imgDataMasked.data[dp+3] = (msk) ? 255 : 0;
+				}
+			}
+			ctx.putImageData(imgData,10,10);
+			// add mask
+			ctx.putImageData(imgDataMasked,10,110);
+		});
+	}
+
 	var xhr = new XMLHttpRequest();
 	xhr.open('GET', 'data.bin', true);
 	//xhr.responseType = 'blob';
@@ -8,37 +56,39 @@ init = function() {
 			// get binary data as a response
 			//var blob = this.response;
 			//console.log(blob.size);
-			var data = new Uint8Array(this.response); 
-			$('#canvas').each(function(idx, el) {
-				var ctx = el.getContext('2d');
-				ctx.clearRect (0, 0, el.width, el.height);
-				var w = 60;
-				var h = 80;
-				var imgData=ctx.createImageData(w,h);
-				var coef5 = 256/(1 << 5);
-				var coef6 = 256/(1 << 6);
-				for(var y=0; y<h; y++) {
-					for(var x=0; x<w; x++) {
-						var dp = (y*w+x)*2;
-						var word = data[dp] + (data[dp+1] << 8);
-//						var b1 = (word & 0x1f) * coef5;
-//						var g1 = ((word >> 5) & 0x3f) * coef6;
-//						var r1 = (word >> 11) * coef5;
-						var b = (data[dp] & 0x1f) * coef5;
-						var g = ((data[dp] >> 5) + ((data[dp+1] & 0x7) << 3)) * coef6;
-						var r = (data[dp+1] >> 3) * coef5;
-						dp = (y*w+x)*4;
-						imgData.data[dp] = r;
-						imgData.data[dp+1] = g;
-						imgData.data[dp+2] = b;
-						imgData.data[dp+3] = 255;
-					}
-				}
-				ctx.putImageData(imgData,10,10);
-			});
+			data = new Uint8Array(this.response);
 		} else {
 			alert('error');
 		}
 	};
 	xhr.send();
+	var xhr = new XMLHttpRequest();
+	xhr.open('GET', 'bwdata.bin', true);
+	//xhr.responseType = 'blob';
+	xhr.responseType = 'arraybuffer';
+	xhr.onload = function(e) {
+		if (this.status == 200) {
+			// get binary data as a response
+			//var blob = this.response;
+			//console.log(blob.size);
+			bwdata = new Uint8Array(this.response);
+		} else {
+			alert('error');
+		}
+	};
+	xhr.send();
+	var count = 10;
+	var try_draw = function() {
+		if(data && bwdata) {
+			draw();
+		} else {
+			count--;
+			if(count > 0) {
+				setTimeout(try_draw, 1000);
+			} else {
+				alert('Data can not be loaded');
+			}
+		}
+	};
+	setTimeout(try_draw, 1000);
 };
