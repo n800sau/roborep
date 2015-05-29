41da129486831af317f175571511f1269e3016ef
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2015-05-29 06:15:47 +0000

    cmucam4 scripts added

---
+++ trunk/ard_esp8266/RoboServers/cpdatabin.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+cp data.bin /var/www/html/rgbframe/cam/data.bin
+cp bwdata.bin /var/www/html/rgbframe/cam/bwdata.bin
+cp twdata.json /var/www/html/rgbframe/cam/twdata.json
+cp tcdata.json /var/www/html/rgbframe/cam/tcdata.json
--- trunk/ard_esp8266/RoboServers/usec.sh
+++ trunk/ard_esp8266/RoboServers/usec.sh
@@ -1,10 +1,5 @@
 esp=192.168.1.97
 wget http://${esp}:8080/selser?params=1 --quiet -O -
 echo
-wget --quiet http://${esp}:8080/message -O -
-echo
 wget http://${esp}:8080/setbaud?params=19200 --quiet -O -
-echo
-echo BAUD
-wget --quiet http://${esp}:8080/message -O -
 echo
