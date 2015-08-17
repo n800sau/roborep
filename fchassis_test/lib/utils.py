c3bdb07f3ffed49a3121b4b91cc593e80b852109
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2015-08-17 05:28:40 +0000

    frobo added

--- fchassis_test/lib/utils.py
+++ fchassis_test/lib/utils.py
@@ -2,3 +2,7 @@ import sys, time, datetime
 
 def dbprint(text):
 	print >>sys.__stderr__, '[%s]:%s' % (datetime.datetime.fromtimestamp(time.time()).strftime('%d/%m/%Y %H:%M:%S.%f'), text)
+
+# angle difference in degrees
+def angle_diff(a1, a2):
+	return ((a1 - a2) + 180) % 360 - 180
