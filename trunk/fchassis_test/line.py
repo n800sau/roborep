118e11eb6c118f86f71ad665c96e456f461d3219
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2015-08-18 01:00:42 +0000

    line.* added, dots attribute

---
+++ trunk/fchassis_test/line.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import sys, os, time
+
+import picamera
+from lib.utils import dbprint
+from lib.camera import update_img
+
+from lib.frobo import frobo
+
+if __name__ == '__main__':
+
+	s_port = '/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AH00ZTCM-if00-port0'
+
+	with frobo(s_port) as c:
+		#c.debug = False
+
+		with picamera.PiCamera() as camera:
+
+			try:
+				c.turn(0)
+				update_img(camera, 'pic0.jpg')
+				c.turn(90)
+				c.fwd_straightly(max_secs=3, max_steps=100)
+				c.turn(0)
+				update_img(camera, 'pic1.jpg')
+				c.turn(90)
+				c.fwd_straightly(max_secs=3, max_steps=100)
+				c.turn(0)
+				update_img(camera, 'pic2.jpg')
+				c.turn(90)
+				c.fwd_straightly(max_secs=3, max_steps=100)
+				c.turn(0)
+				update_img(camera, 'pic3.jpg')
+				c.turn(90)
+				c.fwd_straightly(max_secs=3, max_steps=100)
+				c.turn(0)
+				update_img(camera, 'pic4.jpg')
+			finally:
+				update_img(camera)
---
+++ trunk/fchassis_test/line.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python line.py 2> line.log
