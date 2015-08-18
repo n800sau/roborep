8dfc73fe86a7f5a094892a732b9ea2d1fceabaa3
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2015-08-18 02:45:13 +0000

    dth lib moved

--- trunk/ard_esp8266/RoboServers/usec.sh
+++ trunk/ard_esp8266/RoboServers/usec.sh
@@ -1,5 +1,6 @@
-esp=192.168.1.97
+esp=192.168.1.153
 wget http://${esp}:8080/selser?params=1 --quiet -O -
 echo
-wget http://${esp}:8080/setbaud?params=19200 --quiet -O -
+#wget http://${esp}:8080/setbaud?params=19200 --quiet -O -
+wget http://${esp}:8080/setbaud?params=115200 --quiet -O -
 echo
