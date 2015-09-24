774fed9424aeca2d43bfd1f8854cbbe20bfafb4d
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2015-09-24 02:59:44 +0000

    new main on after giving up firmata

---
+++ trunk/ino/ros_icc_base/src/commands.h
@@ -0,0 +1,27 @@
+#ifndef __COMMANDS_H
+
+#define __COMMANDS_H
+
+// commands
+enum COMMANDS {
+	C_STATE = 0x01,
+	C_MLEFT,
+	C_MRIGHT,
+	C_MBOTH,
+	C_MSTOP
+};
+
+// reply
+enum REPLY {
+	R_OK_0 = 0x01,
+	R_ERROR_0,
+	R_VOLTS_1F,
+	R_MCOUNTS_2F,
+	R_MDIST_2F,
+	R_DIST_1F,
+	R_END                // end of block marker
+};
+
+
+
+#endif //__COMMANDS_H
