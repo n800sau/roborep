fb19be79e6b147c45c46d6cac433d5df61eb135d
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2015-10-15 04:36:32 +0000

    RASPICAM_ROTATE in use

--- trunk/fchassis_test/shapes.py
+++ trunk/fchassis_test/shapes.py
@@ -6,12 +6,7 @@ import picamera, cv2
 from lib.utils import dbprint, html_path
 from lib.camera import update_img, ShapeSearch, capture_cvimage
 
-from lib.frobo_ng import frobo_ng
-
 if __name__ == '__main__':
-
-	c = frobo_ng()
-	#c.debug = True
 
 	with picamera.PiCamera() as camera:
 
@@ -19,6 +14,11 @@ if __name__ == '__main__':
 		try:
 			data = ss.find_shapes()
 			if data:
+				if os.environ.get('RASPICAM_ROTATE', ''):
+					angle = int(os.environ['RASPICAM_ROTATE'])
+					rows,cols,depth = data['frame'].shape
+					M = cv2.getRotationMatrix2D((cols/2,rows/2), 180, 1)
+					data['frame'] = cv2.warpAffine(data['frame'], M, (cols,rows))
 				cv2.imwrite(html_path('shapes.jpg'), data['frame'])
 		finally:
 			update_img(camera)
