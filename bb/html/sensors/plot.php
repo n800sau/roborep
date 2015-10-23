e867741a1c8045abb0e9c1cde1737de7d56e39e3
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2015-10-23 03:32:29 +0000

    bb html block added (with sensors)

---
+++ bb/html/sensors/plot.php
@@ -0,0 +1,19 @@
+<?php
+
+require __DIR__.'/Predis.php';
+
+$hname = 'dht11_data';
+$r = new Predis\Client();
+$data = $r->hgetall($hname);
+ksort($data, SORT_NUMERIC);
+$data = array_slice($data, -10, 10, TRUE);
+foreach($data as $ts => &$d) {
+	$d = json_decode($d);
+	$d->l = date('H:i:s', (int)$ts);
+}
+
+header('Content-type: application/json');
+echo json_encode($data, JSON_PRETTY_PRINT);
+
+?>
+
