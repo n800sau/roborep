52196c2d9fe7054cdc666ce7e51732335de41e91
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2015-12-15 00:42:40 +0000

    working clock with NTP option

--- trunk/ard_esp8266/ds3232_test/src/main.ino
+++ trunk/ard_esp8266/ds3232_test/src/main.ino
@@ -2,17 +2,161 @@
 #include <Time.h>         //http://www.arduino.cc/playground/Code/Time  
 #include <Wire.h>         //http://arduino.cc/en/Reference/Wire (included with Arduino IDE)
 #include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+#include "config.h"
+
+// To read VCC voltage
+// TOUT pin has to be disconnected in this mode.
+ADC_MODE(ADC_VCC);
+
+const char* ssid     = SSID;
+const char* password = PASSWORD;
+
+// NTP stuff
+
+unsigned int ntpLocalPort = 2390;      // local port to listen for UDP packets
+
+/* Don't hardwire the IP address or we won't get the benefits of the pool.
+ *  Lookup the IP address for the host name instead */
+//IPAddress timeServer(129, 6, 15, 28); // time.nist.gov NTP server
+IPAddress timeServerIP; // time.nist.gov NTP server address
+const char* ntpServerName = "time.nist.gov";
+
+const int NTP_PACKET_SIZE = 48; // NTP time stamp is in the first 48 bytes of the message
+
+byte packetBuffer[ NTP_PACKET_SIZE]; //buffer to hold incoming and outgoing packets
+
+// A UDP instance to let us send and receive packets over UDP
+WiFiUDP udp;
+
+// send an NTP request to the time server at the given address
+unsigned long sendNTPpacket(IPAddress& address)
+{
+	Serial.println("sending NTP packet...");
+	// set all bytes in the buffer to 0
+	memset(packetBuffer, 0, NTP_PACKET_SIZE);
+	// Initialize values needed to form NTP request
+	// (see URL above for details on the packets)
+	packetBuffer[0] = 0b11100011;	 // LI, Version, Mode
+	packetBuffer[1] = 0;		 // Stratum, or type of clock
+	packetBuffer[2] = 6;		 // Polling Interval
+	packetBuffer[3] = 0xEC;	// Peer Clock Precision
+	// 8 bytes of zero for Root Delay & Root Dispersion
+	packetBuffer[12]	= 49;
+	packetBuffer[13]	= 0x4E;
+	packetBuffer[14]	= 49;
+	packetBuffer[15]	= 52;
+
+	// all NTP fields have been given values, now
+	// you can send a packet requesting a timestamp:
+	udp.beginPacket(address, 123); //NTP requests are to port 123
+	udp.write(packetBuffer, NTP_PACKET_SIZE);
+	udp.endPacket();
+}
+
+time_t request_time()
+{
+	time_t epoch = 0;
+
+	//get a random server from the pool
+	WiFi.hostByName(ntpServerName, timeServerIP); 
+
+	Serial.print("NTP ip:");
+	Serial.println(timeServerIP);
+
+	sendNTPpacket(timeServerIP); // send an NTP packet to a time server
+	// wait to see if a reply is available
+	delay(1000);
+	
+	int cb = udp.parsePacket();
+	if (!cb) {
+		Serial.println("no packet yet");
+	}
+	else {
+//		Serial.print("packet received, length=");
+//		Serial.println(cb);
+		// We've received a packet, read the data from it
+		udp.read(packetBuffer, NTP_PACKET_SIZE); // read the packet into the buffer
+
+		//the timestamp starts at byte 40 of the received packet and is four bytes,
+		// or two words, long. First, esxtract the two words:
+
+		unsigned long highWord = word(packetBuffer[40], packetBuffer[41]);
+		unsigned long lowWord = word(packetBuffer[42], packetBuffer[43]);
+		// combine the four bytes (two words) into a long integer
+		// this is NTP time (seconds since Jan 1 1900):
+		unsigned long secsSince1900 = highWord << 16 | lowWord;
+//		Serial.print("Seconds since Jan 1 1900 = " );
+//		Serial.println(secsSince1900);
+
+		// now convert NTP time into everyday time:
+		// Unix time starts on Jan 1 1970. In seconds, that's 2208988800:
+		const unsigned long seventyYears = 2208988800UL;
+		// subtract seventy years:
+		epoch = secsSince1900 - seventyYears;
+		// print Unix time:
+//		Serial.print("Unix time = ");
+//		Serial.println(epoch);
+
+
+		// print the hour, minute and second:
+		Serial.print("The UTC time is ");			 // UTC is the time at Greenwich Meridian (GMT)
+		Serial.print((epoch	% 86400L) / 3600); // print the hour (86400 equals secs per day)
+		Serial.print(':');
+		if ( ((epoch % 3600) / 60) < 10 ) {
+			// In the first 10 minutes of each hour, we'll want a leading '0'
+			Serial.print('0');
+		}
+		Serial.print((epoch	% 3600) / 60); // print the minute (3600 equals secs per minute)
+		Serial.print(':');
+		if ( (epoch % 60) < 10 ) {
+			// In the first 10 seconds of each minute, we'll want a leading '0'
+			Serial.print('0');
+		}
+		Serial.println(epoch % 60); // print the second
+		RTC.set(epoch);
+	}
+	return (epoch == 0) ? RTC.get() : epoch;
+}
+
+
 
 void setup(void)
 {
-	WiFi.mode(WIFI_OFF);
 	Wire.begin(12, 14);
 	Serial.begin(115200);
+
+	// Connect to WiFi network
+	WiFi.begin(ssid, password);
+	WiFi.mode(WIFI_STA);
+	Serial.print("\n\r \n\rWorking to connect");
+
+	// Wait for connection
+	int conn_status;
+	do {
+		Serial.print(".");
+		conn_status = WiFi.status();
+		delay(500);
+	} while(conn_status != WL_CONNECTED);
+	Serial.println("");
+	Serial.println("Clock test");
+	Serial.print("Connected to ");
+	Serial.println(ssid);
+	Serial.print("IP address: ");
+	Serial.println(WiFi.localIP());
+	Serial.print("Gateway address: ");
+	Serial.println(WiFi.gatewayIP());
+
+	udp.begin(ntpLocalPort);
+
+//	setSyncProvider(request_time);  //set function to call when sync required
+//	setSyncInterval(5);
 	setSyncProvider(RTC.get);   // the function to get the time from the RTC
-	if(timeStatus() != timeSet) 
-		Serial.println("Unable to sync with the RTC");
-	else
-		Serial.println("RTC has set the system time");      
+//	if(timeStatus() != timeSet) 
+//		Serial.println("Unable to sync with the RTC");
+//	else
+//		Serial.println("RTC has set the system time");      
 }
 
 void loop(void)
