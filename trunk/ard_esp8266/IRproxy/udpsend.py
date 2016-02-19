17bee1b6c66c71d154d8d30bc26b7609df11a891
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-02-19 03:34:32 +0000

    ir code udp send test

--- trunk/ard_esp8266/IRproxy/udpsend.py
+++ trunk/ard_esp8266/IRproxy/udpsend.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python
 
-import socket
+import socket, json, time
 
 MCAST_GRP = '239.0.0.57'
 MCAST_PORT = 12345
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
-sock.sendto("multi data", (MCAST_GRP, MCAST_PORT))
+sock.sendto(json.dumps({
+	'encoding': 'SONY',
+	'ircode': 0x290,
+}), (MCAST_GRP, MCAST_PORT))
+print 'sent'
+time.sleep(1)
