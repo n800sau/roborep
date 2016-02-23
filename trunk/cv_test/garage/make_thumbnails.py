cc14d443a87f499f90782eab9fb4bb69aeb498d7
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-02-23 00:02:17 +0000

    make_thumbnails.py added

---
+++ trunk/cv_test/garage/make_thumbnails.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+import os, sys, imutils, glob, cv2, shutil
+
+SUBDIR = '2016-02-15'
+SRC_PATH = os.path.expanduser('~/sshfs/asus/root/rus_hard/garage')
+DST_PATH = os.path.expanduser('output/images/thumbs')
+
+def process(subdir):
+	srcdir = os.path.join(SRC_PATH, subdir)
+	for fn in glob.glob(os.path.join(srcdir, '*')):
+		bname = os.path.basename(fn)
+		if os.path.isdir(fn):
+			process(os.path.join(subdir, bname))
+		else:
+			if os.path.splitext(fn)[-1] == '.jpg':
+				dfn = os.path.join(DST_PATH, subdir, bname)
+				ddn = os.path.dirname(dfn)
+				if not os.path.exists(ddn):
+					os.makedirs(ddn)
+				image = cv2.imread(fn)
+				image = imutils.resize(image, width=40)
+				cv2.imwrite(dfn, image)
+
+if __name__ == '__main__':
+
+	for fn in glob.glob(os.path.join(SRC_PATH, SUBDIR, '*')):
+		bname = os.path.basename(fn)
+		if os.path.isdir(fn):
+			process(os.path.join(SUBDIR, bname))
