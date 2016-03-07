d4b88c5a22b0a16614215b599ec095284c96b67f
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-03-07 23:54:40 +0000

    pump, pan, tilt

--- trunk/ard_esp8266/ScareCrowWater/runterm.sh
+++ trunk/ard_esp8266/ScareCrowWater/runterm.sh
@@ -3,5 +3,9 @@
 DEV=/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0
 #DEV=/dev/ttyUSB0
 
-miniterm.py -p $DEV -b 115200 --rts 0 --dtr 0 >runterm.log
-#/opt/Espressif/miniterm.py -p $DEV -b 115200 --rts 0 --dtr 0
+#~/bin/miniterm.py -p $DEV -b 74880 --rts 0 --dtr 0 &>runterm.log
+~/bin/miniterm.py -p $DEV -b 115200 --rts 0 --dtr 0 &>runterm.log
+#~/bin/miniterm.py -p $DEV -b 115200 --rts 0 --dtr 0
+#~/bin/miniterm.py -p $DEV -b 74880 --rts 0 --dtr 0
+
+#picocom -b 115200 $DEV
