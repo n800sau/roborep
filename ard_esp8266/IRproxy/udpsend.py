164c5951b1c53002e2a2fe836d01e1d60f96d2d2
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-03-08 03:12:34 +0000

    version with aplay

--- ard_esp8266/IRproxy/udpsend.py
+++ ard_esp8266/IRproxy/udpsend.py
@@ -8,8 +8,8 @@ MCAST_PORT = 12345
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
 sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 2)
 sock.sendto(json.dumps({
-	'encoding': 'SONY',
-	'ircode': 0x291,
+	'encoding': 'NEC',
+	'ircode': 0xfd4ab5,
 }), (MCAST_GRP, MCAST_PORT))
 print 'sent'
 time.sleep(1)
