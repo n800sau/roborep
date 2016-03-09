b147aa11cdaf8c51d5ab2a886e526ec10b222bc6
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-03-09 03:45:44 +0000

    period_file.py added

--- cv_test/garage/misc.py
+++ cv_test/garage/misc.py
@@ -4,10 +4,11 @@ class fname2dt_exc(Exception):
 	pass
 
 # 00D6FB009223(n800sau)_1_20151202020654_2294.jpg
+# 00D6FB009223(n800sau)_1_20160218110531_28316.jpg
 def fname2dt(fname):
 	rs = None
 	bname = os.path.basename(fname)
-	rexp = r'^.+\(n800sau\)_\d_(\d{14})_\d{4}\.jpg$'
+	rexp = r'^.+\(n800sau\)_\d_(\d{14})_\d+\.jpg$'
 	match = re.match(rexp, bname)
 	if match is None:
 		raise fname2dt_exc('File name does not match agains %s' % rexp)
