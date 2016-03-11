29b58408f91e274e13042afbbb0c13f1e3c3533c
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-03-11 00:05:41 +0000

    start on boot fixed

--- ard_esp8266/IRproxy/udprecv.py
+++ ard_esp8266/IRproxy/udprecv.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import sys
+import datetime
 import socket
 import struct
 import json
@@ -16,6 +18,12 @@ VOL_UP = 0xfdb04f
 VOL_DOWN = 0xfd8877
 IRCODE_LIST = (OWN_WEATHER, OUTER_WEATHER, VOL_UP, VOL_DOWN)
 
+MCAST_GRP = '239.0.0.57'
+MCAST_PORT = 12345
+
+def dbprint(text):
+    print >>sys.__stderr__, '[%s]:%s' % (datetime.datetime.fromtimestamp(time.time()).strftime('%d/%m/%Y %H:%M:%S.%f'), text)
+
 def get_last_data():
 	r = redis.Redis('192.168.2.80')
 	data = r.hgetall('dht11_garage')
@@ -24,30 +32,44 @@ def get_last_data():
 	rs['timestamp'] = last_time
 	return rs
 
+def make_sock():
+
+	i = 1
+	while True:
+
+		try:
+			sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+			sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+			sock.bind(('', MCAST_PORT))  # use MCAST_GRP instead of '' to listen only
+                             # to MCAST_GRP, not all groups on MCAST_PORT
+			mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
+
+			sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+			break
+
+		except:
+			traceback.print_exc()
+			time.sleep(5)
+			dbprint('Attempt %d to bind' % i)
+			i += 1
+
+	return sock
+
 if __name__ == '__main__':
 
-	MCAST_GRP = '239.0.0.57'
-	MCAST_PORT = 12345
-
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
-	sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-	sock.bind(('', MCAST_PORT))  # use MCAST_GRP instead of '' to listen only
-                             # to MCAST_GRP, not all groups on MCAST_PORT
-	mreq = struct.pack("4sl", socket.inet_aton(MCAST_GRP), socket.INADDR_ANY)
-
-	sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
+	sock = make_sock()
 
 	last_time = None
 	p = None
 	while True:
 		try:
-			print 'Waiting...'
+			dbprint('Waiting...')
 			data = sock.recv(1000)
 			data = json.loads(data)
-			print 'encoding:%s, value:0x%4.4x' % (data['encoding'], data['ircode'])
+			dbprint('encoding:%s, value:0x%4.4x' % (data['encoding'], data['ircode']))
 			if data['encoding'] == MODEL and data['ircode'] in IRCODE_LIST:
 				if last_time is None or last_time + 2 < time.time():
-					print 'Pass'
+					dbprint('Pass')
 					if data['ircode'] == VOL_UP:
 						subprocess.call(["amixer", "-D", "pulse", "sset", "Master", "5%+"])
 					elif data['ircode'] == VOL_DOWN:
@@ -67,14 +89,16 @@ if __name__ == '__main__':
 						elif data['ircode'] == OUTER_WEATHER:
 							text = '.\n'.join(get_weather_list('Kirrawee'))
 						if text:
-							cmdlst = ['espeak', '-s', '150', text, '--stdout']
-							cmd = '%s | sox -t wav - -r 44100 -t wav - | aplay -v' % subprocess.list2cmdline(cmdlst)
-							print cmd
+							#cmdlst = ['espeak', '-s', '150', text, '--stdout']
+							#cmd = '%s | sox -t wav - -r 44100 -t wav - | aplay -v' % subprocess.list2cmdline(cmdlst)
+							cmdlst = ['echo', text]
+							cmd = '%s | text2wave -f 44100 |aplay -v' % subprocess.list2cmdline(cmdlst)
+							dbprint(cmd)
 							p = subprocess.Popen(cmd, shell=True)
 				else:
-					print 'Too soon'
+					dbprint('Too soon')
 			else:
-				print 'Wrong code'
+				dbprint('Wrong code')
 		except Exception, e:
 			if isinstance(e, KeyboardInterrupt):
 				raise
