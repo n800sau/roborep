98ede1706c1d696a38651399cdb2d4d287888403
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-03-28 22:17:36 +0000

    test.sh added to ScareCrowWater

---
+++ trunk/ard_esp8266/ScareCrowWater/test.sh
@@ -0,0 +1,8 @@
+curl http://scarecrowwater.local
+echo
+curl http://scarecrowwater.local/scare
+echo
+curl http://scarecrowwater.local/tilt?pos=30
+echo
+curl http://scarecrowwater.local/pan?pos=90
+echo
