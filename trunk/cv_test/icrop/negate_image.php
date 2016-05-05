0c911bc9f4dce8b60edf981fd0ff9e057a7d3310
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-05-05 06:58:11 +0000

    detector save works

---
+++ trunk/cv_test/icrop/negate_image.php
@@ -0,0 +1,26 @@
+<?php
+
+	$SRCPATH = 'images';
+	$NEGATIVEPATH = 'negatives';
+
+	$data = file_get_contents("php://input");
+	$data = json_decode($data, TRUE);
+	$fpath = $SRCPATH . '/' . $data['bname'];
+	$itype = exif_imagetype($fpath);
+	if($itype) {
+		$npath = $NEGATIVEPATH . '/' . $data['bname'];
+		$negative = file_exists($npath);
+		if($data['negative'] && !$negative) {
+			// create link
+			symlink(realpath($fpath), $npath);
+		} elseif($negative && !$data['negative']) {
+			// remove link
+			unlink($npath);
+		}
+		echo json_encode(array('negative' => file_exists($npath)));
+	} else {
+		header ("HTTP/1.1 404 Not Found");
+		die();
+	}
+
+?>
