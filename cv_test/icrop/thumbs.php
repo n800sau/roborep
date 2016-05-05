d4b0603b9aad88a05bc3298d6542559028be2b2e
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-05-05 00:57:48 +0000

    adapted to side panel view, draw rect on thumbs, recreate thumbs if drawer changed

--- cv_test/icrop/thumbs.php
+++ cv_test/icrop/thumbs.php
@@ -19,20 +19,39 @@
 			if($i >= $start) {
 				// check if thumb exists
 				$bname = basename($fpath);
+				$ftime = filemtime($fpath);
 				$thpath = $THUMBPATH . '/' . $bname;
-				$thtime = filemtime($thpath);
-				if((!$thtime) || ($thtime < filemtime($fpath))) {
+				$thtime = file_exists($thpath) ? filemtime($thpath) : null;
+				$jpath = $DRAWERSPATH . '/' . $bname . '.json';
+				if(file_exists($jpath)) {
+					$drawer = json_decode(file_get_contents($jpath));
+					$jtime = filemtime($jpath);
+				} else {
+					$drawer = array();
+					$jtime = null;
+				}
+				if(is_null($thtime) || ($thtime < $ftime) || ((!is_null($jtime)) && ($thtime < $jtime))) {
 					// make thumbnail
 					$thumb = new \Imagick($fpath);
+					$draw = new \ImagickDraw();
+					$strokeColor = new \ImagickPixel('red');
+					$draw->setStrokeColor($strokeColor);
+					$draw->setStrokeOpacity(1);
+					$draw->setFillOpacity(0);
+					$draw->setStrokeWidth(5);
+					// draw rects
+					foreach($drawer as $rect) {
+						$draw->rectangle($rect->x1, $rect->y1, $rect->x2, $rect->y2);
+					}
+					$thumb->drawImage($draw);
 					$thumb->resizeImage($TH_WIDTH, $TH_HEIGHT, imagick::INTERPOLATE_BICUBIC, -1);
 					$thumb->writeImage($thpath);
 				}
-				$jpath = $DRAWERSPATH . '/' . $bname . '.json';
 				$flist[] = array(
 					'thumb' => $thpath,
 					'image' => $fpath,
 					'bname' => $bname,
-					'drawer' => file_exists($jpath) ? json_decode(file_get_contents($jpath)) : array()
+					'drawer' => $drawer
 				);
 				$count--;
 				if($count <= 0) {
