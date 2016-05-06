707ba448418c131c8ade4383cda67e01ed91f64f
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-05-06 01:44:56 +0000

    more debugging

--- trunk/cv_test/icrop/train.py
+++ trunk/cv_test/icrop/train.py
@@ -11,24 +11,46 @@ print("[INFO] gathering images and bounding boxes...")
 options = dlib.simple_object_detector_training_options()
 options.be_verbose =True
 #options.num_threads = 4
+options.add_left_right_image_flips = True;
 options.epsilon = 0.01
 options.detection_window_size = 1600
 print('size: %s' % options.detection_window_size)
 
+max_n = 100
+aratio = None
 images = []
 boxes = []
+skipped = 0
 for imPath in paths.list_images(os.path.join(BASEPATH, 'images')):
 
 	bname = os.path.basename(imPath)
 	dPath = os.path.join(BASEPATH, 'drawers', bname + '.json')
 	if os.path.exists(dPath):
-		images.append(io.imread(imPath))
-		bb = [dlib.rectangle(left=long(b['x1']), top=long(b['y1']), right=long(b['x2']), bottom=long(b['y2'])) for b in json.load(file(dPath))]
-		boxes.append(bb)
+		print('%s' % bname)
+		bb = []
+		for b in json.load(file(dPath)):
+			b = dlib.rectangle(left=long(b['x1']), top=long(b['y1']), right=long(b['x2']), bottom=long(b['y2']))
+			b_aratio = float(b.width())/b.height()
+			print('size:%dx%d aspect:%.2f area:%d' % (b.width(), b.height(), b_aratio, b.area()))
+			if aratio is None:
+				aratio = b_aratio
+			else:
+				if (aratio >= 1 and b_aratio < 1) or (aratio < 1 and b_aratio >= 1):
+					b = None
+			if not b is None:
+				bb.append(b)
+		if bb:
+			images.append(io.imread(imPath))
+			boxes.append(bb)
+			if len(images) >= max_n:
+				break
+		else:
+			skipped += 1
+
+print("[INFO] Using %d images (%d boxes) to train. Skipped %d images" % (len(images), len(boxes), skipped))
 
 print("[INFO] training detector...")
 detector = dlib.train_simple_object_detector(images, boxes, options)
-print('detector:%s' % detector)
 print("[INFO] dumping classifier to file...")
 detector.save(os.path.join(BASEPATH, 'detector', 'simple.svm'))
 
