43f580754650c002113951b32adb07a42cbeb3e5
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-05-06 04:52:55 +0000

    web gallery added

---
+++ cv_test/gallery/vars.php
@@ -0,0 +1,10 @@
+<?php
+
+	$BASEPATH = 'data';
+	$ARATIO = 4./3;
+	$SRCPATH = $BASEPATH . '/images';
+	$THUMBPATH = $BASEPATH . '/thumbs';
+	$TH_WIDTH = 64;
+	$TH_HEIGHT = $TH_WIDTH  / $ARATIO;
+
+?>
