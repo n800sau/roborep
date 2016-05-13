ca925032241f0f61baf20279e928db7082603bf6
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-05-13 01:41:16 +0000

    labels toggle added

--- cv_test/icrop/vars.php
+++ cv_test/icrop/vars.php
@@ -6,7 +6,14 @@
 	$THUMBPATH = $BASEPATH . '/thumbs';
 	$NEGATIVEPATH = $BASEPATH . '/negatives';
 	$DRAWERSPATH = $BASEPATH . '/drawers';
+	$LABELSPATH = $BASEPATH . '/labels';
 	$TH_WIDTH = 64;
 	$TH_HEIGHT = $TH_WIDTH  / $ARATIO;
+
+	$ALL_LABELS = array();
+	foreach(glob($LABELSPATH . '/*') as $lpath) {
+		$ALL_LABELS[] = basename($lpath);
+	}
+
 
 ?>
