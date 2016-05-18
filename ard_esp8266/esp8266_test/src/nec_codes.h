b320c2f3312a12cf81bc1a738045235603f06993
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-05-18 03:40:25 +0000

    led on, led off via multicast udp

---
+++ ard_esp8266/esp8266_test/src/nec_codes.h
@@ -0,0 +1,14 @@
+const char MODEL[] = "NEC";
+
+const int
+	TURN_OFF = 0xfd9a65,
+	// 1
+	OWN_WEATHER = 0xfd4ab5,
+	// 2
+	OUTER_WEATHER = 0xfd0af5,
+	VOL_UP = 0xfdb04f,
+	VOL_DOWN = 0xfd8877,
+	// 0
+	WATER_SPIT = 0xfdf00f,
+	LED_ON = 0xfdb04f,
+	LED_OFF = 0xfd8877;
