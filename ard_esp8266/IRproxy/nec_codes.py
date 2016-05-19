b63e0414f20de3425551ea88f447ce4f28e9115a
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-05-19 02:33:09 +0000

    more codes

--- ard_esp8266/IRproxy/nec_codes.py
+++ ard_esp8266/IRproxy/nec_codes.py
@@ -10,5 +10,42 @@ VOL_UP = 0xfdb04f
 VOL_DOWN = 0xfd8877
 # 0
 WATER_SPIT = 0xfdf00f
+
+RES  = 0xfd1ae5
+RATIO = 0xfd18e7
+MUTE = 0xfd9867
+CH_PLUS = 0xfdb24d
+CH_MINUS = 0xfd8a75
+TV_RADIO = 0xfd708f
+PAUSE = 0xfd48b7
+INFO = 0xfda25d
+RECALL = 0xfda05f
+
+UP = 0xfd609f
+DOWN = 0xfd6897
+LEFT = 0xfd5aa5
+RIGHT = 0xfdd827
+OK = 0xfd58a7
+
+MENU = 0xfdaa55
+EXIT = 0xfda857
+IR_1 = 0xfd4ab5
+IR_2 = 0xfd0af5
+IR_3 = 0xfd08f7
+IR_4 = 0xfd6a95
+IR_5 = 0xfd2ad5
+IR_6 = 0xfd28d7
+IR_7 = 0xfd728d
+IR_8 = 0xfd32cd
+IR_9 = 0xfd30cf
+IR_epg = 0xfd52ad
+IR_ttx = 0xfd12ed
+IR_sub = 0xfd10ef
+IR_opt = 0xfdd02f
+RED = 0xfd629d
+IR_GREEN = 0xfd22dd
+IR_YELLOW = 0xfd20df
+IR_BLUE = 0xfde01f
+
 IRCODE_LIST = (OWN_WEATHER, OUTER_WEATHER, VOL_UP, VOL_DOWN, WATER_SPIT)
 
