eccaf6751497eb6606cbd2fef9e95a889750a682
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-06-15 01:49:04 +0000

    android command line with key

--- android/RoboSensorView/build.sh
+++ android/RoboSensorView/build.sh
@@ -1,8 +1,5 @@
-<<<<<<< .mine
 ant debug && sudo cp -v bin/RoboSensorView-debug.apk /var/www/android
 #ant debug && adb install -r bin/RoboSensorView-debug.apk
-=======
-ant debug && cp -v bin/RoboSensorView-debug.apk /var/www/android
-#ant debug && adb install -r bin/RoboSensorView-debug.apk
->>>>>>> .r732
+
+#ant release
 
