b997cfbd1398e11c4ec5b15d03df856af82e764d
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-07-18 06:44:15 +0000

    make_cropped added

---
+++ cv_test/icrop/make_cropped.py
@@ -0,0 +1,20 @@
+import os, cv2, json
+
+BASEPATH = 'data'
+DRAWERS = os.path.join(BASEPATH, 'drawers')
+IMAGES = os.path.join(BASEPATH, 'images')
+OUTPUT = os.path.join(BASEPATH, 'cropped')
+
+coef = 1920 / 200.
+for root, dirs, fnames in os.walk(DRAWERS, followlinks=True):
+	for fname in fnames:
+		name,ext = os.path.splitext(fname)
+		if ext == '.json':
+			ifname = os.path.join(IMAGES, name)
+			iname,iext = os.path.splitext(name)
+			image = cv2.imread(ifname)
+			for i, area in enumerate(json.load(file(os.path.join(root, fname)))):
+				cropped = image[area['y1']*coef:area['y2']*coef, area['x1']*coef:area['x2']*coef]
+				cfname = os.path.join(OUTPUT, '%s.%d%s' % (iname, i, iext))
+				print(cfname, cropped.shape)
+				cv2.imwrite(cfname, cropped)
--- cv_test/icrop/make_hist_chart.py
+++ cv_test/icrop/make_hist_chart.py
@@ -3,27 +3,31 @@ from imutils import paths, resize
 from matplotlib import pyplot as plt
 
 BASEPATH = 'data'
-IMAGES = os.path.join(BASEPATH, 'images')
+IMAGES = os.path.join(BASEPATH, 'cropped')
 HISTOGRAMS = os.path.join(BASEPATH, 'histograms')
 
 if not os.path.exists(HISTOGRAMS):
 	os.makedirs(HISTOGRAMS)
 
 for fname in paths.list_images(IMAGES):
-	image = cv2.imread(fname)
+	image = resize(cv2.imread(fname), width=100, height=100)
 	hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 	h,s,v = cv2.split(image)
 	hist = cv2.calcHist([h], [0], None, [256], [0, 256])
-	hist /= hist.sum()
+#	hist /= hist.max()
 	fig = plt.figure(figsize=(4.5, 1.8))
 	ax = fig.add_subplot(121)
 	ax.set_title("Histogram")
 	ax.set_xlabel("Bins")
 	ax.set_ylabel("# of Pixels")
+	ax.set_ylim([0, 300])
 	ax.plot(hist)
 	ax.set_xlim([0, 256])
 	ax = fig.add_subplot(122)
 	ax.imshow(cv2.cvtColor(resize(image, width=180), cv2.COLOR_BGR2RGB), cmap=plt.cm.gray_r, interpolation="nearest")
+#	ax.imshow(cv2.cvtColor(resize(image, width=180), cv2.COLOR_BGR2RGB), cmap=plt.cm.gray_r, interpolation="nearest")
 	dfname = os.path.join(HISTOGRAMS, os.path.basename(fname))
 	plt.savefig(dfname, dpi = (50))
 	plt.close(fig)
+
+print 'Finished'
