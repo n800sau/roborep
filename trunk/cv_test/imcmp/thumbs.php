e88e056f2dce1fb0e2644963d151bfe894186a73
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-07-18 06:11:14 +0000

    imcmp added

---
+++ trunk/cv_test/imcmp/thumbs.php
@@ -0,0 +1,114 @@
+<?php
+
+	include 'vars.php';
+
+	function mkthumb($fpath, $thpath) {
+		global $TH_WIDTH, $TH_HEIGHT;
+		if(file_exists($fpath)) {
+			$ftime = filemtime($fpath);
+			$thtime = file_exists($thpath) ? filemtime($thpath) : NULL;
+			if(is_null($thtime) || ($thtime < $ftime)) {
+				// make thumbnail
+				$thumb = new \Imagick($fpath);
+//		error_log($TH_WIDTH . ',' . $TH_HEIGHT);
+				$thumb->resizeImage($TH_WIDTH, $TH_HEIGHT, imagick::INTERPOLATE_BICUBIC, -1);
+				$thumb->writeImage($thpath);
+			}
+		}
+	}
+
+	$start = isset($_GET["start"]) ? (int)$_GET["start"] : 0;
+	$count = isset($_GET["count"]) ? (int)$_GET["count"] : 5;
+
+	$flist = array();
+
+	$i = 0;
+
+	class FilesOnlyFilter extends RecursiveFilterIterator
+	{
+		public function accept()
+		{
+			$iterator = $this->getInnerIterator();
+
+			// allow traversal
+			if ($iterator->hasChildren()) {
+				return true;
+			}
+
+			// filter entries, only allow true files
+			return $iterator->current()->isFile();
+		}
+	}
+
+	class VisibleOnlyFilter extends RecursiveFilterIterator
+	{
+		public function accept()
+		{
+			$fileName = $this->getInnerIterator()->current()->getFileName();
+			$firstChar = $fileName[0];
+			return $firstChar !== '.';
+		}
+	}
+
+	$fileinfos = new RecursiveIteratorIterator(
+		new FilesOnlyFilter(
+			new VisibleOnlyFilter(
+				new RecursiveDirectoryIterator($SRCPATH1, FilesystemIterator::UNIX_PATHS)
+			)
+		),
+		RecursiveIteratorIterator::LEAVES_ONLY 
+	);
+
+	foreach ($fileinfos as $fpath => $fileinfo) {
+		if(is_dir($fpath)) {
+			$subdirname = dirname($fileinfos->getSubPathname());
+			$bname = basename($fileinfos->getSubPathname());
+			echo $fpath . ', ' . $subdirname . ', ' . $bname . "\n";
+		}
+	}
+
+	foreach ($fileinfos as $dname)
+	{
+		$fpath1 = $dname->getPathname();
+		$itype = exif_imagetype($fpath1);
+		if($itype) {
+			if($i >= $start) {
+				// check if thumb exists
+				$subfname = $fileinfos->getSubPathname();
+				$bname = basename($subfname);
+				$thpath1 = $THUMBPATH1 . '/' . $subfname;
+				mkthumb($fpath1, $thpath1);
+				$fpath2 = $SRCPATH2 . '/' . $subfname;
+				$thpath2 = $THUMBPATH2 . '/' . $subfname;
+				mkthumb($fpath2, $thpath2);
+
+				$bname = basename($subfname);
+				$flist[] = array(
+					'img1' => array(
+						'thumb' => $thpath1,
+						'image' => $fpath1,
+						'bname' => $bname,
+						'dname' => $subfname,
+					),
+					'img2' => array(
+						'thumb' => $thpath2,
+						'image' => $fpath2,
+						'bname' => $bname,
+						'dname' => $subfname,
+					),
+				);
+				$count--;
+				if($count <= 0) {
+					break;
+				}
+			}
+			$i++;
+		}
+		if($count <= 0) {
+			break;
+		}
+	}
+
+	echo json_encode(array('data' => $flist, 'first' => $start, 'last' => $i));
+
+?>
