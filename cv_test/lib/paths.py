6a32e9e262775e10ae9504f27ab264f7a7c32647
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-07-19 00:32:18 +0000

    fixed list_dirs

--- cv_test/lib/paths.py
+++ cv_test/lib/paths.py
@@ -6,11 +6,12 @@ def list_dirs(root_dir, subdir='.', real_paths=None):
 	if real_paths is None:
 		real_paths = []
 	real_path = os.path.realpath(bpath)
-	if real_path not in real_paths:
+	if real_path not in real_paths and os.path.isdir(real_path):
 		real_paths.append(real_path)
+#		print real_path
+		rs.append(subdir)
 		for d in os.listdir(real_path):
-			if os.path.isdir(os.path.join(real_path, d)):
-				rs += [os.path.join(subdir, dr) for dr in list_dirs(root_dir, os.path.join(subdir, d), real_paths)]
+			rs += list_dirs(root_dir, os.path.join(subdir, d), real_paths)
 	return rs
 
 if __name__ == '__main__':
