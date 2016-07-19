d89606aad1f33dce0cf218df4ec6e9e470b932e4
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-07-19 02:54:44 +0000

    fixed image hsv bug and such

--- cv_test/imcmp/img1_to_img2.py
+++ cv_test/imcmp/img1_to_img2.py
@@ -23,9 +23,18 @@ def process_image(fname, in_dir, out_dir, out_hist_dir):
 	image = cv2.imread(os.path.join(in_dir, fname))
 
 	hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-	h,s,v = cv2.split(image)
-	hist = cv2.calcHist([h], [0], None, [256], [0, 256])
-#	hist /= hist.max()
+	h,s,v = cv2.split(hsv)
+#	lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+#	l,a,b = cv2.split(lab)
+
+	eroded = cv2.erode(h.copy(), None, iterations=10)
+	eroded = cv2.merge([eroded, s, v])
+	eroded = cv2.cvtColor(eroded, cv2.COLOR_HSV2BGR)
+	cv2.imwrite(os.path.join(out_dir, fname), eroded)
+
+
+	hist = cv2.calcHist([eroded], [0], None, [256], [0, 256])
+	hist /= hist.max()
 
 	fig = plt.figure(figsize=(4.5, 1.8))
 	ax = fig.add_subplot(111)
@@ -41,10 +50,6 @@ def process_image(fname, in_dir, out_dir, out_hist_dir):
 
 #	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-	# apply a series of erosions
-	for i in xrange(5, 10, 5):
-		eroded = cv2.erode(h.copy(), None, iterations=i + 1)
-		cv2.imwrite(os.path.join(out_dir, fname), eroded)
 
 for subdir in r_list_dirs(in_dir):
 
