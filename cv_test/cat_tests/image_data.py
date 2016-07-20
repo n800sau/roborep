98a6b0231fc70a4708fec673a692063bea5f1d73
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-07-20 03:05:59 +0000

    saturatiion added to features

--- cv_test/cat_tests/image_data.py
+++ cv_test/cat_tests/image_data.py
@@ -6,6 +6,8 @@ def extract_image_data(image):
 #	image = cv2.resize(image, (IMG_SIDE, IMG_SIDE))
 	hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 	h,s,v = cv2.split(hsv)
-	hist = cv2.calcHist([h], [0], None, [256], [0, 256])
+	hist = cv2.calcHist([h, s], [0, 1], None, [180, 256], [0, 180, 0, 256])
+#	print 'SHAPE', hist.shape
 	hist /= hist.max()
-	return hist.reshape(16, 16)
+	return hist
+#	return hist.reshape(16, 16)
