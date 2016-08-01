4335487c3b77396572632a957d988914fee0ff05
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-08-01 23:58:45 +0000

    working detect_label.py

--- cv_test/garage/detect_label.py
+++ cv_test/garage/detect_label.py
@@ -17,6 +17,7 @@ MODELLIST = ['inside_empty', 'open_close']
 
 MODELPATH = 'models'
 REDIS_INPUT_LIST = 'garage_files2label'
+REDIS_FAIL_LIST = 'garage_failed_files'
 REDIS_OUTPUT_PREFIX = 'garage_label_'
 NO_LABEL = '_'
 NO_LABEL_QUEUE_PREFIX = 'no_label_'
@@ -46,6 +47,7 @@ def detect_image_label(model, ftp_h, fpath):
 	return (detect_label(model, image), imgdata)
 
 #00D6FB009223(n800sau)_1_20160516142921_30928.jpg
+failed_file = ''
 ftp_h = None
 models = None
 r = re.compile('^[0-9A-F]+\(.*\)_\d_(\d+)_\d+\.jpg')
@@ -65,56 +67,55 @@ try:
 					ftp_h = FTP('192.168.1.1')
 					ftp_h.login('writer', 'pfgbcm')
 					ftp_h.cwd('rus_hard/garage')
-				if not ftp_h.size(fpath) is None:
-					if models is None:
-						t = time.time()
-						models = {}
-						for mname in MODELLIST:
-							models[mname] = cPickle.loads(open(os.path.join(MODELPATH, mname + '.svc')).read())
-#	#						cPickle.dump(models[mname], open(os.path.join(MODELPATH, mname + '.svc.new'), 'w'), protocol=cPickle.HIGHEST_PROTOCOL)
-						tdiff = int(time.time() - t)
-						print 'models load time: %d:%d' % (tdiff//60, tdiff%60)
-					msglist = []
-					labellist = []
-					for mname,model in models.items():
-						output_name = REDIS_OUTPUT_PREFIX + mname
-						print bname,
-						label,imgdata = detect_image_label(model, ftp_h, fpath)
-						if label == NO_LABEL:
-							queue_pfx = NO_LABEL_QUEUE_PREFIX + mname
-							redis.rpush(queue_pfx, bname)
-							redis.ltrim(queue_pfx, max(0, redis.llen(queue_pfx) - 100), -1)
-						elif label in ('open', 'close'):
-							redis.set('gate', json.dumps({'label': label, 'ts': time.time()}))
-						if label != NO_LABEL:
-							last_rec = redis.lrange(output_name, -1, -1)
-							if last_rec:
-								last_rec = json.loads(last_rec[0])
-								if last_rec['ts'] < ts and last_rec['label'] != label:
-									msg = '%s changed at %s from %s to %s (diff=%d), %s' % (mname, dt.strftime('%d/%m %H:%M:%S'), last_rec['label'], label, ts - last_rec['ts'], bname)
-									print msg
-									msglist.append(msg)
-									labellist.append((mname, label))
-							else:
-								msg = 'Initial at %s %s' % (dt.strftime('%d/%m %H:%M:%S'), label)
+				if models is None:
+					t = time.time()
+					models = {}
+					for mname in MODELLIST:
+						models[mname] = cPickle.loads(open(os.path.join(MODELPATH, mname + '.svc')).read())
+#						cPickle.dump(models[mname], open(os.path.join(MODELPATH, mname + '.svc.new'), 'w'), protocol=cPickle.HIGHEST_PROTOCOL)
+					tdiff = int(time.time() - t)
+					print 'models load time: %d:%d' % (tdiff//60, tdiff%60)
+				msglist = []
+				labellist = []
+				for mname,model in models.items():
+					output_name = REDIS_OUTPUT_PREFIX + mname
+					print bname,
+					label,imgdata = detect_image_label(model, ftp_h, fpath)
+					if label == NO_LABEL:
+						queue_pfx = NO_LABEL_QUEUE_PREFIX + mname
+						redis.rpush(queue_pfx, bname)
+						redis.ltrim(queue_pfx, max(0, redis.llen(queue_pfx) - 100), -1)
+					elif label in ('open', 'close'):
+						redis.set('gate', json.dumps({'label': label, 'ts': time.time()}))
+					if label != NO_LABEL:
+						last_rec = redis.lrange(output_name, -1, -1)
+						if last_rec:
+							last_rec = json.loads(last_rec[0])
+							if last_rec['ts'] < ts and last_rec['label'] != label:
+								msg = '%s changed at %s from %s to %s (diff=%d), %s' % (mname, dt.strftime('%d/%m %H:%M:%S'), last_rec['label'], label, ts - last_rec['ts'], bname)
 								print msg
 								msglist.append(msg)
-								labellist.append(label)
-							print
-							redis.rpush(output_name, json.dumps({'label': label, 'ts': ts, 'name': fpath}))
-							redis.ltrim(output_name, max(0, redis.llen(output_name) - 100), -1)
-					if msglist:
-						labellist = [label for mname,label in labellist if label != NO_LABEL]
-						if not labellist:
-							labellist = ['_']
-						send_email('itmousecage@gmail.com', '%s: %s' % (dt.strftime('%H:%M:%S %d/%m'), ','.join(labellist)), '\n'.join(msglist), [imgdata])
+								labellist.append((mname, label))
+						else:
+							msg = 'Initial at %s %s' % (dt.strftime('%d/%m %H:%M:%S'), label)
+							print msg
+							msglist.append(msg)
+							labellist.append(label)
+						print
+						redis.rpush(output_name, json.dumps({'label': label, 'ts': ts, 'name': fpath}))
+						redis.ltrim(output_name, max(0, redis.llen(output_name) - 100), -1)
+				if msglist:
+					labellist = [label for mname,label in labellist if label != NO_LABEL]
+					if not labellist:
+						labellist = ['_']
+					send_email('itmousecage@gmail.com', '%s: %s' % (dt.strftime('%H:%M:%S %d/%m'), ','.join(labellist)), '\n'.join(msglist), [imgdata])
 			except:
 				# return fpath back to redis list
-				redis.lpush(REDIS_INPUT_LIST, fpath)
+				redis.rpush(REDIS_FAIL_LIST, fpath)
+				failed_file = fpath
 				raise
-				pass
 except Exception, e:
-	send_email('itmousecage@gmail.com', 'Error occured: %s' % str(e), 'Error details: %s' % traceback.format_exc())
+	send_email('itmousecage@gmail.com', '%s error occured: %s' % (failed_file, str(e)), 'Error details: %s' % traceback.format_exc())
 	pass
 
 if not ftp_h is None:
