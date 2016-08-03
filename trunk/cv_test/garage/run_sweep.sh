b8db0b74e5448b0f614bcb676c80f8ea1f3b3109
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-08-03 00:36:41 +0000

    added already found

--- trunk/cv_test/garage/run_sweep.sh
+++ trunk/cv_test/garage/run_sweep.sh
@@ -7,5 +7,18 @@ cd ~/work/roborep/cv_test/garage
 
 cur_dir=`pwd`
 
+RUNNING=`ps -Af|grep run_sweep.py|grep python|grep -v grep|awk '{print $2}'`
+
+if [ -z "$RUNNING" ]
+then
+
+
+
 /usr/bin/env python "${cur_dir}/run_sweep.py" &>> ${cur_dir}/run_sweep.log
 echo $?
+
+else
+
+echo '###Found already running###'|tee -a ${cur_dir}/run_sweep.log
+
+fi
