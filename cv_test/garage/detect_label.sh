e55d53a532a523954bc094ace972bace13200fbe
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-08-03 00:13:11 +0000

    print to log if detect_label.py is already running

--- cv_test/garage/detect_label.sh
+++ cv_test/garage/detect_label.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 
-RUNNING=`ps -Af|grep detect_label.py|grep python|grep -v grep|awk '{print $2}'`
-
-if [ -z "$RUNNING" ]
-then
-
 source ~/.virtualenvs/gurus/bin/activate
 
 cd ~/work/roborep/cv_test/garage
 
 cur_dir=`pwd`
 
+RUNNING=`ps -Af|grep detect_label.py|grep python|grep -v grep|awk '{print $2}'`
+
+if [ -z "$RUNNING" ]
+then
+
 /usr/bin/env python "${cur_dir}/detect_label.py" 1>> ${cur_dir}/detect_label.log 2>> ${cur_dir}/detect_label_error.log
 echo $?
 
 else
 
-echo 'Found already running'
+echo '###Found already running###'|tee -a ${cur_dir}/detect_label_error.log
 
 fi
