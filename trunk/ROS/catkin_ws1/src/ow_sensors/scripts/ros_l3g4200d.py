ea8730ee79867bcd7c0f7cf88c8a11b19cbead3a
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-09-21 05:17:03 +0000

    Rate in use and launcher to run them all

--- trunk/ROS/catkin_ws1/src/ow_sensors/scripts/ros_l3g4200d.py
+++ trunk/ROS/catkin_ws1/src/ow_sensors/scripts/ros_l3g4200d.py
@@ -14,15 +14,17 @@ if __name__ == '__main__':
 		rospy.loginfo("L3G4200 on address 0x%x" % (gyro.address))
 		# Ordered this way to minimize wait time.
 		rospy.init_node('l3g4200', anonymous = True)
+		r = rospy.Rate(0.01)
 		pub_degsec = rospy.Publisher('/sensors/l3g4200/degsec', Vector3, queue_size=3)
 		pub_t = rospy.Publisher('/sensors/l3g4200/t', Float32, queue_size=3)
 		while not rospy.is_shutdown():
-			st = gyro.bus.read_byte_data(gyro.address, gyro.STATUS_REG)
-			if st:
-				degsec = gyro.getDegPerSecAxes()
-				t = gyro.getDieTemperature()
-				pub_degsec.publish(Vector3(*degsec))
-				pub_t.publish(Float32(t))
-			rospy.sleep(0.01)
+			if pub_t.get_num_connections() > 0 or pub_degsec.get_num_connections() > 0:
+				st = gyro.bus.read_byte_data(gyro.address, gyro.STATUS_REG)
+				if st:
+					degsec = gyro.getDegPerSecAxes()
+					t = gyro.getDieTemperature()
+					pub_degsec.publish(Vector3(*degsec))
+					pub_t.publish(Float32(t))
+			r.sleep()
 	except rospy.ROSInterruptException:
 		pass
