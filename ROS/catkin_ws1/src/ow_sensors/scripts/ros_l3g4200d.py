7b67949371db93904c1050b1c017c0e2874a071d
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-09-21 05:39:54 +0000

    sensors Rate fixed

--- ROS/catkin_ws1/src/ow_sensors/scripts/ros_l3g4200d.py
+++ ROS/catkin_ws1/src/ow_sensors/scripts/ros_l3g4200d.py
@@ -14,7 +14,7 @@ if __name__ == '__main__':
 		rospy.loginfo("L3G4200 on address 0x%x" % (gyro.address))
 		# Ordered this way to minimize wait time.
 		rospy.init_node('l3g4200', anonymous = True)
-		r = rospy.Rate(0.01)
+		r = rospy.Rate(10)
 		pub_degsec = rospy.Publisher('/sensors/l3g4200/degsec', Vector3, queue_size=3)
 		pub_t = rospy.Publisher('/sensors/l3g4200/t', Float32, queue_size=3)
 		while not rospy.is_shutdown():
