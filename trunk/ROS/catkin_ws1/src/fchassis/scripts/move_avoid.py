7be38be7591d200fd3b0648706df42d2b999d498
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-10-03 23:42:20 +0000

    chassis update

--- trunk/ROS/catkin_ws1/src/fchassis/scripts/move_avoid.py
+++ trunk/ROS/catkin_ws1/src/fchassis/scripts/move_avoid.py
@@ -7,29 +7,51 @@ from fchassis import msg
 
 class MoveAvoid:
 
+	def __init__(self):
+		self.direction = None
+		self.change_time = None
+
 	def run(self):
 		rospy.init_node('move_avoid', anonymous = True)
-		rospy.Subscriber("/fchassis/state", msg.state, self.on_state)
+#		rospy.Subscriber("/fchassis/state", msg.state, self.on_state)
 		self.pub_command = rospy.Publisher('/fchassis/command', msg.command, queue_size=1)
-		rospy.spin()
+		rospy.sleep(10)
+		self.pub_command.publish(command='mboth', lpwm=40, lfwd=False, rpwm=40, rfwd=False)
+		rospy.sleep(10)
+		self.pub_command.publish(command='walk_around', lpwm=50, rpwm=50)
+		rospy.loginfo('Start walking')
+		rospy.sleep(60)
+		self.pub_command.publish(command='mstop')
+		rospy.loginfo('Stop')
+#		rospy.signal_shutdown("Walk around shutdown")
 
 	def on_state(self, data):
 		if data.sonar > 0:
 			if data.sonar < 0.5:
 				# back
-				self.pub_command.publish(command='mboth', lpwm=40, lfwd=True, rpwm=40, rfwd=True)
-				rospy.sleep(0.2)
+				self.direction = 'back'
+				self.pub_command.publish(command='mboth', lpwm=40, lfwd=False, rpwm=40, rfwd=False)
+				rospy.loginfo('Back')
+				rospy.sleep(2)
 				if random.choice([True, False]):
-					self.pub_command.publish(command='mright', rpwm=50, rfwd=True)
-					rospy.sleep(0.2)
+					self.direction = 'right'
+					self.pub_command.publish(command='mright', rpwm=50, rfwd=False)
+					rospy.loginfo('Right')
+					rospy.sleep(2)
 				else:
-					self.pub_command.publish(command='mleft', lpwm=50, lfwd=True)
-					rospy.sleep(0.2)
+					self.direction = 'left'
+					self.pub_command.publish(command='mleft', lpwm=50, lfwd=False)
+					rospy.loginfo('Left')
+					rospy.sleep(2)
 			else:
 				# forward
-				self.pub_command.publish(command='mboth', lpwm=50, lfwd=False, rpwm=50, rfwd=False)
-				rospy.sleep(0.2)
+				self.direction = 'forward'
+				self.pub_command.publish(command='mboth', lpwm=50, lfwd=True, rpwm=50, rfwd=True)
+				rospy.loginfo('Forward')
+				rospy.sleep(2)
 		else:
+			rospy.loginfo('Stop')
+			self.direction = 'stop'
 			self.pub_command.publish(command='mstop')
 
 
