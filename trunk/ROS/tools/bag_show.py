7cdd1c7c30649c332015051d899bdb8dc98ed525
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-10-03 23:41:39 +0000

    bag tools updated

--- trunk/ROS/tools/bag_show.py
+++ trunk/ROS/tools/bag_show.py
@@ -6,8 +6,8 @@ import time
 tlist = {
 	'/sensors/l3g4200/axes':  '   gyro',
 	'/sensors/adxl345/axes':  '        acc',
-	'/sensors/hmc5883l/axes': '            compass',
-	'/fchassis/state':        '                    sonar',
+	'/sensors/hmc5883l/heading': '            compass',
+	'/fchassis/state':        '                    state',
 	'/fchassis/command':      '                          command',
 }
 
@@ -22,7 +22,17 @@ with open(bagfname + '.csv', 'w+') as csvfile:
 
 	i = 0
 	for topic, msg, t in bag.read_messages(topics=tlist.keys()):
-		fcsv.writerow([t.secs, t.nsecs, tlist[topic]])
+		name = tlist[topic]
+		vals = []
+		if name.strip() == 'state':
+			vals = ['%.2f' % msg.sonar]
+		elif name.strip() == 'compass':
+			vals = ['%d' % msg.heading]
+		elif name.strip() == 'command':
+			vals = [msg.command]
+		elif name.strip() in ('acc', 'gyro'):
+			vals = ['%d' % msg.vector.x , '%d' % msg.vector.y, '%d' % msg.vector.z]
+		fcsv.writerow([t.secs, t.nsecs, name] + vals)
 		i += 1
 #		if i > 300:
 #			break
--- trunk/ROS/tools/bagcsv_plot.py
+++ trunk/ROS/tools/bagcsv_plot.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 
+import matplotlib
+matplotlib.use('Agg')
 import sys, os, csv
 import datetime as dt
 import matplotlib.pyplot as plt
@@ -8,27 +10,67 @@ from matplotlib.lines import Line2D
 
 fname = sys.argv[1]
 
-tops = []
-
 x = []
-y = []
+ysonar = []
+ycommand = []
+ycompass = []
+ygyro = [[], [], []]
 
-marker_style = dict(linestyle=':', color='cornflowerblue', markersize=10)
+# start with the first command
+# stop after 10 secs after the mstop command unless another command occurs
 
+active = False
+stopsecs = None
 i = 0
 with open(fname) as csvfile:
 	for r in csv.reader(csvfile, delimiter = ','):
-		x.append(int(r[0]) + float(r[1]) / 1000000000)
-		if r[2] not in tops:
-			tops.append(r[2])
-		y.append(tops.index(r[2]))
-		i += 1
-		if i > 100:
-			break
+		name = r[2].strip()
+		if not active:
+			if name == 'command':
+				active = True
+		if active:
+			secs = int(r[0])
+			x.append(secs + float(r[1]) / 1000000000)
+			ycompass.append(r[3] if name == 'compass' else None)
+			ysonar.append(r[3] if name == 'state' else None)
+			if name == 'gyro':
+				ygyro[0].append(r[3])
+				ygyro[1].append(r[4])
+				ygyro[2].append(r[5])
+			else:
+				ygyro[0].append(None)
+				ygyro[1].append(None)
+				ygyro[2].append(None)
+			if name == 'command':
+				if r[3] == 'mstop':
+					ycommand.append(1)
+					stopsecs = secs + 10
+				elif not stopsecs is None:
+					ycommand.append(2)
+					stopsecs = None
+				else:
+					ycommand.append(2)
+			else:
+				ycommand.append(None)
+			i += 1
+			if not stopsecs is None:
+				if stopsecs < secs:
+					break
+#		if i > 100:
+#			break
 
-fig, ax = plt.subplots()
-ax.plot(x, y, marker='*', **marker_style)
+fig, axes = plt.subplots(nrows=4, ncols=1, sharex=True, sharey=False)
 
-#savefig('foo.png', bbox_inches='tight')
+axes[0].plot(x, ycommand, marker='x', linestyle='', color='cornflowerblue', markersize=5)
 
-plt.show()
+axes[1].plot(x, ysonar, color='green', linestyle='-', marker='.', markersize=2)
+
+axes[2].plot(x, ycompass, color='blue', linestyle='-', marker='.', markersize=2)
+
+axes[3].plot(x, ygyro[0], color='green', linestyle='-', marker='.', markersize=2)
+axes[3].plot(x, ygyro[1], color='blue', linestyle='-', marker='.', markersize=2)
+axes[3].plot(x, ygyro[2], color='orange', linestyle='-', marker='.', markersize=2)
+
+plt.savefig(os.path.expanduser('~/public_html/bag/plot.png'), bbox_inches='tight')
+
+#plt.show()
