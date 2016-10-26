be41492356db399180fc6ddfda0adb49bdddd6d1
n800s1 <n800s1@f7cc7854-0776-44af-9a67-d73aa6a6a94f>
2016-10-26 03:02:17 +0000

    some rearrangement

--- opcplus/catkin_ws/video_rec.sh
+++ opcplus/catkin_ws/video_rec.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 #rosrun image_view video_recorder image:=/duplex_h/image_raw _filename:=out.avi &> video_rec.log
-rosrun image_view video_recorder image:=/duplex_h/image_raw _filename:=out_h.avi _codec:=XVID &> video_rec.log
+#rosrun image_view video_recorder image:=/duplex_h/image_raw _filename:=out_h.avi _codec:=XVID _fps:=2 &> video_rec.log
 #rosrun image_view video_recorder image:=/duplex/image_raw _filename:=out.avi _codec:=XVID &> video_rec.log
+
+rosrun image_view video_recorder image:=/image_raw _filename:=out.avi _codec:=XVID _fps:=2 &> video_rec.log
