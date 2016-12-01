97868f9f47f76f2d2ae808199059d0a6414635a0
Grigory Dorokhov <n800sau@gmail.com>
2016-12-01 11:41:41 +1100

    added fchassis prefix

--- nodes/hubee/catkin_ws/c_left_a_little.sh
+++ nodes/hubee/catkin_ws/c_left_a_little.sh
@@ -1,2 +1,2 @@
 #rostopic pub -1 /fchassis/command fchassis_msgs/command '{mcommand: mleft, lpwr: 50, lfwd: false, timeout: 1}'
-rosservice call /exec_command '{mcommand: mleft, lPwr: 60, lFwd: false, timeout: 4}'
+rosservice call /fchassis/exec_command '{mcommand: mleft, lPwr: 60, lFwd: false, timeout: 4}'
--- nodes/hubee/catkin_ws/c_right_a_little.sh
+++ nodes/hubee/catkin_ws/c_right_a_little.sh
@@ -1,2 +1,2 @@
 #rostopic pub -1 /fchassis/command fchassis_msgs/command '{mcommand: mright, rpwr: 80, rfwd: false, timeout: 2}'
-rosservice call /exec_command '{mcommand: mright, rPwr: 60, rFwd: false, timeout: 4}'
+rosservice call /fchassis/exec_command '{mcommand: mright, rPwr: 60, rFwd: false, timeout: 4}'
