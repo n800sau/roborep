66160001f9ac8b82dedbfc9ae24d75023fa35e73
n800sau <n800sau@mail.com>
2017-02-09 15:59:26 +1100

    multiple changes

---
+++ nodes/rpi3/catkin_ws/pub_left.sh
@@ -0,0 +1,3 @@
+rostopic pub -1 /cmd_vel geometry_msgs/Twist  '{linear:  {x: 0.0, y: 0.0, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 3.5}}'
+#sleep 1
+rostopic pub -1 /cmd_vel geometry_msgs/Twist  '{linear:  {x: 0, y: 0.0, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}'
