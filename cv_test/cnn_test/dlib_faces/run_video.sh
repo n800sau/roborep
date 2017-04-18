0500ca902d7f980b75ed9776db90e8d82bebf0cd
n800s <n800sau@gmail.com>
2017-04-18 10:18:36 +1000

    --output added and DISPLAY removed

--- cv_test/cnn_test/dlib_faces/run_video.sh
+++ cv_test/cnn_test/dlib_faces/run_video.sh
@@ -1,2 +1,2 @@
-DISPLAY=:5 python -u video_facial_landmarks.py --shape-predictor shape_predictor_68_face_landmarks.dat -i in.avi &>run_video.log
+python -u video_facial_landmarks.py --shape-predictor shape_predictor_68_face_landmarks.dat -i in.avi -o output.avi &>run_video.log
 echo $?
