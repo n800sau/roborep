55d3bc302fc88b0872b51c781a5d8082157b4bcd
n800s <n800sau@gmail.com>
2017-05-12 16:09:13 +1000

    removed DISPLAY

--- cv_test/cnn_test/dlib_faces/run_detect.sh
+++ cv_test/cnn_test/dlib_faces/run_detect.sh
@@ -5,7 +5,7 @@ for IMG in /home/n800s/sshfs/asus/root/rus_hard/n800s/mydvd/pics_sony/2015-02_02
 do
 
 echo $IMG >>run_detect.log
-time DISPLAY=:5 python -u detect_face_parts.py --shape-predictor shape_predictor_68_face_landmarks.dat --image "$IMG" &>>run_detect.log
+python -u detect_face_parts.py --shape-predictor shape_predictor_68_face_landmarks.dat --image "$IMG" &>>run_detect.log
 echo $?
 
 done
