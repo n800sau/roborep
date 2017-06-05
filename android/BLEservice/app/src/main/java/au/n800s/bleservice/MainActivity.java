a45ee1e502aa00ab201b7a749eeb850f96656605
n800s <n800sau@gmail.com>
2017-06-05 15:04:21 +1000

    added log output to mainactivity

--- android/BLEservice/app/src/main/java/au/n800s/bleservice/MainActivity.java
+++ android/BLEservice/app/src/main/java/au/n800s/bleservice/MainActivity.java
@@ -2,15 +2,19 @@ package au.n800s.bleservice;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends Activity
 {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+	private static final String TAG = "bleservice.MainActivity";
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState)
+	{
+
+		super.onCreate(savedInstanceState);
+		Log.d(TAG, "Successfully started");
+		setContentView(R.layout.main);
+	}
 }
