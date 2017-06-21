002a2cc830026c5b4be03d85a16f6151661e0c29
n800s <n800sau@gmail.com>
2017-06-21 11:03:47 +1000

    simplification of bleservice

--- android/BLEService/app/src/main/java/n800s/bleservice/BLEService.java
+++ android/BLEService/app/src/main/java/n800s/bleservice/BLEService.java
@@ -6,10 +6,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Binder;
 import android.os.IBinder;
-import android.widget.Toast;
+//import android.widget.Toast;
 import android.util.Log;
 import android.os.Handler;
 import java.lang.Runnable;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.support.v4.content.LocalBroadcastManager;
 
 /**
  * The Class BLEService.
@@ -17,9 +21,15 @@ import java.lang.Runnable;
 public class BLEService extends Service {
 
 	private static final String TAG = "bleservice.BLEService";
+    private BluetoothAdapter mBluetoothAdapter;
+    private Handler mHandler;
+    private boolean mScanning;
 
 	/** The ctx. */
 	public Context ctx = this;
+
+    // Stops scanning after 100 seconds.
+    private static final long SCAN_PERIOD = 100000;
 
 	/**
 	 * Class for clients to access. Because we know this service always runs in
@@ -43,16 +53,25 @@ public class BLEService extends Service {
 	 */
 	@Override
 	public void onCreate() {
+		mHandler = new Handler();
 		// Display a notification about us starting. We put an icon in the
 		// status bar.
 		showNotification();
-		Log.d(TAG, "The Service created");
-		new Handler().postDelayed(new Runnable() {
-			@Override
-			public void run() {
-				stopSelf();
-			}
-		}, 10000);
+		sendMessage("The Service created");
+//		Toast.makeText(this, "The Service created", Toast.LENGTH_SHORT).show();
+
+        // Initializes a Bluetooth adapter.  For API level 18 and above, get a reference to
+        // BluetoothAdapter through BluetoothManager.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(this.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+//		new Handler().postDelayed(new Runnable() {
+//			@Override
+//			public void run() {
+//				stopSelf();
+//			}
+//		}, 10000);
 	}
 
 	/**
@@ -66,6 +85,7 @@ public class BLEService extends Service {
 
 		// This is the method we would put the code that does the work
 
+       scanLeDevice(true);
 		return START_STICKY;
 	}
 
@@ -76,8 +96,8 @@ public class BLEService extends Service {
 	public void onDestroy() {
 
 		// Tell the user we stopped.
-		Toast.makeText(this, "The Service has Stopped", Toast.LENGTH_SHORT).show();
-		Log.d(TAG, "The Service has Stopped");
+//		Toast.makeText(this, "The Service has Stopped", Toast.LENGTH_SHORT).show();
+		sendMessage("The Service has Stopped");
 	}
 
 	/**
@@ -98,12 +118,58 @@ public class BLEService extends Service {
 	 */
 	private void showNotification() {
 		CharSequence text = "Service has started...";
+//		Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show();
 		// This is the intent that is fired off after the user clicks the
 		// notification
-		PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
-				new Intent(this, StopServiceActivity.class), 0);
-		Log.d(TAG, text.toString());
+//		PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+//				new Intent(this, StopServiceActivity.class), 0);
+		sendMessage(text.toString());
 
 	}
+
+	private void scanLeDevice(final boolean enable) {
+		if (enable) {
+			// Stops scanning after a pre-defined scan period.
+			mHandler.postDelayed(new Runnable() {
+				@Override
+				public void run() {
+					mScanning = false;
+			sendMessage("scanLeDevice stop on timeout");
+					mBluetoothAdapter.stopLeScan(mLeScanCallback);
+				}
+			}, SCAN_PERIOD);
+
+			mScanning = true;
+			sendMessage("scanLeDevice start");
+			mBluetoothAdapter.startLeScan(mLeScanCallback);
+		} else {
+			mScanning = false;
+			sendMessage("scanLeDevice stop");
+			mBluetoothAdapter.stopLeScan(mLeScanCallback);
+		}
+	}
+
+	private BluetoothAdapter.LeScanCallback mLeScanCallback =
+			new BluetoothAdapter.LeScanCallback() {
+
+		@Override
+		public void onLeScan(final BluetoothDevice device, int rssi, byte[] scanRecord) {
+			String deviceName = device.getName();
+			if(!(deviceName == null)) {
+				sendMessage("Found " + deviceName);
+			}
+		}
+	};
+
+  // Send an Intent with an action named "custom-event-name". The Intent
+  // sent should
+  // be received by the ReceiverActivity.
+  private void sendMessage(String message) {
+    Log.d(TAG, message);
+    Intent intent = new Intent("ble_message");
+    // You can also include some extra data.
+    intent.putExtra("message", message);
+    LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
+  }
 
 }
