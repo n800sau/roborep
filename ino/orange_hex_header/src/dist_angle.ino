f9ba4c244c096d6b44b32daa336bd219f8a5de48
nonamed <n800sau@gmail.com>
2017-07-16 10:40:21 +1000

    dist_angle.ino added

---
+++ ino/orange_hex_header/src/dist_angle.ino
@@ -0,0 +1,259 @@
+// I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
+// for both classes must be in the include path of your project
+#include "I2Cdev.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    #include "Wire.h"
+#endif
+
+#include "MPU6050_6Axis_MotionApps20.h"
+#include <VL53L0X.h>
+#include <Servo.h>
+
+VL53L0X sensor;
+
+const int SONAR_INCR = 30;
+
+const int SONAR_PAN_ANGLE_MIN = 35;
+const int SONAR_PAN_ANGLE_MAX = 135;
+const int SONAR_PAN_CENTER = SONAR_PAN_ANGLE_MIN + (SONAR_PAN_ANGLE_MAX - SONAR_PAN_ANGLE_MIN) / 2;
+
+const int SONAR_TILT_ANGLE_MIN = 80;
+const int SONAR_TILT_ANGLE_MAX = 145;
+const int SONAR_TILT_CENTER = SONAR_TILT_ANGLE_MIN + (SONAR_TILT_ANGLE_MAX - SONAR_TILT_ANGLE_MIN) / 2;
+
+// up-down
+const int headTiltServoPin = 6;
+// left-right
+const int headPanServoPin = 5;
+
+Servo head_pan_servo;
+Servo head_tilt_servo;
+
+const int LED_PIN = 8;
+
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+MPU6050 mpu;
+
+#define INTERRUPT_PIN2 2
+#define INTERRUPT_PIN3 3
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+	mpuInterrupt = true;
+}
+
+// watch how much data block is in hex
+// it can grow more than 100% !!!
+#define MAX_COUNT 200
+
+struct {
+	int mm;
+	int pinch;
+} data[MAX_COUNT];
+
+int data_size;
+int lost_data_size;
+
+void setup()
+{
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+	Serial.begin(115200);
+
+	pinMode(LED_PIN, OUTPUT);
+	digitalWrite(LED_PIN, HIGH);
+
+	Serial.println("\ndist_angle start");
+
+
+	head_pan_servo.attach(headPanServoPin);
+	head_tilt_servo.attach(headTiltServoPin);
+
+	mpu.initialize();
+	pinMode(INTERRUPT_PIN2, INPUT);
+	pinMode(INTERRUPT_PIN3, INPUT);
+	Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
+
+	// load and configure the DMP
+	Serial.println(F("Initializing DMP..."));
+	devStatus = mpu.dmpInitialize();
+
+	// supply your own gyro offsets here, scaled for min sensitivity
+//	mpu.setXGyroOffset(220);
+//	mpu.setYGyroOffset(76);
+//	mpu.setZGyroOffset(-85);
+//	mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+	// make sure it worked (returns 0 if so)
+	if (devStatus == 0) {
+		// turn on the DMP, now that it's ready
+		Serial.println(F("Enabling DMP..."));
+		mpu.setDMPEnabled(true);
+
+		// enable Arduino interrupt detection
+		Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+		attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN2), dmpDataReady, RISING);
+		attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN3), dmpDataReady, RISING);
+		mpuIntStatus = mpu.getIntStatus();
+
+		// set our DMP Ready flag so the main loop() function knows it's okay to use it
+		Serial.println(F("DMP ready! Waiting for first interrupt..."));
+		dmpReady = true;
+
+		// get expected DMP packet size for later comparison
+		packetSize = mpu.dmpGetFIFOPacketSize();
+
+		sensor.init();
+		sensor.setTimeout(500);
+		// lower the return signal rate limit (default is 0.25 MCPS)
+		sensor.setSignalRateLimit(0.1);
+		// increase laser pulse periods (defaults are 14 and 10 PCLKs)
+		sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodPreRange, 18);
+		sensor.setVcselPulsePeriod(VL53L0X::VcselPeriodFinalRange, 14);
+	} else {
+		// ERROR!
+		// 1 = initial memory load failed
+		// 2 = DMP configuration updates failed
+		// (if it's going to break, usually the code will be 1)
+		Serial.print(F("DMP Initialization failed (code "));
+		Serial.print(devStatus);
+		Serial.println(F(")"));
+	}
+	delay(500);
+	digitalWrite(LED_PIN, LOW);
+}
+
+void collect_data()
+{
+	// wait for MPU interrupt or extra packet(s) available
+	if(mpuInterrupt)
+	{
+		digitalWrite(LED_PIN, LOW);
+		// reset interrupt flag and get INT_STATUS byte
+		mpuInterrupt = false;
+		mpuIntStatus = mpu.getIntStatus();
+
+		// get current FIFO count
+		fifoCount = mpu.getFIFOCount();
+
+		// check for overflow (this should never happen unless our code is too inefficient)
+		if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+			// reset so we can continue cleanly
+			mpu.resetFIFO();
+			Serial.println(F("FIFO overflow!"));
+
+		// otherwise, check for DMP data ready interrupt (this should happen frequently)
+		} else if (mpuIntStatus & 0x02) {
+			// wait for correct available data length, should be a VERY short wait
+			while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+
+			// read a packet from FIFO
+			mpu.getFIFOBytes(fifoBuffer, packetSize);
+			
+			// track FIFO count here in case there is > 1 packet available
+			// (this lets us immediately read more without waiting for an interrupt)
+			fifoCount -= packetSize;
+
+			// display Euler angles in degrees
+			mpu.dmpGetQuaternion(&q, fifoBuffer);
+			mpu.dmpGetGravity(&gravity, &q);
+			mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+
+			int mm = sensor.readRangeSingleMillimeters();
+			if (sensor.timeoutOccurred()) {
+				Serial.print(" TIMEOUT ");
+			} else {
+				if(data_size < MAX_COUNT - 1) {
+					data[data_size].mm = mm;
+					data[data_size].pinch = ypr[1] * 180/M_PI;
+					data_size++;
+				} else {
+					lost_data_size++;
+				}
+				Serial.print(mm);
+				Serial.print("\t");
+			}
+			// just pinch is needed
+			Serial.println(ypr[1] * 180/M_PI);
+		}
+	} else {
+		digitalWrite(LED_PIN, HIGH);
+	}
+}
+
+void collect_data_for(unsigned long ms)
+{
+	unsigned long last_ms = millis();
+	mpu.resetFIFO();
+	do {
+		collect_data();
+	} while(millis() - last_ms < ms);
+}
+
+unsigned long last_ms;
+
+void swing()
+{
+	data_size = 0;
+	lost_data_size = 0;
+	Serial.println("Start collecting");
+	for(int tilt=SONAR_TILT_ANGLE_MIN; tilt<SONAR_TILT_ANGLE_MAX; tilt+=SONAR_INCR) {
+		Serial.print("tilt=");
+		Serial.println(tilt);
+		head_tilt_servo.write(tilt);
+		head_pan_servo.write(SONAR_PAN_ANGLE_MIN);
+		delay(200);
+		mpu.resetFIFO();
+		Serial.println("Swing forward");
+		head_pan_servo.write(SONAR_PAN_ANGLE_MAX);
+		collect_data_for(500);
+		Serial.println("Swing back");
+		head_pan_servo.write(SONAR_PAN_ANGLE_MIN);
+		collect_data_for(500);
+	}
+	Serial.println("End collecting");
+	Serial.print("Lost data size:");
+	Serial.println(lost_data_size);
+}
+
+void loop()
+{
+	// if programming failed, don't try to do anything
+	if (!dmpReady) return;
+
+	if(Serial.available()) {
+		switch(Serial.read()) {
+			case 'r':
+				swing();
+				break;
+			case 'g':
+				Serial.print("data size:");
+				Serial.println(data_size);
+				for(int i=0; i<data_size; i++) {
+					Serial.print("!d#");
+					Serial.print(data[i].mm);
+					Serial.print("@");
+					Serial.print(data[i].pinch);
+					Serial.println("@");
+				}
+				break;
+		}
+	}
+}
+
