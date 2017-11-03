e6f0d39f605925fe9ff113d8a38e1f456c986189
n800s <n800sau@gmail.com>
2017-11-03 16:57:15 +1100

    added odom and tf (not working yet)

--- ROS/custom_src/ros2bwh/bin/ros2bwh.py
+++ ROS/custom_src/ros2bwh/bin/ros2bwh.py
@@ -4,8 +4,10 @@
 import sys, os, random, time
 import rospy
 import serial
-from geometry_msgs.msg import Quaternion, Twist, Pose
-from tf.broadcaster import TransformBroadcaster
+from math import sin, cos, pi
+import tf
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Point, Pose, Quaternion, Twist, Vector3
 
 DEFPORT = '/dev/ttyS1'
 # max timeout of no data before debug message is emitted
@@ -58,10 +60,60 @@ class BlueWheel:
 
 	def run(self):
 		rospy.init_node('blue_wheel', anonymous = True)
+
 		# Subscriptions
 		rospy.Subscriber("cmd_vel", Twist, self.cmdVelCallback)
+
 		# Publications
-		rospy.spin()
+		pub_odom = rospy.Publisher('', Imu, queue_size=10)
+		pub_odom = rospy.Publisher("odom", Odometry, queue_size=50)
+		odom_broadcaster = tf.TransformBroadcaster()
+
+		last_time = rospy.Time.now()
+		r = rospy.Rate(10)
+		while not rospy.is_shutdown():
+			current_time = rospy.Time.now()
+
+			# compute odometry in a typical way given the velocities of the robot
+			dt = (current_time - last_time).to_sec()
+			delta_x = (vx * cos(th) - vy * sin(th)) * dt
+			delta_y = (vx * sin(th) + vy * cos(th)) * dt
+			delta_th = vth * dt
+
+			x += delta_x
+			y += delta_y
+			th += delta_th
+
+			# since all odometry is 6DOF we'll need a quaternion created from yaw
+			odom_quat = tf.transformations.quaternion_from_euler(0, 0, th)
+
+			# first, we'll publish the transform over tf
+			odom_broadcaster.sendTransform(
+						(x, y, 0.),
+						odom_quat,
+						current_time,
+						"base_link",
+						"odom"
+			)
+
+			# next, we'll publish the odometry message over ROS
+			odom = Odometry()
+			odom.header.stamp = current_time
+			odom.header.frame_id = "odom"
+
+			# set the position
+			odom.pose.pose = Pose(Point(x, y, 0.), Quaternion(*odom_quat))
+
+			# set the velocity
+			odom.child_frame_id = "base_link"
+			odom.twist.twist = Twist(Vector3(vx, vy, 0), Vector3(0, 0, vth))
+
+			# publish the message
+			pub_odom.publish(odom)
+
+			last_time = current_time
+
+			r.sleep()
 
 if __name__ == '__main__':
 
