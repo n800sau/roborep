3cee14777a9548e58387766b3939b8bdd82a979f
n800s <n800sau@gmail.com>
2017-11-13 22:23:41 +1100

    adjustments PID coefs, motors, encoders, directions etc

--- ard_stm32/blue_wheel_driver_ros/src/main.ino
+++ ard_stm32/blue_wheel_driver_ros/src/main.ino
@@ -3,73 +3,72 @@
 #include <std_msgs/Int16.h>
 #include <std_srvs/Empty.h>
 
-const int left_pwm = PB8;
-const int left_in1 = PB15;
-const int left_in2 = PB14;
+const int left_pwm = PB9;
+const int left_in1 = PB12;
+const int left_in2 = PB13;
 
-const int right_pwm = PB9;
-const int right_in3 = PB12;
-const int right_in4 = PB13;
-
+const int right_pwm = PB8;
+const int right_in1 = PB15;
+const int right_in2 = PB14;
 
 #define LED_BUILDING PC13
 
-const int leftQeiAPin = PB3;
-const int leftQeiBPin = PB4;
+const int leftQeiAPin = PA15;
+const int leftQeiBPin = PA12;
 
-const int rightQeiAPin = PA15;
-const int rightQeiBPin = PA12;
+const int rightQeiAPin = PB4;
+const int rightQeiBPin = PB3;
 
 
-//const int minPwr = 128;
-const int minPwr = 0;
+const int minPwr = 128;
+//const int minPwr = 64;
 const int maxPwr = 255;
+
+// encoders
 volatile int16_t leftCounts = 0, rightCounts = 0;
 volatile int16_t oldLeftCounts = 0, oldRightCounts = 0;
 // micros per encoder step
 volatile unsigned long int leftElapsedTime = 0, rightElapsedTime = 0;
 volatile unsigned long int leftOldElapsedTime = 0, rightOldElapsedTime = 0;
 
-bool cur_left_fwd = true;
-bool cur_right_fwd = true;
+// rate
+int req_left_rate = 0;
+int req_right_rate = 0;
 
 int leftPower = 0, oldLeftPower = 0;
 int rightPower = 0, oldRightPower = 0;
 
-bool stop_mode = true;
-
 //PID
 
 //adjust these values to fit your own design
-double Kp = 20;
-double Kd = 3;
-double Ki = 10;
+double Kp = 0.04;
+double Kd = 0.03;
+double Ki = 0.05;
 
-bool left_fwd = true;
-double left_rate = 0;
-double left_cur_rate, left_power;
-PID left_pid(&left_cur_rate, &left_power, &left_rate, Kp, Ki, Kd, DIRECT);
+double pid_req_left_rate = 0;
+double pid_cur_left_rate, pid_left_power;
+PID left_pid(&pid_cur_left_rate, &pid_left_power, &pid_req_left_rate, Kp, Ki, Kd, DIRECT);
 
-bool right_fwd = true;
-double right_rate = 0;
-double right_cur_rate, right_power;
-PID right_pid(&right_cur_rate, &right_power, &right_rate, Kp, Ki, Kd, DIRECT);
-
-bool st_changed = false;
+double pid_req_right_rate = 0;
+double pid_cur_right_rate, pid_right_power;
+PID right_pid(&pid_cur_right_rate, &pid_right_power, &pid_req_right_rate, Kp, Ki, Kd, DIRECT);
 
 ros::NodeHandle nh;
 
 // pwr: -255 .. 255
-void move_motor(int pin1, int pin2, int pin_pwm, int pwr, bool fwd)
+void move_motor(int pin1, int pin2, int pin_pwm, int pwr)
 {
-	if(fwd) {
+	if(pwr == 0) {
+		digitalWrite(pin1, LOW);
+		digitalWrite(pin2, LOW);
+	} else if(pwr > 0) {
 		digitalWrite(pin1, LOW);
 		digitalWrite(pin2, HIGH);
 	} else {
 		digitalWrite(pin1, HIGH);
 		digitalWrite(pin2, LOW);
 	}
-	analogWrite(pin_pwm, pwr);
+	analogWrite(pin_pwm, abs(pwr));
 }
 
 std_msgs::Int16 l_ticks;
@@ -77,40 +76,42 @@ ros::Publisher l_ticks_pub("lwheel_ticks", &l_ticks);
 std_msgs::Int16 r_ticks;
 ros::Publisher r_ticks_pub("rwheel_ticks", &r_ticks); 
 
+std_msgs::Int16 l_rate;
+ros::Publisher l_rate_pub("lwheel_rate", &l_rate); 
+std_msgs::Int16 r_rate;
+ros::Publisher r_rate_pub("rwheel_rate", &r_rate); 
+
 std_msgs::Int16 l_pwr;
 ros::Publisher l_pwr_pub("lwheel_pwr", &l_pwr); 
 std_msgs::Int16 r_pwr;
 ros::Publisher r_pwr_pub("rwheel_pwr", &r_pwr); 
 
 
-void move()
+void move_left()
 {
-	int l_p, r_p;
-	bool l_f, r_f;
-	if(stop_mode) {
-		l_p = r_p = 0;
-		l_f = r_f = true;
+	int l_p;
+	if(req_left_rate == 0) {
+		l_p = 0;
 	} else {
-		if(leftPower == 0) {
-			nh.loginfo(String("leftPower zeroed").c_str());
-		}
-		if(rightPower == 0) {
-			nh.loginfo(String("rightPower zeroed").c_str());
-		}
 		l_p = leftPower;
-		r_p = rightPower;
-		l_f = left_fwd;
-		r_f = right_fwd;
 	}
-	move_motor(left_in1, left_in2, left_pwm, l_p, l_f);
-	move_motor(right_in3, right_in4, right_pwm, r_p, r_f);
-	l_p = l_f ? l_p : -l_p;
-	r_p = r_f ? r_p : -r_p;
+	move_motor(left_in1, left_in2, left_pwm, l_p);
 	if(oldLeftPower != l_p) {
 		l_pwr.data = l_p;
 		l_pwr_pub.publish(&l_pwr);
 		oldLeftPower = l_p;
 	}
+}
+
+void move_right()
+{
+	int r_p;
+	if(req_right_rate == 0) {
+		r_p = 0;
+	} else {
+		r_p = rightPower;
+	}
+	move_motor(right_in1, right_in2, right_pwm, r_p);
 	if(oldRightPower != r_p) {
 		r_pwr.data = r_p;
 		r_pwr_pub.publish(&r_pwr);
@@ -120,38 +121,34 @@ void move()
 
 void on_lwheel_rate(const std_msgs::Int16 &msg)
 {
-	left_fwd = msg.data >= 0;
-	left_rate = abs(msg.data);
-	if(msg.data != 0) {
-		stop_mode = false;
-//		if(leftPower == 0) {
-//			// set initial power
-//			leftPower = maxPwr;
-//			move();
-//		}
+	if(req_left_rate != msg.data) {
+		req_left_rate = msg.data;
+		nh.loginfo((String("left_rate:") + String(req_left_rate)).c_str());
+		leftElapsedTime = 0;
+		// initial start
+		pid_req_left_rate = abs(req_left_rate);
+		move_left();
 	}
-	nh.loginfo((String("left_rate:") + String(left_rate) + "," + String((left_fwd) ? "forward" : "back")).c_str());
 }
 
 void on_rwheel_rate(const std_msgs::Int16 &msg)
 {
-	right_fwd = msg.data >= 0;
-	right_rate = abs(msg.data);
-	if(msg.data != 0) {
-		stop_mode = false;
-//		if(rightPower == 0) {
-//			// set initial power
-//			rightPower = maxPwr;
-//			move();
-//		}
+	if(req_right_rate != msg.data) {
+		req_right_rate = msg.data;
+		nh.loginfo((String("right_rate:") + String(req_right_rate)).c_str());
+		rightElapsedTime = 0;
+		// initial start
+		pid_req_right_rate = abs(req_right_rate);
+		move_right();
 	}
-	nh.loginfo((String("right_rate:") + String(right_rate) + "," + String((right_fwd) ? "forward" : "back")).c_str());
 }
 
 void stop_cb(const std_srvs::Empty::Request& request, std_srvs::Empty::Response& response)
 {
-	stop_mode = true;
-	nh.loginfo("stop mode set");
+	req_left_rate = req_right_rate = 0;
+	move_left();
+	move_right();
+	nh.loginfo("stop motors");
 }
 
 ros::Subscriber<std_msgs::Int16> sub_lw_rate("lwheel_desired_rate", &on_lwheel_rate);
@@ -181,7 +178,6 @@ void leftQuickQEI()
 	microTime = micros();
 	leftElapsedTime = microTime-leftOldElapsedTime;
 	leftOldElapsedTime = microTime;
-	cur_left_fwd = oldLeftCounts - leftCounts;
 	oldLeftCounts = leftCounts;
 }
 
@@ -206,7 +202,6 @@ void rightQuickQEI()
 	microTime = micros();
 	rightElapsedTime = microTime-rightOldElapsedTime;
 	rightOldElapsedTime = microTime;
-	cur_right_fwd = oldRightCounts - rightCounts;
 	oldRightCounts = rightCounts;
 }
 
@@ -217,8 +212,8 @@ void setup()
 	pinMode(left_in1, OUTPUT);
 	pinMode(left_in2, OUTPUT);
 	pinMode(left_pwm, OUTPUT);
-	pinMode(right_in3, OUTPUT);
-	pinMode(right_in4, OUTPUT);
+	pinMode(right_in1, OUTPUT);
+	pinMode(right_in2, OUTPUT);
 	pinMode(right_pwm, OUTPUT);
 	pinMode(leftQeiAPin, INPUT_PULLUP);
 	pinMode(rightQeiAPin, INPUT_PULLUP);
@@ -227,12 +222,12 @@ void setup()
 
 	//setup left PID
 	left_pid.SetMode(AUTOMATIC);
-	left_pid.SetSampleTime(10);
+	left_pid.SetSampleTime(5);
 	left_pid.SetOutputLimits(minPwr, maxPwr);
 
 	//setup right PID
 	right_pid.SetMode(AUTOMATIC);
-	right_pid.SetSampleTime(10);
+	right_pid.SetSampleTime(5);
 	right_pid.SetOutputLimits(minPwr, maxPwr);
 
 	attachInterrupt(leftQeiAPin, leftQuickQEI, CHANGE);
@@ -243,6 +238,8 @@ void setup()
 	nh.initNode();
 	nh.advertise(l_ticks_pub);
 	nh.advertise(r_ticks_pub);
+	nh.advertise(l_rate_pub);
+	nh.advertise(r_rate_pub);
 	nh.advertise(l_pwr_pub);
 	nh.advertise(r_pwr_pub);
 	nh.subscribe(sub_lw_rate);
@@ -258,51 +255,53 @@ void ledSwitch()
 
 void loop()
 {
-//	if (nh.connected()) {
 
-		if(stop_mode) {
-			// in move it stops
-			move();
-		} else {
-
-			left_cur_rate = leftElapsedTime > 0 ? ((cur_left_fwd ? 1000000. : -1000000.) / leftElapsedTime) : 0;
-			right_cur_rate = rightElapsedTime > 0 ? ((cur_right_fwd ? 1000000. : -1000000.) / rightElapsedTime) : 0;
-
-			if(left_rate == 0) {
-				st_changed = leftPower != 0;
-				leftPower = 0;
-			} else if(left_pid.Compute()) { 
-				st_changed = true;
-				leftPower = left_power;
+	int l_dir = (req_left_rate >= 0 ? 1 : -1);
+	if(leftElapsedTime > 0) {
+		pid_cur_left_rate = 1000000. / leftElapsedTime;
+		if(left_pid.Compute()) {
+			l_rate.data = pid_cur_left_rate * l_dir;
+			l_rate_pub.publish(&l_rate);
+			if(pid_left_power > 0) {
+				leftPower = pid_left_power * l_dir;
+				move_left();
 			}
-
-			if(right_rate == 0) {
-				st_changed = rightPower != 0;
-				rightPower = 0;
-			} else if(right_pid.Compute()) {
-				st_changed = true;
-				rightPower = right_power;
-			}
-			if(st_changed) {
-				move();
-			}
-
 		}
-
 		if(oldLeftCounts != leftCounts) {
 			l_ticks.data = leftCounts;
 			l_ticks_pub.publish(&l_ticks);
+		}
+	} else if(req_left_rate != 0) {
+		// if req_rate != 0 motor must move
+		if(abs(leftPower) < maxPwr) {
+			leftPower += 10 * l_dir;
+		}
+		move_left();
+	}
+
+	int r_dir = (req_right_rate >= 0 ? 1 : -1);
+	if(rightElapsedTime > 0) {
+		pid_cur_right_rate = 1000000. / rightElapsedTime;
+		if(right_pid.Compute()) {
+			r_rate.data = pid_cur_right_rate;
+			r_rate_pub.publish(&r_rate);
+			if(pid_right_power > 0) {
+				rightPower = pid_right_power * r_dir;
+				move_right();
+			}
 		}
 		if(oldRightCounts != rightCounts) {
 			r_ticks.data = rightCounts;
 			r_ticks_pub.publish(&r_ticks);
 		}
+	} else if(req_right_rate != 0) {
+		// if req_rate != 0 motor must move
+		if(abs(rightPower) < maxPwr) {
+			rightPower += 10 * r_dir;
+		}
+		move_right();
+	}
 
-		nh.spinOnce();
-
-//	} else {
-//		delay(1);
-//	}
+	nh.spinOnce();
 
 }
-
