ba50733eac9e169229f82d5a6c5f2b3b0fff99b6
n800s <n800sau@gmail.com>
2017-11-13 22:24:47 +1100

    more shell scripts

--- nodes/opcplus/catkin_ws/pub_back.sh
+++ nodes/opcplus/catkin_ws/pub_back.sh
@@ -1,3 +1,11 @@
-rostopic pub -1 /opcplus/cmd_vel geometry_msgs/Twist  '{linear:  {x: -2.0, y: 0.0, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}'
-#sleep 0
+rostopic pub -1 /opcplus/cmd_vel geometry_msgs/Twist  '{linear:  {x: -1.5, y: 0.0, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}'
+sleep 5
 rostopic pub -1 /opcplus/cmd_vel geometry_msgs/Twist  '{linear:  {x: 0, y: 0.0, z: 0.0}, angular: {x: 0.0,y: 0.0,z: 0.0}}'
+
+#rostopic pub -1 /opcplus/lwheel_desired_rate std_msgs/Int16 -- -1000
+#rostopic pub -1 /opcplus/rwheel_desired_rate std_msgs/Int16 -- 1000
+
+#sleep 5
+
+#rostopic pub -1 /opcplus/lwheel_desired_rate std_msgs/Int16  0 &
+#rostopic pub -1 /opcplus/rwheel_desired_rate std_msgs/Int16  0 &
