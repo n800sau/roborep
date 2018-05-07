8d7326f947abc30b91bdb199c09e40fc89c090a1
n800s <n800sau@gmail.com>
2018-05-07 20:20:17 +1000

    more espota

--- ard_esp8266/esp8266_ota_test/espota.sh
+++ ard_esp8266/esp8266_ota_test/espota.sh
@@ -1,2 +1,3 @@
-./espota.py --ip=myesp8266.local -f .pioenvs/esp01/firmware.bin -a Zagruzka -r -d &>espota.log
+./espota.py --ip=balancebot.local -f .pioenvs/esp01/firmware.bin -a Zagruzka -r -d &>espota.log
+#./espota.py --ip=balancebot.local -f data.spiff -a Zagruzka -s -d &>espota.log
 echo $?
