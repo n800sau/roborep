ceb08136f13cd55df9d4eb42dfde3a8f7c4cf3f0
n800s <n800sau@gmail.com>
2018-10-14 12:05:49 +1100

    fixed imports etc

--- cv_test/garage/motion/motion2ftp.py
+++ cv_test/garage/motion/motion2ftp.py
@@ -3,12 +3,14 @@
 import os, sys, time, traceback, re
 sys.path.append(os.path.expanduser('~n800s/private'))
 import redis
+from datetime import datetime
 from ftplib import FTP, all_errors
 from credential import cred
 
 R_QUEUE = 'q.motion_captured'
 
 reg = re.compile('([0-9]{14})-\d+(-\d+)?\.(jpg|avi)')
+BASE_DNAME = 'rus_hard/garage'
 
 r = redis.Redis()
 
@@ -19,21 +21,20 @@ while True:
 			ftp_h = FTP('192.168.1.1')
 			try:
 				ftp_h.login('writer', cred['writer@192.168.1.1'])
-				ftp_h.cwd('rus_hard/garage')
+				ftp_h.cwd(BASE_DNAME)
 				if pathlist is None:
-					pathlist = ftp_h.nlst()
+					pathlist = [os.path.basename(dname) for dname in ftp_h.nlst() if re.match(r'^\d+-\d+-\d+_((pics)|(vids))$', os.path.basename(dname))]
+					print pathlist
 				while True:
 					fname = r.rpop(R_QUEUE)
 					if not fname:
 						break
 					if os.path.exists(fname):
-						print 'Sending', fname, '...'
 						bname = os.path.basename(fname)
 						m = reg.match(bname)
 						if m and m.groups():
 							dt = datetime.strptime(m.groups()[0], '%Y%m%d%H%M%S')
-							bdname = dt.strftime('%Y-%m-%d')
-							dname = os.path.join(os.path.dirname(srcname), bdname)
+							dname = dt.strftime('%Y-%m-%d')
 							if os.path.splitext(bname)[-1] == '.avi':
 								dname += '_vids'
 							else:
@@ -41,12 +42,15 @@ while True:
 							if dname not in pathlist:
 								print 'Current directory', ftp_h.pwd()
 								print 'Create directory', dname
-								ftp_h.mkd(dname)
 								pathlist.append(dname)
+								ftp_h.mkd(dname)
+							dfname = os.path.join(dname, bname)
+							print 'Sending', fname, ' to ', dfname, '...'
 							f = open(fname, 'rb')
 							try:
-								ftp_h.storbinary('STOR ' + os.path.join(dname, bname), f)
+								ftp_h.storbinary('STOR ' + dfname, f)
 								f.close()
+								print fname, 'sent'
 								os.unlink(fname)
 							except:
 								r.lpush(R_QUEUE, fname)
