8b6b287cca4bf5a0742b8bc1bffbc645d4507bc0
n800s <n800sau@gmail.com>
2019-02-04 22:01:25 +1100

    calib.py added

--- cv_test/timelapse/timelapse.py
+++ cv_test/timelapse/timelapse.py
@@ -3,7 +3,7 @@ import cv2
 
 OFNAME = 'output.avi'
 OSIZE = (640, 480)
-MAX_SECONDS = 60
+MAX_SECONDS = 60*60
 
 fps = 0.5
 
@@ -12,7 +12,7 @@ fps = 0.5
 #vs = cv2.VideoCapture('http://192.168.1.132:3333/')
 #vs = cv2.VideoCapture('http://192.168.1.132:5001/')
 #vs = cv2.VideoCapture('http://192.168.1.132:8080/')
-vs = cv2.VideoCapture('http://hubee.local:8080/stream')
+vs = cv2.VideoCapture('http://hubee.local:8090/?action=stream')
 time.sleep(1.0)
 
 fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
@@ -25,15 +25,18 @@ while True:
 	(grabbed, frame) = vs.read()
 	t2 = time.time()
 	dt = t2 - t1
-	if grabbed and dt > 1/float(fps):
-		t1 = t2
-		print(n, ':', time.strftime('%H:%M:%S', time.localtime(t2)))
-		n = n + 1
-		oframe = cv2.resize(frame, OSIZE)
-		out.write(oframe)
+	if grabbed:
+		cv2.imshow('preview',frame)
+		cv2.waitKey(100)
+		if dt > 1/float(fps):
+			t1 = t2
+			print(n, ':', time.strftime('%H:%M:%S', time.localtime(t2)))
+			n = n + 1
+			oframe = cv2.resize(frame, OSIZE)
+			out.write(oframe)
 	if t2 - t > MAX_SECONDS:
 		break
 
 vs.release()
 out.release()
-
+cv2.destroyAllWindows()
