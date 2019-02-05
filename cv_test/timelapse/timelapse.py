0659a7555e25b5c7f654d47d4bf345a50c805f9c
n800s <n800sau@gmail.com>
2019-02-05 21:08:27 +1100

    minor

--- cv_test/timelapse/timelapse.py
+++ cv_test/timelapse/timelapse.py
@@ -2,17 +2,22 @@ import sys, os, time
 import cv2
 
 OFNAME = 'output.avi'
-OSIZE = (640, 480)
+#OSIZE = (640, 480)
+OSIZE = (1280, 720)
 MAX_SECONDS = 60*60
 
 fps = 0.5
+
+LAPSE_ID_FNAME = 'lapse_id.txt'
+lapse_id = open(LAPSE_ID_FNAME).read().strip() if os.path.exists(LAPSE_ID_FNAME) else ''
 
 #vs = cv2.VideoCapture('/dev/video0')
 #vs = cv2.VideoCapture('http://admin:9video9@192.168.1.94:38081/0/stream')
 #vs = cv2.VideoCapture('http://192.168.1.132:3333/')
 #vs = cv2.VideoCapture('http://192.168.1.132:5001/')
 #vs = cv2.VideoCapture('http://192.168.1.132:8080/')
-vs = cv2.VideoCapture('http://hubee.local:8090/?action=stream')
+#vs = cv2.VideoCapture('http://hubee.local:8090/?action=stream')
+vs = cv2.VideoCapture('http://hubee.local:9000/')
 time.sleep(1.0)
 
 fourcc = cv2.VideoWriter_fourcc('X', 'V', 'I', 'D')
@@ -21,11 +26,13 @@ out = cv2.VideoWriter(OFNAME, fourcc, 30, OSIZE)
 t = time.time()
 t1 = time.time()
 n = 0
+not_printed = True
 while True:
 	(grabbed, frame) = vs.read()
 	t2 = time.time()
 	dt = t2 - t1
 	if grabbed:
+		not_printed = True
 		cv2.imshow('preview',frame)
 		cv2.waitKey(1)
 		if dt > 1/float(fps):
@@ -33,7 +40,12 @@ while True:
 			print(n, ':', time.strftime('%H:%M:%S', time.localtime(t2)))
 			n = n + 1
 			oframe = cv2.resize(frame, OSIZE)
+			cv2.putText(oframe, lapse_id + time.strftime(' %d/%m%/%Y %H:%M:%S'), (20, 40) , cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 255))
 			out.write(oframe)
+	else:
+		if not_printed:
+			print('Not grabbed')
+			not_printed = False
 	if t2 - t > MAX_SECONDS:
 		break
 
