aab1cc5144b2ba5469a18a94ff84549bf6d40c09
n800s <n800sau@gmail.com>
2019-07-24 23:18:35 +1000

    micro-rtsp added

---
+++ ard_esp32/micro-rtsp/src/wifikeys.h
@@ -0,0 +1,3 @@
+// copy this file to wifikeys.h and edit
+const char *ssid =     "Slow Internet Connection";         // Put your SSID here
+const char *password = "1,tpGfhjkz2";     // Put your PASSWORD here
