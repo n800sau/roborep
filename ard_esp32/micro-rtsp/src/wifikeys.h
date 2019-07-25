39a2704654be8740700b5ecb7287b79981121392
n800s <n800sau@gmail.com>
2019-07-25 15:13:21 +1000

    some output for testing added

--- ard_esp32/micro-rtsp/src/wifikeys.h
+++
@@ -1,3 +0,0 @@
-// copy this file to wifikeys.h and edit
-const char *ssid =     "Slow Internet Connection";         // Put your SSID here
-const char *password = "1,tpGfhjkz2";     // Put your PASSWORD here
