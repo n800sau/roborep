6c05a6f52f0ed181934b2549cf741a2ae528e8c6
n800s <n800sau@gmail.com>
2019-09-02 09:53:28 +1000

    some changes

--- ard_esp32/blink/make_bumpRTS.sh
+++ ard_esp32/blink/make_bumpRTS.sh
@@ -1,1 +1,2 @@
 gcc bumpRTS.c -o bumpRTS
+echo $?
--- ard_esp32/blink/upload.sh
+++ ard_esp32/blink/upload.sh
@@ -1,7 +1,8 @@
 source vars.sh
 
 #platformio run -v -t upload --upload-port $DEV
-platformio run -t upload --upload-port $DEV &>upload.log
+#platformio run -v -t upload --upload-port $DEV &>upload.log
+platformio run -v -t upload --upload-port 192.168.1.151 &>upload.log
 echo $?
 
 
