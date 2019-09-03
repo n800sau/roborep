bb4a1b59d063c475e8dc4e2a424c36122cfc6cf1
n800s <n800sau@gmail.com>
2019-09-03 10:14:13 +1000

    keylib separated

--- ino/buttons_test/src/main.ino
+++ ino/buttons_test/src/main.ino
@@ -1,54 +1,19 @@
+#include <keylib.h>
 
 const uint8_t oline[] = {11, 12, 13};
 const uint8_t iline[] = {8, 9, 10};
 
+readKey keylib = readKey();
+
 void setup()
 {
-
-	for(byte i=0; i<sizeof(oline); i++) {
-		pinMode(oline[i], OUTPUT);
-		digitalWrite(oline[i], HIGH);
-	}
-
-	for(byte i=0; i<sizeof(iline); i++) {
-		pinMode(iline[i], INPUT_PULLUP);
-	}
-
 	Serial.begin(115200);
+	keylib.begin(sizeof(oline), sizeof(iline), oline, iline);
 	Serial.println("\nButton test");
-}
-
-byte last_key = 0;
-unsigned long  last_change_millis = 0;
-const unsigned long delta_millis = 500;
-
-byte read_key()
-{
-	byte rs = 0;
-	for(byte i=0; i<sizeof(oline) && rs == 0; i++) {
-		digitalWrite(oline[i], LOW);
-		for(byte j=0; j<sizeof(iline); j++) {
-			if(digitalRead(iline[j]) == LOW) {
-				rs = (i+1)<<4 | (j+1);
-				break;
-			}
-		}
-		digitalWrite(oline[i], HIGH);
-	}
-	return rs;
 }
 
 void loop()
 {
-	byte key = read_key();
-	if(last_key != key) {
-		unsigned long m = millis();
-		if(key == 0 && delta_millis + last_change_millis > m) {
-			key = last_key;
-			last_change_millis = m;
-		} else {
-			last_key = key;
-		}
-		Serial.println(key, HEX);
-	}
+	byte key = keylib.read_key_debounce();
+	Serial.println(key, HEX);
 }
