8fcb157aff929f327f56aaab9cb3ddc5cc200882
n800s <n800sau@gmail.com>
2019-09-08 00:28:46 +1000

    finding rts dts way to program

--- ard_stm32/blink/serial.sh
+++ ard_stm32/blink/serial.sh
@@ -1,4 +1,2 @@
 source vars.sh
-#picocom -b 115200 $DEV
-DEV=/dev/ttyUSB1
-../../bin/miniterm.py -p $DEV -b 115200
+../../bin/miniterm.py -p $DEV --dtr 0 -b 115200
--- ard_stm32/blink/upload.sh
+++ ard_stm32/blink/upload.sh
@@ -4,11 +4,16 @@ export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/n800s/.platformio/packages/tool-
 
 #../../ino/set_stm32_mode/set_pmode.py
 
-DEV=/dev/ttyUSB1
+#DEV=/dev/ttyUSB1
 #platformio run -t upload --upload-port $DEV
 #platformio run -v -t upload
 #stm32flash -g 0x08000000 -b 115200 -w ".pio/build/maple/firmware.bin" "/dev/ttyUSB1"
-stm32flash -g 0x08000000 -b 115200 -w ".pio/build/maple/firmware.bin" "$DEV"
+#/usr/bin/stm32flash -g 0x08000000 -i 'rts,-dtr,dtr' -b 115200 -w ".pio/build/maple/firmware.bin" "$DEV"
+#/usr/bin/stm32flash -g 0x08000000 -R -i 'rts,dtr,-dtr:rts,-dtr,dtr' -b 115200 -w ".pio/build/maple/firmware.bin" "$DEV"
+
+# flasher working with usb-serial rts -> 10K -> BOOT0 <- 10K <- GND, and DTR -> RESET <- 3.3v
+# pip install -U stm32loader
+stm32loader -a 0x08000000 -V -e -w -f F1 -p "$DEV" ".pio/build/maple/firmware.bin"
 
 #sleep 1
 #../../ino/set_stm32_mode/set_wmode.py
