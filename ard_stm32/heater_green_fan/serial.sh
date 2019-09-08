62e4dd7b0ea3c5f6014c30944f1a9355c323dcbd
n800s <n800sau@gmail.com>
2019-09-08 22:37:39 +1000

    heater_green_fan added to ard_stm32

---
+++ ard_stm32/heater_green_fan/serial.sh
@@ -0,0 +1,2 @@
+source vars.sh
+../../bin/miniterm.py -p $DEV --dtr 0 -b 115200
---
+++ ard_stm32/heater_green_fan/upload.sh
@@ -0,0 +1,20 @@
+source vars.sh
+
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/n800s/.platformio/packages/tool-stlink/lib
+
+#../../ino/set_stm32_mode/set_pmode.py
+
+#DEV=/dev/ttyUSB1
+#platformio run -t upload --upload-port $DEV
+#platformio run -v -t upload
+#stm32flash -g 0x08000000 -b 115200 -w ".pio/build/maple/firmware.bin" "/dev/ttyUSB1"
+#/usr/bin/stm32flash -g 0x08000000 -i 'rts,-dtr,dtr' -b 115200 -w ".pio/build/maple/firmware.bin" "$DEV"
+#/usr/bin/stm32flash -g 0x08000000 -R -i 'rts,dtr,-dtr:rts,-dtr,dtr' -b 115200 -w ".pio/build/maple/firmware.bin" "$DEV"
+
+# flasher working with usb-serial rts -> 10K -> BOOT0 <- 10K <- GND, and DTR -> RESET <- 3.3v
+# pip install -U stm32loader
+stm32loader -a 0x08000000 -e -w -f F1 -p "$DEV" ".pio/build/maple/firmware.bin" &>upload.log
+echo $?
+
+#sleep 1
+#../../ino/set_stm32_mode/set_wmode.py
