9128df51ee37b5f9b888f6298b1d6f335df0544d
n800s <n800sau@gmail.com>
2019-09-18 13:41:23 +1000

    emergency stop code

--- ino/heater_green_fan_pid_i2c/src/heater_green_fan_pid_i2c.ino
+++ ino/heater_green_fan_pid_i2c/src/heater_green_fan_pid_i2c.ino
@@ -33,6 +33,11 @@ PID heaterPID(&Input, &Output, &Setpoint, hKp, hKi, hKd, REVERSE);
 
 #endif //USE_PID
 
+bool emergency_stop = false;
+const int EMERGENCY_LED_PIN = 13;
+bool emergency_led_on = false;
+unsigned long emergency_last_ts = 0;
+
 int heater_pwm = 0;
 const int MIN_PWM = -400, MAX_PWM = 400;
 
@@ -45,15 +50,19 @@ const int FAN_PIN = 7;
 #define UNKNOWN_VAL -1000
 
 double temp = UNKNOWN_VAL, temp2set = UNKNOWN_VAL;
-const uint16_t max_temp = 100, min_temp = 0;
+const double max_temp = 100, min_temp = 0;
 
-float Vcc = 3.3;
-float Vref = 1.1;
-const float T_0 = 273.15;
-const float T_25 = T_0 + 25;
+// for emergency stop test
+double old_temp = UNKNOWN_VAL;
+int old_heater_pwm = 0;
+
+double Vcc = 3.3;
+double Vref = 1.1;
+const double T_0 = 273.15;
+const double T_25 = T_0 + 25;
 // 100k NTC
-const float beta = 3950;
-const float R_25 = 100000L; // 100k ohm
+const double beta = 3950;
+const double R_25 = 100000L; // 100k ohm
 const unsigned long Rs = 470000L;
 
 void display_status()
@@ -122,28 +131,36 @@ void set_temp2set(int t)
 void process_proc()
 {
 	update_temp();
+	if(emergency_stop) {
+		if(heater_pwm != 0) {
+			heater_pwm = 0;
+			Serial.println("Emergency stop. Reboot me !!!");
+			digitalWrite(FAN_PIN, HIGH);
+		}
+	} else {
 #ifdef USE_PID
-	Input = temp;
-	Setpoint = temp2set;
-//	Serial.print("Error:");
-//	Serial.println(temp2set-temp);
-	heaterPID.Compute();
-	heater_pwm = Output;
-//	Serial.print("Output:");
-//	Serial.println(heater_pwm);
+		Input = temp;
+		Setpoint = temp2set;
+//		Serial.print("Error:");
+//		Serial.println(temp2set-temp);
+		heaterPID.Compute();
+		heater_pwm = Output;
+//		Serial.print("Output:");
+//		Serial.println(heater_pwm);
 #else
-	if(temp < temp2set) {
-		heater_pwm = MAX_PWM;
-	} else if(temp > temp2set) {
-		heater_pwm = MIN_PWM;
-	}
+		if(temp < temp2set) {
+			heater_pwm = MAX_PWM;
+		} else if(temp > temp2set) {
+			heater_pwm = MIN_PWM;
+		}
 #endif // USE_PID
-	Peltier.setSpeed(heater_pwm); // motor full-speed "backward"
-	if(heater_pwm > MAX_PWM/2) {
-		digitalWrite(FAN_PIN, LOW);
-	} else if(heater_pwm < 0) {
-		digitalWrite(FAN_PIN, HIGH);
+		if(heater_pwm > MAX_PWM/2) {
+			digitalWrite(FAN_PIN, LOW);
+		} else if(heater_pwm < 0) {
+			digitalWrite(FAN_PIN, HIGH);
+		}
 	}
+	Peltier.setSpeed(heater_pwm);
 }
 
 int16_t i2c_data_buf = UNKNOWN_VAL;
@@ -187,13 +204,47 @@ void requestEvent()
 	i2c_data_buf = UNKNOWN_VAL;
 }
 
+void emergency_test()
+{
+	if(!emergency_stop) {
+		if(old_temp == UNKNOWN_VAL) {
+			old_temp = temp;
+			old_heater_pwm = heater_pwm;
+			emergency_last_ts = millis();
+		} else {
+			if(millis() - emergency_last_ts > 10000) {
+				emergency_last_ts = millis();
+				if(old_heater_pwm > 10 && heater_pwm > 10 && temp - old_temp <= 0) {
+					emergency_stop = true;
+				} else if(old_heater_pwm < 10 && heater_pwm < 10 && temp - old_temp >= 0) {
+					emergency_stop = true;
+				}
+				old_temp = temp;
+				old_heater_pwm = heater_pwm;
+			} else {
+				if(old_heater_pwm*heater_pwm <= 0) {
+					old_temp = temp;
+					old_heater_pwm = heater_pwm;
+					emergency_last_ts = millis();
+				}
+			}
+		}
+	} else {
+		emergency_led_on = !emergency_led_on;
+		digitalWrite(EMERGENCY_LED_PIN, emergency_led_on ? HIGH : LOW);
+	}
+}
+
 // sample time of PID
 Ticker process_timer(process_proc, 200, 0, MILLIS);
 Ticker status_timer(display_status, 1000, 0, MILLIS);
+Ticker emergency_timer(emergency_test, 1000, 0, MILLIS);
 
 void setup()
 {
 	Serial.begin(115200);
+	pinMode(EMERGENCY_LED_PIN, OUTPUT);
+	digitalWrite(EMERGENCY_LED_PIN, LOW);
 	pinMode(FAN_PIN, OUTPUT);
 	digitalWrite(FAN_PIN, LOW);
 	analogReference(INTERNAL);
@@ -214,10 +265,12 @@ void setup()
 
 	process_timer.start();
 	status_timer.start();
+	emergency_timer.start();
 }
 
 void loop()
 {
 	process_timer.update();
 	status_timer.update();
+	emergency_timer.update();
 }
