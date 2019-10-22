885829bf6f1bf66d3cc41493510503ce9bbd222c
Your Name <n800sau@gmail.com>
2019-10-22 21:21:02 +1100

    working esp32cam

--- ard_esp32/webcam/serial.sh
+++ ard_esp32/webcam/serial.sh
@@ -1,7 +1,7 @@
 source vars.sh
 
-../../bin/miniterm.py -p $DEV -b 115200 --rts=0 --dtr=0 &> serial.log
-#../../bin/miniterm.py -p $DEV -b 115200 --rts=0 --dtr=0
+#../../bin/miniterm.py -p $DEV -b 115200 --rts=0 --dtr=0 &> serial.log
+../../bin/miniterm.py -p $DEV -b 115200 --rts=0 --dtr=0
 
 #platformio run -t monitor --upload-port $DEV &>serial.log
 
