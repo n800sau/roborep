2b7aabe87fdeff294923f6b8d0989d15472ab286
n800s <n800sau@gmail.com>
2019-12-21 19:25:03 +1100

    variable spindel

--- ino/grbl/serial.sh
+++ ino/grbl/serial.sh
@@ -1,3 +1,6 @@
 source vars.sh
 #../../bin/miniterm.py -p $DEV -b 115200 &> serial.log
 ../../bin/miniterm.py -p $DEV -b 115200 --rts 0 --dtr 0
+
+# connect to socat virtual tty
+#../../bin/miniterm.py -p $HOME/dev/ttyV0 -b 115200
