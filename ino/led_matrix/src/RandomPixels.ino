d72a017edf5fbf8cf35510dd73ab749b7f7f0a32
Your Name <n800sau@gmail.com>
2019-12-29 12:39:37 +1100

    failed attempgt to use internal oscillator

--- ino/led_matrix/src/RandomPixels.ino
+++ ino/led_matrix/src/RandomPixels.ino
@@ -11,6 +11,8 @@
 // Global variables
 byte led_pins[] = {4, 5, 6, 7, 8, 9, 10, 11}; // Pins for LEDs
 
+const int led_pin_count = sizeof(led_pins)/sizeof(led_pins[0]);
+
 int row = 0, col = 0;
 bool running = false;
 
@@ -23,6 +25,9 @@ void wakeUp()
 void gotoSleep()
 {
 	digitalWrite(LED_BUILTIN, LOW);
+	for(unsigned i=0; i<led_pin_count; i++) {
+		digitalWrite(led_pins[i], LOW);
+	}
 	attachInterrupt(digitalPinToInterrupt(BTN_PIN), wakeUp, LOW);
 	LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF); 
 	detachInterrupt(digitalPinToInterrupt(BTN_PIN));
@@ -34,12 +39,30 @@ void setup()
 	Output.begin(115200);
 	pinMode(BTN_PIN, INPUT_PULLUP);
 	pinMode(LED_BUILTIN, INPUT_PULLUP);
-
+	Output.print("pins count:");
+	Output.println(sizeof(led_pins));
+	Output.flush();
 	// Initialize and clear display
 	Plex.init(led_pins);
 	Plex.clear();
 	Plex.display();
 	gotoSleep();
+}
+
+void set_all()
+{
+	for(int r=0; r<N_ROWS; r++) {
+		for(int c=0; c<N_COLS; c++) {
+			Plex.pixel(c, r, HIGH);
+		}
+	}
+	Plex.display();
+}
+
+void clear_all()
+{
+	Plex.clear();
+	Plex.display();
 }
 
 void loop()
@@ -58,18 +81,18 @@ void loop()
 			}
 			if(row >= N_ROWS) {
 				row = 0;
-				for(int c=0; c<N_COLS; c++) {
-					Plex.pixel(c, 3, HIGH);
-				}
-				Plex.display();
 				running = false;
-				delay(3000);
-				Plex.clear();
-				Plex.display();
+				for(int i=0; i<5; i++) {
+					set_all();
+					delay(200);
+					clear_all();
+					delay(200);
+				}
 				gotoSleep();
 			}
 		}
-		delay(100);
+		delay(2*60000L/((N_ROWS-1)*N_COLS));
+//		delay(100);
 	} else {
 		if(digitalRead(BTN_PIN) == LOW) {
 			running = true;
--- ino/led_matrix/upload.sh
+++ ino/led_matrix/upload.sh
@@ -1,4 +1,6 @@
 source vars.sh
 
-platformio run -t upload --upload-port $DEV
+#platformio run -t upload --upload-port $DEV -e internal1M &>upload.log
+#platformio run -t upload --upload-port $DEV -e internal1M
+platformio run -t upload --upload-port $DEV -e uno
 echo $?
