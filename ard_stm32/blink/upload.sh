d1bddb0a7a2d3c560d1cb4f4fa9e51d044019594
Your Name <n800sau@gmail.com>
2020-01-09 21:44:12 +1100

    some adj

--- ard_stm32/blink/upload.sh
+++ ard_stm32/blink/upload.sh
@@ -5,7 +5,8 @@ export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/n800s/.platformio/packages/tool-
 #../../ino/set_stm32_mode/set_pmode.py
 
 #DEV=/dev/ttyUSB1
-#platformio run -t upload --upload-port $DEV
+#platformio run -t upload --upload-port $DEV -e maple
+#platformio run -t upload --upload-port $DEV -e demo_f030f4
 #platformio run -v -t upload
 #stm32flash -g 0x08000000 -b 115200 -w ".pio/build/maple/firmware.bin" "/dev/ttyUSB1"
 #/usr/bin/stm32flash -g 0x08000000 -i 'rts,-dtr,dtr' -b 115200 -w ".pio/build/maple/firmware.bin" "$DEV"
@@ -14,10 +15,10 @@ export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/n800s/.platformio/packages/tool-
 # flasher working with usb-serial rts -> 10K -> BOOT0 <- 10K <- GND, and DTR -> RESET <- 3.3v
 # pip install -U stm32loader
 
-platformio run &> upload.log && \
-stm32loader -a 0x08000000 -e -w -f F1 -p "$DEV" ".pio/build/maple/firmware.bin" &>upload.log
+#platformio run &> upload.log &&
+#stm32loader -a 0x08000000 -e -w -f F1 -p "$DEV" ".pio/build/maple/firmware.bin" &>upload.log
 
-#platformio run && \
+#platformio run -e maple && \
 #stm32loader -a 0x08000000 -e -w -f F1 -p "$DEV" ".pio/build/maple/firmware.bin"
 
 echo $?
