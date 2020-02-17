6ce3c0847cd7474f287fb7bd627c52feb81dc6f7
Your Name <n800sau@gmail.com>
2020-02-17 21:00:09 +1100

    uncommented

--- ard_stm32/grbl_passthrough/upload.sh
+++ ard_stm32/grbl_passthrough/upload.sh
@@ -2,8 +2,8 @@ source vars.sh
 
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/home/n800s/.platformio/packages/tool-stlink/lib
 
-#platformio run && tm32loader -a 0x08000000 -e -w -f F1 -p "$DEV" ".pio/build/maple/firmware.bin"
+platformio run && stm32loader -a 0x08000000 -e -w -f F1 -p "$DEV" ".pio/build/maple/firmware.bin"
 
-platformio run &> upload.log && stm32loader -a 0x08000000 -e -w -f F1 -p "$DEV" ".pio/build/maple/firmware.bin" &>upload.log
+#platformio run &> upload.log && stm32loader -a 0x08000000 -e -w -f F1 -p "$DEV" ".pio/build/maple/firmware.bin" &>upload.log
 
 echo $?
