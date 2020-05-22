72cdb5deadf058a1d1653656bfcfbf2c803ef079
n800s <n800sau@gmail.com>
2020-05-22 16:41:02 +1000

    first working example

---
+++ ard_esp8266/MQ135sensor/post.sh
@@ -0,0 +1,2 @@
+#wget -O posted.html --post-data 'period=10000&submit=calibrate' http://mq135.local/write_settings.php
+wget -O posted.html --post-data 'period=10000&submit=calibrate' http://192.168.1.133/write_settings.php
--- ard_esp8266/MQ135sensor/src/main.cpp
+++ ard_esp8266/MQ135sensor/src/main.cpp
@@ -8,35 +8,47 @@
 #include <time.h>
 #include <blinker.h>
 #include <StreamString.h>
+#include <MQUnifiedsensor.h>
+#include <AM2320.h>
 
 #include "config.h"
 
+#define SETTINGS_INIT_SUM 0x1534
+
 const char *ssid = WIFI_SSID;
 const char *password = WIFI_PASSWORD;
-#define HOSTNAME "mq2"
+
+#define HOSTNAME "mq135"
 
 #define LED_PIN 15
 Blinker blinker;
 
-Ticker ticker;
+Ticker send_ticker;
 
-String sensor_id = "MQ135";
-
-#define SETTINGS_INIT_SUM 0x1234
-
-/************************Hardware Related Macros************************************/
-#define         RL_VALUE_MQ2                 (5. * (47 + 10)/(5 + 47 + 10))     //define the load resistance on the board, in kilo ohms
-#define         RO_CLEAN_AIR_FACTOR_MQ2      (9.577)  //RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/R0,
-                                                     //which is derived from the chart in datasheet
+#define VOLTAGE_RESOLUTION (1./(10./47.))
+#define RL_MODULE 10.
+#define RL RL_MODULE*(47+10)/(RL_MODULE+47+10)
 
 /***********************Software Related Macros************************************/
-#define         CALIBARAION_SAMPLE_TIMES     (50)    //define how many samples you are going to take in the calibration phase
+#define         CALIBRATION_SAMPLE_TIMES     (50)    //define how many samples you are going to take in the calibration phase
 #define         CALIBRATION_SAMPLE_INTERVAL  (500)   //define the time interal(in milisecond) between each samples in the
                                                      //cablibration phase
+String sensor_id = "MQ135";
+MQUnifiedsensor mq135("ESP8266", VOLTAGE_RESOLUTION, 10, A0, "MQ-135");
 
-/**********************Application Related Macros**********************************/
-#define         GAS_CO2                       (0)
-/*****************************Globals************************************************/
+// Create an instance of sensor
+AM2320 th_sensor;
+
+#define RatioMQ135CleanAir 3.6 // RS / R0 = 3.6 ppm
+
+// Parameters to model temperature and humidity dependence
+#define CORA = 0.00035
+#define CORB = 0.02718
+#define CORC = 1.39538
+#define CORD = 0.0018
+#define CORE = -0.003333333
+#define CORF = -0.001923077
+#define CORG = 1.130128205
 
 // Multicast declarations
 IPAddress ipMulti(239, 0, 0, 57);
@@ -50,66 +62,22 @@ boolean udpConnected = false;
 
 typedef struct __attribute__((packed)) {
 	int32_t data_send_period;
-	double rs_air;
-	double r0;
+	float r0;
 	uint16_t checksum;
 } settings_t;
 
-settings_t settings = {.data_send_period = 5000, .rs_air = 0, .r0 = 10};
+settings_t settings = {.data_send_period = 5000, .r0 = 10};
 
 typedef struct {
 	time_t ts;
-	double voltage;
-	double res;
+	float co2;
+	float h;
+	float t;
 } val_t;
 
-val_t cur_data = {0, 0};
+val_t cur_data = {.ts=0, .co2=0, .h=0, .t=0};
 
-double sensorMinValue = -1, sensorMaxValue = -1;
-// kOm between sensor pin and GND
-const double boardResistance = 10 + 47;
-
-double readVoltage()
-{
-	double sensorValue = 0;
-	sensorMinValue = -1;
-	sensorMaxValue = -1;
-	const double div_coef = 10 / boardResistance;
-	for(int x = 0 ; x < 500 ; x++) //Start for loop 
-	{
-		int v = analogRead(A0);
-		sensorValue += v;
-		if(sensorMinValue < 0 || sensorMinValue > v) {
-			sensorMinValue = v;
-		}
-		if(sensorMaxValue < 0 || sensorMaxValue < v) {
-			sensorMaxValue = v;
-		}
-	}
-	sensorMinValue = sensorMinValue / 1023 / div_coef;
-	sensorMaxValue = sensorMaxValue / 1023 / div_coef;
-//	Serial.print("min:");
-//	Serial.print(sensorMinValue);
-//	Serial.print(", max:");
-//	Serial.println(sensorMaxValue);
-	return sensorValue / 500 / 1023 / div_coef;
-}
-
-
-/****************** MQResistance ****************************************
-Input:   voltage - calculated voltage adc
-Output:  the calculated sensor resistance
-Remarks: The sensor and the load resistor forms a voltage divider. Given the voltage
-         across the load resistor and its resistance, the resistance of the sensor
-         could be derived.
-************************************************************************************/
-double MQResistance(double voltage=0)
-{
-//	if(!voltage) {
-		voltage = readVoltage();
-//	}
-	return RL_VALUE_MQ2 * (5 - voltage);
-}
+float sensorMinValue = -1, sensorMaxValue = -1;
 
 void load_settings()
 {
@@ -161,10 +129,10 @@ Remarks: This function assumes that the sensor is in clean air. It use
 	       and then divides it with RO_CLEAN_AIR_FACTOR. RO_CLEAN_AIR_FACTOR is about
 	       10, which differs slightly between different sensors.
 ************************************************************************************/
-class MQCalibrationClass {
+class MQCalibration {
 
 		int step;
-		double rs_air;
+		float r0;
 		Ticker t;
 	public:
 
@@ -174,18 +142,20 @@ class MQCalibrationClass {
 			if(rs) {
 				Serial.println("Calibration already running");
 			} else {
-				Serial.printf("Calibration started (it will takes %d secs)\n", (CALIBARAION_SAMPLE_TIMES*CALIBRATION_SAMPLE_INTERVAL)/1000);
-				rs_air = 0;
+				Serial.printf("Calibration started (it will takes %d secs)\n", (CALIBRATION_SAMPLE_TIMES*CALIBRATION_SAMPLE_INTERVAL)/1000);
+				r0 = 0;
 				step = 0;
-				t.attach_ms(CALIBRATION_SAMPLE_INTERVAL, std::bind(&MQCalibrationClass::do_step, this));
+				t.attach_ms(CALIBRATION_SAMPLE_INTERVAL, std::bind(&MQCalibration::do_step, this));
 			}
 			return !rs;
 		}
 
 		void do_step()
 		{
-			rs_air += MQResistance();
-			if((++step) >= CALIBARAION_SAMPLE_TIMES)
+			mq135.update();
+			r0 += mq135.calibrate(RatioMQ135CleanAir);
+
+			if((++step) >= CALIBRATION_SAMPLE_TIMES)
 			{
 				stop();
 			}
@@ -194,10 +164,15 @@ class MQCalibrationClass {
 		void stop()
 		{
 			t.detach();
-			settings.rs_air = rs_air/CALIBARAION_SAMPLE_TIMES;              //calculate the average value
-			settings.r0 = settings.rs_air/RO_CLEAN_AIR_FACTOR_MQ2;                      //RS_AIR_val divided by RO_CLEAN_AIR_FACTOR yields the R0
-			save_settings();
-			Serial.printf("Calibration finished (rs_air:%.2f, r0:%.2f)", settings.rs_air, settings.r0);
+			settings.r0 = r0/CALIBRATION_SAMPLE_TIMES;              //calculate the average value
+			if(isinf(r0)) {
+				Serial.println("Warning: Conection issue founded, R0 is infite (Open circuit detected) please check your wiring and supply");
+			} else if(r0 == 0) {
+				Serial.println("Warning: Conection issue founded, R0 is zero (Analog pin with short circuit to ground) please check your wiring and supply");
+			} else {
+				save_settings();
+				Serial.printf("Calibration finished (r0:%.2f)", settings.r0);
+			}
 		}
 
 		bool is_running()
@@ -207,48 +182,54 @@ class MQCalibrationClass {
 
 		int percent()
 		{
-			return (step * 100)/CALIBARAION_SAMPLE_TIMES;
+			return (step * 100)/CALIBRATION_SAMPLE_TIMES;
 		}
 };
 
-MQCalibrationClass calibr;
+
+//MQCalibration calibr;
 
 // 25 sec
-bool MQCalibration()
+void run_calibration()
 {
-	return calibr.start();
-}
 
-/*****************************  MQGetGasPercentage **********************************
-Input:   rs_ro_ratio - Rs divided by R0
-       gas_id      - target gas type
-Output:  ppm of the target gas
-Remarks: This function uses different equations representing curves of each gas to
-       calculate the ppm (parts per million) of the target gas.
-************************************************************************************/
-int MQGetGasPercentage(double rs_ro_ratio, int gas_id)
-{
-	if ( gas_id == GAS_CO2 ) {
-		return pow(10,(-1.92 * log10(rs_ro_ratio) / -0.42));
+	Serial.print("Calibrating please wait.");
+	float calcR0 = 0;
+	for(int i=0; i<10; i++)
+	{
+		mq135.update(); // Update data, the arduino will be read the voltage on the analog pin
+		calcR0 += mq135.calibrate(RatioMQ135CleanAir);
+		Serial.print(".");
 	}
-	return 0;
+	Serial.println("  done!.");
+	settings.r0 = calcR0 / 10.;
+	Serial.print("R0=");
+	Serial.println(settings.r0);
+	save_settings();
+
+//	calibr.start();
 }
 
 void printVals(Print &p=Serial, String sep=" ")
 {
-	if(settings.rs_air) {
-		double val = MQResistance()/settings.r0;
+	if(settings.r0) {
 		p.print("co2:");
-		p.print(MQGetGasPercentage(val, GAS_CO2));
+		p.print(cur_data.co2);
 		p.print( "ppm" );
+		p.print(sep);
+		p.print("h:");
+		p.print(cur_data.h);
+		p.print(sep);
+		p.print("t:");
+		p.print(cur_data.t);
 		p.print(sep);
 	}
 	p.println();
-	if(calibr.is_running()) {
-		p.printf("Calibrating...%d%%", calibr.percent());
-		p.print(sep);
-		p.println();
-	}
+//	if(calibr.is_running()) {
+//		p.printf("Calibrating...%d%%", calibr.percent());
+//		p.print(sep);
+//		p.println();
+//	}
 }
 
 const String home_link = "<a href=\"/\">Home</a>";
@@ -298,27 +279,28 @@ void send_udp_string(String js)
 
 void send_data()
 {
-	if(wifiConnected && udpConnected && settings.rs_air) {
-		cur_data.voltage = readVoltage();
-		cur_data.res = MQResistance(cur_data.voltage);
-		cur_data.ts = time(NULL);
-		Serial.print("V=");
-		Serial.println(cur_data.voltage);
-		double val = cur_data.res/settings.r0;
+	if(wifiConnected && udpConnected && settings.r0) {
+		Serial.print("t=");
+		Serial.println(cur_data.t);
+		Serial.print("h=");
+		Serial.println(cur_data.h);
+		Serial.print("co2=");
+		Serial.println(cur_data.co2);
 		String js = "{\"sensor_id\":\"" + sensor_id + "\",\"ts\":" + String(cur_data.ts) +
-			",\"rawval\":" + String(cur_data.voltage) + ", \"r0\":" + String(settings.r0) +
-			",\"rs_air\":" + String(settings.rs_air) + ",\"res\":" + String(cur_data.res);
-		js += ",\"co2\":" + String(MQGetGasPercentage(val, GAS_CO2));
+			",\"temperature\":" + String(cur_data.t) + ",\"humidity\":" + String(cur_data.h);
+		js += ",\"co2\":" + String(cur_data.co2);
 		js += "}";
 		send_udp_string(js);
 		blinker.blink(3);
 	}
 }
 
-void make_ticker()
+void make_send_ticker()
 {
-	ticker.detach();
-	ticker.attach_ms(settings.data_send_period, send_data);
+	if(send_ticker.active()) {
+		send_ticker.detach();
+	}
+	send_ticker.attach_ms(settings.data_send_period, send_data);
 }
 
 void handleForm()
@@ -331,12 +313,12 @@ void handleForm()
 		if(v > 1000) {
 			settings.data_send_period = v;
 			save_settings();
-			make_ticker();
+			make_send_ticker();
 			message = "Success";
 			String submit_type = server.arg("submit");
 			Serial.printf("submit_type:%s\n", submit_type.c_str());
 			if(submit_type == "calibrate") {
-				MQCalibration(); //Calibrating the sensor. Please make sure the sensor is in clean air
+				run_calibration(); //Calibrating the sensor. Please make sure the sensor is in clean air
 			}
 		} else {
 			message = "Error in form";
@@ -351,7 +333,7 @@ void handleRoot()
 	struct tm tmstruct;
 	StreamString page;
 	localtime_r(&cur_data.ts, &tmstruct);
-	page.printf("V: %.2f (range: %.2f..%.2f)<br/>%d-%02d-%02d %02d:%02d:%02d UTC<br>ts: %li<br/>", cur_data.voltage, sensorMinValue, sensorMaxValue,
+	page.printf("co2: %.2f (range: %.2f..%.2f)<br/>%d-%02d-%02d %02d:%02d:%02d UTC<br>ts: %li<br/>", cur_data.co2, sensorMinValue, sensorMaxValue,
 		(tmstruct.tm_year) + 1900, (tmstruct.tm_mon) + 1, tmstruct.tm_mday, tmstruct.tm_hour, tmstruct.tm_min, tmstruct.tm_sec, cur_data.ts);
 	printVals(page, "<br/>");
 	server.send(200, "text/html", wrap_html(page, reload_script));
@@ -392,7 +374,6 @@ boolean connectWifi()
 	return state;
 }
 
-
 // connect to UDP returns true if successful or false if not
 boolean connectUDP()
 {
@@ -411,6 +392,40 @@ boolean connectUDP()
 	return state;
 }
 
+void th_update()
+{
+	// sensor.measure() returns boolean value
+	// - true indicates measurement is completed and success
+	// - false indicates that either sensor is not ready or crc validation failed
+	//	 use getErrorCode() to check for cause of error.
+	if (th_sensor.measure()) {
+		cur_data.t = th_sensor.getTemperature();
+		cur_data.h = th_sensor.getHumidity();
+	} else {
+		int errorCode = th_sensor.getErrorCode();
+		switch (errorCode) {
+			case 1:
+				Serial.println("ERR: Sensor is offline");
+				break;
+			case 2:
+				Serial.println("ERR: CRC validation failed.");
+				break;
+		}
+	}
+}
+
+void sensor_update()
+{
+//	if(!calibr.is_running()) {
+		th_update();
+		mq135.setR0(settings.r0);
+		mq135.update();
+		cur_data.ts = time(NULL);
+		mq135.setA(110.47); mq135.setB(-2.862); // Configurate the ecuation values to get CO2 concentration 
+		cur_data.co2 = mq135.readSensor(); // Sensor will read PPM concentration using the model and a and b values setted before or in the setup
+//	}
+}
+
 void setup()
 {
 	blinker.begin(LED_PIN);
@@ -419,6 +434,14 @@ void setup()
 	Serial.println();
 	WiFi.mode(WIFI_STA);
 	load_settings();
+
+	mq135.init();
+	mq135.setRegressionMethod(1);
+	Serial.print("RL=");
+	Serial.print(RL);
+	mq135.setRL(RL);
+
+	th_sensor.begin();
 
 	server.on("/", handleRoot);
 	server.on("/index.html", handleRoot);
@@ -429,10 +452,10 @@ void setup()
 	server.onNotFound(handleNotFound);
 	server.begin();
 	Serial.println("HTTP server started");
-	make_ticker();
+	make_send_ticker();
 	digitalWrite(LED_PIN, LOW);
-	if(!settings.rs_air) {
-		MQCalibration();
+	if(!settings.r0) {
+		run_calibration();
 	}
 	Serial.print("Ro=");
 	Serial.print(settings.r0);
@@ -465,6 +488,6 @@ void loop()
 			}
 		}
 	}
+	sensor_update();
 	delay(10);
 }
-
