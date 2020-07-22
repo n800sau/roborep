654ed54e936956fa697f137f5289e5bea74184be
Your Name <n800sau@gmail.com>
2020-07-22 20:52:20 +1000

    platformio.ini added

--- ino/mpu6050/upload.sh
+++ ino/mpu6050/upload.sh
@@ -1,7 +1,8 @@
 source vars.sh
 
+platformio run -e LGT8F328P -v -t upload --upload-port $DEV
 #platformio run -t upload --upload-port $DEV
-platformio run -t upload --upload-port $DEV &>upload.log
+#platformio run -t upload --upload-port $DEV &>upload.log
 echo $?
 
 
