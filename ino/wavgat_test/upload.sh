98c948d143e0027da2548151b98634e7c0bf16ec
Your Name <n800sau@gmail.com>
2020-07-22 20:52:39 +1000

    platformio.ini added

--- ino/wavgat_test/upload.sh
+++ ino/wavgat_test/upload.sh
@@ -1,7 +1,7 @@
 source vars.sh
 
-#platformio run -e LGT8F328P -v -t upload --upload-port $DEV
-platformio run -e LGT8F328P -v -t upload --upload-port $DEV &>upload.log
+platformio run -e LGT8F328P -v -t upload --upload-port $DEV
+#platformio run -e LGT8F328P -v -t upload --upload-port $DEV &>upload.log
 
 #platformio run -e arduino_uno  -v -t upload --upload-port $DEV
 #platformio run -e arduino_uno -t upload --upload-port $DEV &>upload.log
