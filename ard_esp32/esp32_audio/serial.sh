202c0ed10a67af009912fd60912e6432da327ed3
n800sau <n800sau@mail.com>
2020-12-28 11:02:54 +1100

    esp32_audio added

---
+++ ard_esp32/esp32_audio/serial.sh
@@ -0,0 +1,9 @@
+source vars.sh
+
+#./bumpRTS $DEV
+../../bin/miniterm.py -p $DEV -b 115200 --rts=0 --dtr=0
+#../../bin/miniterm.py -p $DEV -b 115200 --rts=0 --dtr=0 &> serial.log
+#../../bin/miniterm.py -p $DEV -b 115200 --exit-char=0x1b --rts=0 --dtr=1 &> serial.log
+
+#platformio run -t monitor --upload-port $DEV &>serial.log
+
