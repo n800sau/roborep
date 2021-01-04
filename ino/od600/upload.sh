1b94b335cf142b2b8d7abb164a67d5c3c27aa0a3
n800sau <n800sau@mail.com>
2021-01-04 19:32:15 +1100

    minor

--- ino/od600/upload.sh
+++ ino/od600/upload.sh
@@ -1,6 +1,7 @@
 source vars.sh
 
 platformio run -v -e LGT8F328P -t upload --upload-port $DEV
+#platformio run -v -e uno -t upload --upload-port $DEV
 #platformio run -t upload --upload-port $DEV &>upload.log
 echo $?
 
