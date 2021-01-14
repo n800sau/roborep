7df94977d3e5fd7eb7633e77ffe69626c2af42f8
n800s <n800sau@gmail.com>
2021-01-15 10:07:30 +1100

    reading temp and hum added

--- ino/encubator/src/main.ino
+++ ino/encubator/src/main.ino
@@ -1,9 +1,39 @@
 /*
 
 am2320
-sda - A0
-scl - A1
+sda - A4
+scl - A5
 
 heater pwm - 3
 
 */
+
+#include <AM2320.h>
+
+AM2320 am2320;
+
+void setup()
+{
+	Serial.begin(115200);
+	am2320.begin();
+}
+
+void loop()
+{
+	if (am2320.measure()) {
+		Serial.print("Temperature: ");
+		Serial.println(am2320.getTemperature());
+		Serial.print("Humidity: ");
+		Serial.println(am2320.getHumidity());
+	}
+	else
+	{
+		int errorCode = am2320.getErrorCode();
+		switch (errorCode)
+		{
+			case 1: Serial.println("ERR: am2320 is offline"); break;
+			case 2: Serial.println("ERR: CRC validation failed."); break;
+		}
+	}
+	delay(500);
+}
