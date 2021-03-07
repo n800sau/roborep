9cf8143abe2330adb5730f1f07519b10a71633a9
n800sau <n800sau@mail.com>
2021-03-07 19:30:28 +1100

    some changes

--- ino/incubator/upload.sh
+++ ino/incubator/upload.sh
@@ -7,7 +7,9 @@ source vars.sh
 #sleep 5
 #platformio run -v -t upload -e esp --upload-port $DEV
 
-platformio run -t upload -e esp --upload-port $DEV &>upload.log
+#python3 "/home/n800s/.platformio/packages/tool-esptoolpy/esptool.py" --chip esp8266 --port "/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0" --baud 115200 write_flash 0x0 .pio/build/esp/firmware.bin
+
+platformio run -v -t upload -e esp --upload-port $DEV &>upload.log
 #~/.platformio/packages/tool-esptoolpy/esptool.py --chip esp8266 --port $DEV erase_flash
 echo $?
 
--- ino/incubator/upload_all.sh
+++
@@ -1,4 +0,0 @@
-#cd src/data && for file in `ls -A1`; do curl -vvv --digest -F "data=@$file" -F "filename=/$file" --user admin:admin --trace-ascii ../../trace.txt http://192.168.1.89/edit; done && cd -
-#cd src/data && for file in `ls -A1`; do http -A digest -a admin:admin pie.dev/digest-auth/httpie/username/password -f POST pie.dev/post data@"$file" http://192.168.1.89/edit; done && cd -
-cd src/data && for file in `ls -A1`; do curl -v --user admin:admin --digest -X PUT -T "$file" "http://192.168.1.89/edit?path=/$file"; done && cd -
-
