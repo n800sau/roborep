0fa82384774595f65f621c9aec6607b45419b585
n800s <n800sau@gmail.com>
2021-04-30 12:22:28 +1000

    config.json detailed

--- ino/incubator.ng/lib/FSBrowserNG/src/FSWebServerLib.cpp
+++ ino/incubator.ng/lib/FSBrowserNG/src/FSWebServerLib.cpp
@@ -361,6 +361,11 @@ bool AsyncFSWebServer::load_user_config(String name, String &value) {
 	Serial.println(temp);
 #endif
 
+	if(!json.containsKey(name)) {
+		DEBUGLOG("Has no \"%s\" in config file\r\n", name);
+		return false;
+	}
+
 	value = json[name].asString();
 
 	DEBUGLOG("Data initialized.\r\n");
