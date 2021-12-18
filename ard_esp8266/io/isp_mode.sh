701ac79c48f701358d23e0eeefed3f6e0d8704ce
n800sau <n800sau@mail.com>
2021-12-18 11:42:37 +1100

    misc

--- ard_esp8266/io/isp_mode.sh
+++ ard_esp8266/io/isp_mode.sh
@@ -2,14 +2,15 @@ LOGNAME=isp_mode.log
 echo  > $LOGNAME
 # 5 - boot0
 # 6 - reset
-wget -q -O - http://192.168.1.87/mode/5/o >> $LOGNAME
-wget -q -O - http://192.168.1.87/mode/6/o >> $LOGNAME
-wget -q -O - http://192.168.1.87/digital/6/1 >> $LOGNAME
-wget -q -O - http://192.168.1.87/digital/5/1 >> $LOGNAME
-wget -q -O - http://192.168.1.87/digital/6/0 >> $LOGNAME
+URI=http://192.168.1.178:8080
+wget -q -O - $URI/mode/5/o >> $LOGNAME
+wget -q -O - $URI/mode/6/o >> $LOGNAME
+wget -q -O - $URI/digital/6/1 >> $LOGNAME
+wget -q -O - $URI/digital/5/1 >> $LOGNAME
+wget -q -O - $URI/digital/6/0 >> $LOGNAME
 #sleep 1
-wget -q -O - http://192.168.1.87/digital/6/1 >> $LOGNAME
+wget -q -O - $URI/digital/6/1 >> $LOGNAME
 #sleep 1
-#wget -q -O - http://192.168.1.87/digital/5/0 >> $LOGNAME
+#wget -q -O - $URI/digital/5/0 >> $LOGNAME
 
 
