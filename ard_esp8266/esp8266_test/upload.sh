7bc22b85d2a7d610f1bbd384495ea35695ae3712
n800sau <n800sau@mail.com>
2021-12-18 11:40:46 +1100

    misc

--- ard_esp8266/esp8266_test/upload.sh
+++ ard_esp8266/esp8266_test/upload.sh
@@ -1,7 +1,11 @@
 source vars.sh
 
-#platformio run -v -t upload --upload-port $DEV
-platformio run -t upload --upload-port $DEV &>upload.log
+platformio run -v -t upload --upload-port $DEV
+#platformio run -t upload --upload-port $DEV &>upload.log
+
+#~/.local/bin/esptool.py --before no_reset --after soft_reset --chip esp8266 --port "/dev/serial/by-id/usb-1a86_USB2.0-Ser_-if00-port0" --baud 115200 write_flash 0x0 .pio/build/esp01/firmware.bin
+#~/.local/bin/esptool.py --before usb_reset --after soft_reset --chip esp8266 --port "/dev/serial/by-id/usb-1a86_USB2.0-Ser_-if00-port0" --baud 74880 write_flash 0x0 .pio/build/esp01/firmware.bin
+
 echo $?
 
 
