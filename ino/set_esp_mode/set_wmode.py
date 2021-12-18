5b2719f5aa6e0897917b0ed9a84d91c05cb40e86
n800s <n800sau@gmail.com>
2021-12-18 12:38:52 +1100

    set_*mode.py  added

---
+++ ino/set_esp_mode/set_wmode.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import sys, os, time
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lib_py'))
+
+from serial import Serial
+
+execfile(os.path.join(os.path.dirname(__file__), "vars.sh"))
+
+s_port = DEV
+s_baud = 115200
+
+ser = Serial(s_port, s_baud, timeout=5, writeTimeout=5)
+#print s_port, s_baud
+
+for i in range(5):
+	if ser.readline().strip() == 'Ready':
+		ser.write('w')
+		ser.flush()
+
+		print ser.readline()
+#		print ser.readline()
+		break
+
+
