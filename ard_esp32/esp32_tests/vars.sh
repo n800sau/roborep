d9a19ba2c289e58ab15b5830ff08ad618880c022
n800sau <n800sau@mail.com>
2021-12-18 11:39:58 +1100

    added another DEV

--- ard_esp32/esp32_tests/vars.sh
+++ ard_esp32/esp32_tests/vars.sh
@@ -1,3 +1,4 @@
 #DEV=/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0
 #DEV=/dev/serial/by-id/usb-Silicon_Labs_CP2104_USB_to_UART_Bridge_Controller_018614EC-if00-port0
 DEV=/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0
+#DEV=/dev/serial/by-id/usb-Silicon_Labs_CP2104_USB_to_UART_Bridge_Controller_02PL3R0K-if00-port0
