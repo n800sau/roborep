d5dd36f4a6e598422a94552c6955156dffe4d509
n800sau <n800sau@mail.com>
2021-12-19 13:29:09 +1100

    ->python3

--- ino/set_esp_mode/set_pmode.py
+++ ino/set_esp_mode/set_pmode.py
@@ -1,30 +1,27 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys, os, time
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lib_py'))
 
 from serial import Serial
 
-execfile(os.path.join(os.path.dirname(__file__), "vars.sh"))
+exec(open(os.path.join(os.path.dirname(__file__), "vars.sh")).read())
 
 s_port = DEV
 s_baud = 115200
 
 ser = Serial(s_port, s_baud, timeout=5, writeTimeout=5)
-#print s_port, s_baud
-
-time.sleep(1)
+ser.write(b'?')
+ser.flush()
 
 for i in range(5):
-	if ser.readline().strip() == 'Ready':
-		ser.write('p')
+	if ser.readline().decode().strip() == 'Ready':
+		ser.write(b'p')
 		ser.flush()
 
 		for i in range(100):
-			line = ser.readline().strip()
+			line = ser.readline().decode().strip()
 			print(line)
 			if line == 'end':
 				break
 		break
-
-
