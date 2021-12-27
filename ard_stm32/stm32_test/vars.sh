9a8055ec233a3daf21153f32833e8884f16e9b39
n800s <n800sau@gmail.com>
2021-12-27 13:53:16 +1100

    stm32_test added

---
+++ ard_stm32/stm32_test/vars.sh
@@ -0,0 +1,5 @@
+#DEV=/dev/serial/by-id/usb-LeafLabs_Maple-if00
+#DEV=/dev/serial/by-id/usb-Silicon_Labs_CP2102_USB_to_UART_Bridge_Controller_0001-if00-port0
+#DEV=/dev/ttyACM0
+#DEV=/dev/maple
+DEV='/dev/serial/by-id/usb-R2AXZ_Kirill_Kotyagin_Bluepill_Serial_Monster_9683010210134753384C4E00-if02'
