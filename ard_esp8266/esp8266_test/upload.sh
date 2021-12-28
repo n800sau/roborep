325b5c9c6b81d4148d998818a7d9818d1920eaec
n800s <n800sau@gmail.com>
2021-12-29 09:45:06 +1100

    auto set mode commented out

--- ard_esp8266/esp8266_test/upload.sh
+++ ard_esp8266/esp8266_test/upload.sh
@@ -3,11 +3,11 @@ source vars.sh
 #~/.local/bin/esptool.py --before no_reset --after soft_reset --chip esp8266 --port "/dev/serial/by-id/usb-1a86_USB2.0-Ser_-if00-port0" --baud 115200 write_flash 0x0 .pio/build/esp01/firmware.bin
 #~/.local/bin/esptool.py --before usb_reset --after soft_reset --chip esp8266 --port "/dev/serial/by-id/usb-1a86_USB2.0-Ser_-if00-port0" --baud 74880 write_flash 0x0 .pio/build/esp01/firmware.bin
 
-~/work/roborep/ino/set_esp_mode/set_pmode.py
+#~/work/roborep/ino/set_esp_mode/set_pmode.py
 
 #platformio run -v -t upload --upload-port $DEV
 platformio run -v -t upload --upload-port $DEV &>upload.log
 echo $?
 
-~/work/roborep/ino/set_esp_mode/set_wmode.py
+#~/work/roborep/ino/set_esp_mode/set_wmode.py
 
