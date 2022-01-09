50d53c4318f6c7e6e69ea78c53a1670398c8156d
n800s <n800sau@gmail.com>
2022-01-10 09:12:52 +1100

    stm mode

--- ino/set_esp_mode/setmode.py
+++ ino/set_esp_mode/setmode.py
@@ -3,8 +3,12 @@
 import sys, os, time
 sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'lib_py'))
 import tty, termios
-
+import argparse
 from serial import Serial
+
+parser = argparse.ArgumentParser(description='Programmer control')
+parser.add_argument('--mode', choices=['esp', 'stm'], default='esp')
+args = parser.parse_args()
 
 exec(open("vars.sh").read())
 
@@ -19,7 +23,7 @@ ser.flush()
 
 def loop():
 	while True:
-		print('Input:(p,w,r,i,x)', end='')
+		print('Input %s:(p,w,r,i,x)' % args.mode, end='')
 		sys.stdout.flush()
 		val = sys.stdin.read(1)
 #		print('CHAR:', val)
@@ -30,6 +34,11 @@ def loop():
 				break
 			if not val:
 				val = "#"
+			if args.mode == 'stm':
+				if val.upper() == 'W':
+					val = 'p'
+				elif val.upper() == 'P':
+					val = 'w'
 			ser.write(val.encode('ascii'))
 			ser.flush()
 
