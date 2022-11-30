17d647d5c2b6c55a3cc03fec36938638dcb31411
n800s <n800sau@gmail.com>
2022-11-30 13:21:44 +1100

    added more and transcoding

--- raspi_streamer/test_streamer.sh
+++ raspi_streamer/test_streamer.sh
@@ -1,1 +1,3 @@
-wget http://localhost:9000/cam/stream.mjpg
+#wget http://localhost:9000/cam/stream.mjpg
+#wget http://rpi2.local:9000/cam/stream.mjpg
+ffmpeg -i stream.mjpg -c:v vp9 -c:a libvorbis stream.mkv
