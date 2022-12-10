a3cfee37693e3ea444ec9ed29c85cb6ddd78502f
n800sau <n800sau@mail.com>
2022-12-10 12:20:38 +1100

    misc

--- ard_esp8266/io/serial.sh
+++ ard_esp8266/io/serial.sh
@@ -1,11 +1,11 @@
 source vars.sh
 
 #../../bin/miniterm.py -p $DEV -b 115200 --rts 0 --dtr 0 &>serial.log
-../../bin/miniterm.py -p $DEV -b 115200
-#../../bin/miniterm.py -p $DEV -b 9600 --rts 0 --dtr 0 &>serial.log
-#../../bin/miniterm.py -p $DEV -b 74880 --rts 0 --dtr 0 &>serial.log
-#platformio device monitor -b 115200 &>runterm.log
-#platformio device monitor -b 115200
+#miniterm.py -p $DEV -b 115200
+#../../bin/miniterm.py -p $DEV -b 4800 --rts 0 --dtr 0 --raw
+#../../bin/miniterm.py -p $DEV -b 74880 --rts 0 --dtr 0
+#platformio device monitor -p $DEV -b 115200
+platformio device monitor -p $DEV -b 115200 --rts 0 --dtr 0
 
 #picocom -b 115200 $DEV
 
--- bt4/list.sh
+++ bt4/list.sh
@@ -1,1 +1,2 @@
 bluetoothctl list
+echo $?
