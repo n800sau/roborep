33f2933fa734621e8e1e8e365e2cbe8d5adc4b4b
n800sau <n800sau@mail.com>
2023-04-27 22:47:25 +1000

    python3 scripts

--- ino/set_stm32_mode/vars.sh
+++ ino/set_stm32_mode/vars.sh
@@ -1,2 +1,3 @@
 #DEV='/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A9E9P33R-if00-port0'
-DEV='/dev/serial/by-id/usb-www.freetronics.com_Eleven_64936333137351D06221-if00'
+#DEV='/dev/serial/by-id/usb-www.freetronics.com_Eleven_64936333137351D06221-if00'
+DEV='/dev/serial/by-id/usb-1a86_USB2.0-Serial-if00-port0'
