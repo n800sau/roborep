ab21e2e345ac269a0843edfefdc4bbff4112c7f4
n800sau <n800sau@mail.com>
2024-01-27 15:41:21 +1100

    more scripts

---
+++ ard_rpi2040/microros_test/vars.sh
@@ -0,0 +1,4 @@
+DEV='/dev/serial/by-id/usb-Raspberry_Pi_Picoprobe__CMSIS-DAP__E66130100F128E38-if01'
+#DEV='/dev/serial/by-id/usb-Raspberry_Pi_Pico_E6625C48933E4033-if00'
+#DEV='/dev/serial/by-id/usb-TinyUSB_TinyUSB_Device_123456-if00'
+#DEV='/dev/serial/by-id/usb-TinyUSB_TinyUSB_Device_123456-if02'
