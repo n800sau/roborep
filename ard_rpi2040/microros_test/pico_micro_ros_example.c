253c865d4ae7c0bbbceaae31ba79ad88064820dd
n800sau <n800sau@mail.com>
2024-01-29 13:14:23 +1100

    reinit on new agent connection

--- ard_rpi2040/microros_test/pico_micro_ros_example.c
+++ ard_rpi2040/microros_test/pico_micro_ros_example.c
@@ -96,9 +96,82 @@ void timer_callback(rcl_timer_t *timer, int64_t last_call_time)
 	uart_puts(UART_ID, "\nHello, uart interrupts\n");
 }
 
+#define RCCHECK(fn) { rcl_ret_t temp_rc = fn; if((temp_rc != RCL_RET_OK)){return false;}}
+#define EXECUTE_EVERY_N_MS(MS, X)  do { \
+  static volatile int64_t init = -1; \
+  if (init == -1) { init = uxr_millis();} \
+  if (uxr_millis() - init > MS) { X; init = uxr_millis();} \
+} while (0)
+
+enum states {
+  WAITING_AGENT,
+  AGENT_AVAILABLE,
+  AGENT_CONNECTED,
+  AGENT_DISCONNECTED
+} state;
+
+rclc_support_t support;
+rcl_node_t node;
+rcl_timer_t timer;
+rclc_executor_t executor;
+rcl_allocator_t allocator;
+rcl_publisher_t publisher;
+std_msgs__msg__Int32 msg;
+bool micro_ros_init_successful;
+
+
+bool create_entities()
+{
+  allocator = rcl_get_default_allocator();
+
+  // create init_options
+  RCCHECK(rclc_support_init(&support, 0, NULL, &allocator));
+
+  // create node
+  RCCHECK(rclc_node_init_default(&node, "pico_publisher", "", &support));
+
+  // create publisher
+  RCCHECK(rclc_publisher_init_best_effort(
+    &publisher,
+    &node,
+    ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
+    "pico_message"));
+
+  // create timer,
+  const unsigned int timer_timeout = 1000;
+  RCCHECK(rclc_timer_init_default(
+    &timer,
+    &support,
+    RCL_MS_TO_NS(timer_timeout),
+    timer_callback));
+
+  // create executor
+  executor = rclc_executor_get_zero_initialized_executor();
+  RCCHECK(rclc_executor_init(&executor, &support.context, 1, &allocator));
+  RCCHECK(rclc_executor_add_timer(&executor, &timer));
+
+  msg.data = 0;
+
+  return true;
+}
+
+void destroy_entities()
+{
+  rmw_context_t * rmw_context = rcl_context_get_rmw_context(&support.context);
+  (void) rmw_uros_set_context_entity_destroy_session_timeout(rmw_context, 0);
+
+  rcl_publisher_fini(&publisher, &node);
+  rcl_timer_fini(&timer);
+  rclc_executor_fini(&executor);
+  rcl_node_fini(&node);
+  rclc_support_fini(&support);
+}
+
+
 int main()
 {
 
+	state = WAITING_AGENT;
 	rmw_uros_set_custom_transport(
 		true,
 		NULL,
@@ -111,51 +184,35 @@ int main()
 	gpio_init(LED_PIN);
 	gpio_set_dir(LED_PIN, GPIO_OUT);
 
-	rcl_timer_t timer;
-	rcl_node_t node;
-	rcl_allocator_t allocator;
-	rclc_support_t support;
-	rclc_executor_t executor;
-
-	allocator = rcl_get_default_allocator();
-
-	// Wait for agent successful ping for 2 minutes.
-	const int timeout_ms = 1000; 
-	const uint8_t attempts = 120;
-
-	rcl_ret_t ret = rmw_uros_ping_agent(timeout_ms, attempts);
-
-	if (ret != RCL_RET_OK)
-	{
-		// Unreachable agent, exiting program.
-		return ret;
-	}
-
-	rclc_support_init(&support, 0, NULL, &allocator);
-
-	rclc_node_init_default(&node, "pico_node", "", &support);
-	rclc_publisher_init_default(
-		&publisher,
-		&node,
-		ROSIDL_GET_MSG_TYPE_SUPPORT(std_msgs, msg, Int32),
-		"pico_publisher");
-
-	rclc_timer_init_default(
-		&timer,
-		&support,
-		RCL_MS_TO_NS(1000),
-		timer_callback);
-
-	rclc_executor_init(&executor, &support.context, 1, &allocator);
-	rclc_executor_add_timer(&executor, &timer);
-
 	custom_uart_init();
 
 	gpio_put(LED_PIN, 1);
 
-	msg.data = 0;
 	while (true)
 	{
+  switch (state) {
+    case WAITING_AGENT:
+      EXECUTE_EVERY_N_MS(500, state = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_AVAILABLE : WAITING_AGENT;);
+      break;
+    case AGENT_AVAILABLE:
+      state = (true == create_entities()) ? AGENT_CONNECTED : WAITING_AGENT;
+      if (state == WAITING_AGENT) {
+        destroy_entities();
+      };
+      break;
+    case AGENT_CONNECTED:
+      EXECUTE_EVERY_N_MS(200, state = (RMW_RET_OK == rmw_uros_ping_agent(100, 1)) ? AGENT_CONNECTED : AGENT_DISCONNECTED;);
+      if (state == AGENT_CONNECTED) {
+        rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
+      }
+      break;
+    case AGENT_DISCONNECTED:
+      destroy_entities();
+      state = WAITING_AGENT;
+      break;
+    default:
+      break;
+  }
 		rclc_executor_spin_some(&executor, RCL_MS_TO_NS(100));
 	}
 	return 0;
