bcf14384f2ec07fd6c1167976d96d41d1e0f9482
n800s <n800sau@gmail.com>
2026-08-11 12:37:56 +1000

    added logrotate etc

--- dumbproxy/setup_etc.sh
+++ dumbproxy/setup_etc.sh
@@ -1,6 +1,5 @@
 systemctl daemon-reload
-systemctl enable dumbproxy.socket
-systemctl start dumbproxy.socket
 systemctl enable dumbproxy.service
 systemctl start dumbproxy.service
 service rsyslog restart
+service logrotate restart
